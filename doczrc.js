db31b580609410190e0491ca62f36ee0626956b9
Robert Douglas <rob@anucreative.com>
2019-04-19 11:04:16 +0200

    Temporary theme
    Signed-off-by: Robert Douglas <robert@wttj.co>

--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,51 @@
+import { welcomeTheme } from './src/theme/welcome'
+import { createTheme } from './src/theme/core'
+
+const theme = createTheme(welcomeTheme)
+
 export default {
   wrapper: 'wrapper',
   hashRouter: true,
+  htmlContext: {
+    favicon: 'https://cdn.welcometothejungle.co/wttj-front/assets/images/favicon.ico'
+  },
   themeConfig: {
-    // mode: 'dark'
+    colors: {
+      primary: theme.colors.primary
+    },
+    styles: {
+      body: {
+        fontFamily: theme.fontFamily.texts,
+        'nav a': {
+          fontSize: theme.fontSize.sm
+        }
+      },
+      h1: {
+        fontFamily: theme.fontFamily.headings,
+        fontSize: theme.fontSize.xxxl,
+        fontWeight: theme.fontWeight.bold,
+        marginBottom: '1rem'
+      },
+      h2: {
+        fontFamily: theme.fontFamily.headings,
+        fontSize: theme.fontSize.xl,
+        fontWeight: theme.fontWeight.black,
+        textTransform: 'uppercase',
+        color: theme.colors.secondary,
+        borderBottom: 'none',
+        marginTop: '4rem'
+      },
+      h3: {
+        fontFamily: theme.fontFamily.headings,
+        fontSize: theme.fontSize.md,
+        fontWeight: theme.fontWeight.bold,
+        textTransform: 'uppercase'
+      }
+    },
+    logo: {
+      src: 'https://cdn.welcometothejungle.co/wttj-front/assets/images/logos/wttj-long.svg',
+      width: 200
+    }
   },
   port: 3020
 }
