1c8badef1389a6585df096d421ca2d9b8ee360d2
theojungle <theo.mesnil@wttj.co>
2019-05-13 17:23:18 +0200

    refactor: transform DummyComponent to functional component

--- src/atoms/DummyComponent/index.js
+++ src/atoms/DummyComponent/index.js
@@ -1,15 +1,17 @@
-import React, { PureComponent } from 'react'
-import { oneOf, string } from 'prop-types'
+import React from 'react'
+import { oneOf, string, node } from 'prop-types'
 
 import StyledDummyComponent from './styles'
 
-export class DummyComponent extends PureComponent {
-  render() {
-    return <StyledDummyComponent {...this.props}>{this.props.children}</StyledDummyComponent>
-  }
-}
+export const DummyComponent = ({ children, size, type, ...props }) => (
+  <StyledDummyComponent size={size} type={type} {...props}>
+    {children}
+  </StyledDummyComponent>
+)
 
 DummyComponent.propTypes = {
+  /** Node component from parent */
+  children: node,
   /** Size of component */
   size: oneOf(['auto', 'sm', 'md', 'lg']),
   /** Type of component */
