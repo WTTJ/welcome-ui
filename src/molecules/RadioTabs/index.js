0b6670de7b3bb24625e7d4b057219befc761c472
theojungle <theo.mesnil@wttj.co>
2019-05-13 18:02:22 +0200

    refactor: destructuring RadioTabs

--- src/molecules/RadioTabs/index.js
+++ src/molecules/RadioTabs/index.js
@@ -1,24 +1,21 @@
 import React from 'react'
 
-// atmoms
+// atoms
 import { RadioTab } from '../../atoms/RadioTab/index'
 
 import { RadioTabs as StyledRadioTabs } from './styles'
 
-export const RadioTabs = props => {
-  const { checked, name, onChange, options } = props
-  return (
-    <StyledRadioTabs>
-      {options.map((radio, i) => (
-        <RadioTab
-          checked={checked === radio.id}
-          id={radio.id}
-          label={radio.label}
-          name={name}
-          key={radio.id}
-          onClick={onChange}
-        />
-      ))}
-    </StyledRadioTabs>
-  )
-}
+export const RadioTabs = ({ checked, name, onChange, options }) => (
+  <StyledRadioTabs>
+    {options.map((radio, i) => (
+      <RadioTab
+        checked={checked === radio.id}
+        id={radio.id}
+        label={radio.label}
+        name={name}
+        key={radio.id}
+        onClick={onChange}
+      />
+    ))}
+  </StyledRadioTabs>
+)
