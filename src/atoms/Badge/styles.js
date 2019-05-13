5b349c62f74622e10069662d28c03634ab5e8c09
theojungle <theo.mesnil@wttj.co>
2019-05-13 17:07:07 +0200

    refactor: add css props function on Badges

--- src/atoms/Badge/styles.js
+++ src/atoms/Badge/styles.js
@@ -1,24 +1,27 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { getVariantColor } from '../../common/styles/form'
 import { get } from '../../theme/helpers'
 
-export const Badge = styled.div`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  min-width: ${props => props.size};
-  max-width: ${props => (props.length === 1 ? props.size : '100%')};
-  height: ${props => props.size};
-  padding-right: ${props =>
-    props.length === 1 ? 0 : get('padding', props.padding)(props) || props.padding || null};
-  padding-left: ${props =>
-    props.length === 1 ? 0 : get('padding', props.padding)(props) || props.padding || null};
-  color: ${get('color', 'white')};
-  ${get('textStyles', 'badge')};
-  font-size: ${props => get('fontSize', props.fontsize)(props) || props.fontsize || null};
-  background-color: ${props => getVariantColor(props.variant)};
-  border-radius: ${props => get('radius', props.radius)(props) || props.radius || props.size};
-`
+export const Badge = styled.div(
+  props => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    min-width: ${props.size};
+    max-width: ${props.length === 1 ? props.size : '100%'};
+    height: ${props.size};
+    padding-right: ${props.length === 1
+      ? 0
+      : get('padding', props.padding)(props) || props.padding || null};
+    padding-left: ${props =>
+      props.length === 1 ? 0 : get('padding', props.padding)(props) || props.padding || null};
+    color: ${get('color', 'white')};
+    ${get('textStyles', 'badge')};
+    font-size: ${get('fontSize', props.fontsize)(props) || props.fontsize || null};
+    background-color: ${getVariantColor(props.variant)};
+    border-radius: ${get('radius', props.radius)(props) || props.radius || props.size};
+  `
+)
 
 export default Badge
