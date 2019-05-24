68da84b50cc728f62959fbc68f00d53ecd12aefa
Robert Douglas <rob@anucreative.com>
2019-05-24 06:34:48 +0200

    Get final-form elements working
    Signed-off-by: Robert Douglas <robert@wttj.co>

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -3,22 +3,41 @@ import PropTypes from 'prop-types'
 
 import { StyledInputText } from './styles'
 
-export const InputText = ({ disabled, fieldProps, name, placeholder, variant, type }) => (
-  <StyledInputText
-    disabled={disabled}
-    id={name}
-    placeholder={placeholder}
-    type={type}
-    variant={variant}
-    {...fieldProps}
-  />
-)
+export const InputText = ({
+  disabled,
+  name,
+  onChange,
+  onBlur,
+  onFocus,
+  placeholder,
+  variant,
+  value,
+  type = 'text'
+}) => {
+  return (
+    <StyledInputText
+      disabled={disabled}
+      id={name}
+      name={name}
+      onBlur={onBlur}
+      onChange={onChange}
+      onFocus={onFocus}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      variant={variant}
+    />
+  )
+}
 
 InputText.propTypes = {
   disabled: PropTypes.bool,
-  fieldProps: PropTypes.object,
   name: PropTypes.string,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
   placeholder: PropTypes.string,
   type: PropTypes.string,
+  value: PropTypes.string,
   variant: PropTypes.oneOf(['error', 'info', 'valid', 'warning'])
 }
--- src/components/RadioGroup/styles.js
+++ src/components/RadioGroup/styles.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 
 import { system } from '../../utils/utils'
 
-export const Radios = styled.div(
+export const StyledRadios = styled.div(
   () => css`
     display: flex;
     flex-direction: column;
