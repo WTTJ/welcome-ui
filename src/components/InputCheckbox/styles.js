4416e39718e5a0728f0c6015c47e458c7af65f8c
Robert Douglas <rob@anucreative.com>
2019-05-27 10:42:47 +0200

    Update docs and rename components

--- src/components/InputCheckbox/styles.js
+++ src/components/InputCheckbox/styles.js
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components'
 import { system } from '../../utils/utils'
 import { get, getCss } from '../../theme/helpers'
 import { fieldTypeStyles } from '../../common/styles/form'
-import { StyledCheckbox } from '../InputCheckbox/styles'
 
 const checkedStyles = css`
   ${getCss('fields.checkboxes.checked')};
