571a36fffc0bb2fb6ef396c3eb30343dcf4ef3f9
Théo <50322149+theojungle@users.noreply.github.com>
2019-05-28 12:15:38 +0200

    docs: add doc for Box and Icon (#31)

--- docz.styled.js
+++ docz.styled.js
@@ -1,6 +1,9 @@
-import styled from 'styled-components'
 import { Playground } from 'docz'
+import React from 'react'
+import styled from 'styled-components'
 
+import { Box } from './src/components/Box'
+import { Icon } from './src/components/Icon'
 import { get } from './src/theme/helpers'
 
 export const StyledPlayground = styled(Playground)`
@@ -11,5 +14,34 @@ export const StyledPlayground = styled(Playground)`
     margin-bottom: ${get('space.sm')};
   }
 `
+
+export const IconsList = icons => (
+  <Box display="flex" flexWrap="wrap">
+    {icons.map(icon => (
+      <Box
+        alignItems="center"
+        border="1px solid #CED4DE"
+        display="flex"
+        flexDirection="column"
+        key={icon}
+        margin="10px"
+        width="120px"
+      >
+        <Box display="flex" padding="15px 10px">
+          <Icon name={icon} size="lg" />
+        </Box>
+        <Box
+          borderTop="1px solid #CED4DE"
+          fontSize="meta2"
+          padding="5px 0"
+          textAlign="center"
+          width={1}
+        >
+          {icon}
+        </Box>
+      </Box>
+    ))}
+  </Box>
+)
 
 export default StyledPlayground
