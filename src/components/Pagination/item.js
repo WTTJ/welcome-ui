6eb668379f6c0f3198472b0426f7f10ab71ddde9
theojungle <mesniltheo@gmail.com>
2019-06-10 11:57:13 +0200

    refactor: change onClick on Pagination item

--- src/components/Pagination/item.js
+++ src/components/Pagination/item.js
@@ -3,13 +3,19 @@ import React, { forwardRef } from 'react'
 
 import * as S from './styles'
 
-export const PageItem = forwardRef(({ page, activePage, handleChange }, ref) => (
-  <li>
-    <S.PageItem active={page === activePage} onClick={() => handleChange(page)} ref={ref}>
-      {page}
-    </S.PageItem>
-  </li>
-))
+export const PageItem = forwardRef(({ page, activePage, handleChange }, ref) => {
+  const handleClick = () => {
+    handleChange(page)
+  }
+
+  return (
+    <li>
+      <S.PageItem active={page === activePage} onClick={handleClick} ref={ref}>
+        {page}
+      </S.PageItem>
+    </li>
+  )
+})
 
 PageItem.displayName = 'PageItem'
 
