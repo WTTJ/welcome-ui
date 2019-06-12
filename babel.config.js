fea2cc0c521a486d074ab465a70d077415027b0b
Greg Bergé <greg@smooth-code.com>
2019-06-12 11:11:39 +0200

    fix: fix rollup build (#73)
    * fix: fix rollup build
    
    * perf: enable loose mode (reduce build size)

--- babel.config.js
+++ babel.config.js
@@ -4,16 +4,10 @@ module.exports = {
       plugins: ['@babel/plugin-transform-modules-commonjs']
     }
   },
-  presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+  presets: [['@babel/preset-env', { modules: false, loose: true }], '@babel/preset-react'],
   plugins: [
     ['transform-react-remove-prop-types', { removeImport: true }],
-    [
-      'styled-components',
-      {
-        ssr: true,
-        displayName: true
-      }
-    ],
-    '@babel/plugin-proposal-class-properties'
+    ['styled-components', { ssr: true, displayName: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
   ]
 }
