d2aca586ec4b26623256051ae745cc535437e41a
Robert Douglas <rob@anucreative.com>
2019-06-20 12:09:17 +0200

    style: format mdx with prettier (#120)
    * style: prettier format mdx files
    
    * style: fix previous lint rules

---
+++ src/components/ConnectedField/index.test.js
@@ -0,0 +1,49 @@
+import React, { createRef } from 'react'
+import { Form } from 'react-final-form'
+
+import { render } from '../../utils/tests'
+import { InputText } from '../InputText/index'
+
+import { ConnectedField } from './index'
+
+const content = 'Jungle'
+
+const noop = () => {}
+
+describe.skip('<ConnectedField>', () => {
+  it('should render correctly and hold `ref`', () => {
+    const inputRef = createRef()
+    const { getByTestId } = render(
+      <Form
+        initialValues={{ jungle: content }}
+        onSubmit={noop}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <ConnectedField component={InputText} name="jungle" ref={inputRef} />
+          </form>
+        )}
+      />
+    )
+
+    const input = getByTestId('input')
+    expect(input).toHaveValue(content)
+    expect(inputRef.current.name).toEqual('jungle')
+  })
+
+  it.skip('should throw error if invalid component provided', () => {
+    const { getByTestId } = render(
+      <Form
+        initialValues={{ jungle: content }}
+        onSubmit={noop}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <ConnectedField component={noop} name="jungle" />
+          </form>
+        )}
+      />
+    )
+
+    const input = getByTestId('input')
+    expect(input).toHaveValue(content)
+  })
+})
