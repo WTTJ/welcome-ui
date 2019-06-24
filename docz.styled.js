d568085adc12748da93adcb5b941211ef2402f29
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2019-06-24 14:42:11 +0200

    docs: dynamize colors palette (#129)

--- docz.styled.js
+++ docz.styled.js
@@ -63,14 +63,14 @@ export const Colors = ({ colors, withBorder }) => (
       <Box key={color} marginBottom="10px" marginRight="10px">
         <Box
           backgroundColor={colors[color]}
-          borderColor={withBorder && 'nude.300'}
+          borderColor={withBorder && 'nude.100'}
           borderStyle={withBorder && 'solid'}
           borderWidth={withBorder && '1px'}
           height="40px"
           width="90px"
         />
         <Box
-          borderColor="nude.300"
+          borderColor="nude.100"
           borderStyle="solid"
           borderWidth="0 1px 1px 1px"
           padding="5px"
