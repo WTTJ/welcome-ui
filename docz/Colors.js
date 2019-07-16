014f9bee08578428df668eebc89539e926be6627
theojungle <mesniltheo@gmail.com>
2019-07-16 18:06:34 +0200

    docs: add theming select

--- docz/Colors.js
+++ docz/Colors.js
@@ -1,33 +1,40 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { withTheme } from '@xstyled/styled-components'
 
 import { Box } from '../src/index'
 
-export const Colors = ({ colors, withBorder }) => (
-  <Box display="flex" flexWrap="wrap">
-    {Object.keys(colors).map(color => (
-      <Box key={color} marginBottom="10px" marginRight="10px">
-        <Box
-          backgroundColor={colors[color]}
-          borderColor={withBorder && 'nude.100'}
-          borderStyle={withBorder && 'solid'}
-          borderWidth={withBorder && '1px'}
-          height="40px"
-          width="90px"
-        />
-        <Box
-          borderColor="nude.100"
-          borderStyle="solid"
-          borderWidth="0 1px 1px 1px"
-          padding="5px"
-          textAlign="center"
-        >
-          <Box fontWeight="medium">{color}</Box>
-          <Box color="nude.700" fontSize="meta2" marginTop="3px">
-            {colors[color]}
+const Wrapper = ({ name, theme, withBorder }) => {
+  const colors = theme.colors[name]
+
+  return (
+    <Box display="flex" flexWrap="wrap">
+      {Object.entries(colors).map(([key, depth]) => (
+        <Box key={key} marginBottom="10px" marginRight="10px">
+          <Box
+            backgroundColor={`${name}.${key}`}
+            borderColor={withBorder && 'nude.100'}
+            borderStyle={withBorder && 'solid'}
+            borderWidth={withBorder && '1px'}
+            height="40px"
+            width="90px"
+          />
+          <Box
+            borderColor="nude.100"
+            borderStyle="solid"
+            borderWidth="0 1px 1px 1px"
+            padding="5px"
+            textAlign="center"
+          >
+            <Box fontWeight="medium">{key}</Box>
+            <Box color="nude.700" fontSize="meta2" marginTop="3px">
+              {depth}
+            </Box>
           </Box>
         </Box>
-      </Box>
-    ))}
-  </Box>
-)
+      ))}
+    </Box>
+  )
+}
+
+export const Colors = withTheme(Wrapper)
--- wrapper.js
+++ wrapper.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { node } from 'prop-types'
 import { ThemeProvider } from '@xstyled/styled-components'
 import { Provider } from 'reakit'
 
+import { Select } from './src'
 import { welcomekitTheme } from './src/theme/welcomekit'
+import { welcomeTheme } from './src/theme/welcome'
 import { createTheme } from './src/theme/core'
 import { GlobalStyle } from './src/utils/'
 
-const theme = createTheme(welcomekitTheme)
+const THEMES = [
+  { value: 'welcomekit', label: 'WelcomeKit' },
+  { value: 'welcome', label: 'WelcomeToTheJungle' },
+  { value: 'core', label: 'Core' }
+]
 
 export default function Wrapper({ children }) {
+  const [theme, setTheme] = useState(welcomekitTheme)
+
+  const chooseTheme = e => {
+    const value = e.target.value.value
+    // console.log(value)
+    switch (value) {
+      case 'welcomekit':
+        setTheme(welcomekitTheme)
+        break
+      case 'welcome':
+        setTheme(welcomeTheme)
+        break
+      default:
+        setTheme()
+    }
+  }
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={createTheme(theme)}>
       <Provider>
+        <div style={{ position: 'fixed', zIndex: 9999, right: '80px', top: '4px' }}>
+          <Select onChange={chooseTheme} options={THEMES} size="sm" value={THEMES[0]} />
+        </div>
         <GlobalStyle />
         {children}
       </Provider>
