3808e252815b5fec4b6f806470c2be3c557884c7
Robert Douglas <rob@anucreative.com>
2019-07-25 16:42:04 +0200

    fix: remove duplicate label

--- src/components/RadioGroup/index.js
+++ src/components/RadioGroup/index.js
@@ -3,17 +3,11 @@ import { bool, node, string } from 'prop-types'
 
 import { DIRECTIONS_TYPE } from '../../utils'
 import { FieldGroup } from '../FieldGroup'
-import { Label } from '../Label'
 
 import * as S from './styles'
 
 export const RadioGroup = ({ children, flexDirection, label, required }) => (
-  <FieldGroup>
-    {label && (
-      <Label as="legend" required={required}>
-        {label}
-      </Label>
-    )}
+  <FieldGroup label={label} required={required}>
     <S.Radios flexDirection={flexDirection}>
       {children.map(child => cloneElement(child, { key: child.props.value, flexDirection }))}
     </S.Radios>
