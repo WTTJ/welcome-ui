f7d4a133708e8b472b95e81e53768e0fd21c4215
Robert Douglas <rob@anucreative.com>
2019-09-12 13:06:01 +0200

    refactor: use css from xstyled

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,9 +1,15 @@
-export function getTransformStyle(transform) {
-  return {
-    transform,
-    MozTransform: transform,
-    WebkitTransform: transform,
-    OTransform: transform,
-    MsTransform: transform
-  }
-}
+import { css } from '@xstyled/styled-components'
+
+export const getTransformStyle = transform => ({
+  transform,
+  MozTransform: transform,
+  WebkitTransform: transform,
+  OTransform: transform,
+  MsTransform: transform
+})
+
+export const centerContent = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
