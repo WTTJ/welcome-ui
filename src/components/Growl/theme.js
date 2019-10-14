8fdb69772dc0a7c9d390040bd877d89d604d86ec
Paul-Xavier Ceccaldi <pix@wttj.co>
2019-10-14 14:42:52 +0200

    fix: growl close overlaping title and general line-heights missing (#385)
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- src/components/Growl/theme.js
+++ src/components/Growl/theme.js
@@ -1,21 +1,26 @@
-export const getGrowls = ({ colors, fontSizes, fontWeights, radii }) => ({
-  default: {
-    'font-size': fontSizes.body3,
-    'background-color': colors.light[200],
-    'border-color': colors.nude[200],
-    'border-width': '1px',
-    'border-style': 'solid',
-    'border-radius': radii.md
-  },
-  title: {
-    'font-size': fontSizes.body2,
-    'font-weight': fontWeights.bold
-  },
-  close: {
-    'background-color': colors.nude[200],
-    'border-radius': '50%',
-    '&:hover, &:focus': {
-      'background-color': colors.nude[400]
+import { getTexts } from '../../theme/typography'
+
+export const getGrowls = theme => {
+  const { colors, fontWeights, radii } = theme
+  return {
+    default: {
+      ...getTexts(theme).body3,
+      'background-color': theme.colors.light[200],
+      'border-color': colors.nude[200],
+      'border-width': '1px',
+      'border-style': 'solid',
+      'border-radius': radii.md
+    },
+    title: {
+      ...getTexts(theme).body2,
+      'font-weight': fontWeights.bold
+    },
+    close: {
+      'background-color': colors.nude[200],
+      'border-radius': '50%',
+      '&:hover, &:focus': {
+        'background-color': colors.nude[400]
+      }
     }
   }
-})
+}
