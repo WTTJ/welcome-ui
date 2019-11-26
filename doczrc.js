9f8d8eef8046bdc34c3a16e2ee95ac45e3676504
theojungle <mesniltheo@gmail.com>
2019-11-26 17:14:44 +0100

    docs: remove useless menu

--- doczrc.js
+++ doczrc.js
@@ -3,6 +3,7 @@ export default {
   description: 'Design system of @wttj with react, styled-components and styled-system',
   dest: '/docs',
   menu: ['Getting Started', 'Theming', 'Components', 'Fields'],
+  ignore: ['README.md', 'CHANGELOG.md', 'CONTRIBUTING.md'],
   port: 3020,
   // https://github.com/pedronauck/docz/issues/777#issuecomment-489947783
   filterComponents: files => files.filter(filepath => /w*.(js|jsx|ts|tsx)$/.test(filepath))
