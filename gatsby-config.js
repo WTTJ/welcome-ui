6b669e84074c4e528837597e3810d0cdd30839b4
theojungle <mesniltheo@gmail.com>
2019-12-10 17:03:47 +0100

    docs: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: ['gatsby-theme-docz', 'gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components']
+  plugins: [
+    'gatsby-theme-docz',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-56009608-24'
+      }
+    }
+  ]
 }
