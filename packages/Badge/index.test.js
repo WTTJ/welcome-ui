06f3dc18b4eadf89a0279b186eb60374d86ba2ba
Robert Douglas <rob@anucreative.com>
2019-12-12 15:17:51 +0100

    chore: import input-text

---
+++ packages/Badge/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { render } from '../utils/tests'
+
+import { Badge } from './index'
+
+const content = '1'
+
+describe('<Badge>', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Badge>{content}</Badge>)
+
+    expect(container).toHaveTextContent(content)
+  })
+})
--- src/components/Badge/index.js
+++
@@ -1,7 +0,0 @@
-import React, { forwardRef } from 'react'
-
-import { Tag } from '../Tag'
-
-export const Badge = forwardRef((props, ref) => <Tag ref={ref} size="sm" {...props} />)
-
-Badge.displayName = 'Badge'
