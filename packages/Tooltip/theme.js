496e721f7a9cda09376cb944fdb8856052d2cb8c
Paul-Xavier Ceccaldi <pix@wttj.co>
2019-12-12 21:18:14 +0000

    fix: add UniversalLink and Tooltip as packages
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

---
+++ packages/Tooltip/theme.js
@@ -0,0 +1,6 @@
+export const getTooltips = ({ colors, radii, toRem }) => ({
+  'max-width': toRem(200),
+  color: colors.light[200],
+  background: colors.dark[700],
+  'border-radius': radii.md
+})
