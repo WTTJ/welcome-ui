475e8d49cd610dc796e15c7c49b28787c0df7559
Robert Douglas <rob@anucreative.com>
2019-12-13 13:48:03 +0100

    chore: convert shape

---
+++ packages/Shape/styles.js
@@ -0,0 +1,29 @@
+import styled, { css } from '@xstyled/styled-components'
+
+import { getMax } from '../Core/utils/getMax'
+import { system } from '../Core/utils/system'
+
+const shapeStyles = (width, height, shape) => css`
+  width: ${getMax(width, height)};
+  height: ${getMax(width, height)};
+  border-radius: ${shape === 'circle' && '50%'};
+`
+
+export const Shape = styled.div(
+  ({ height, shape, width }) => css`
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    ${system}
+    ${shape && shapeStyles(width, height, shape)}
+
+  img {
+      object-fit: cover;
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+    }
+  `
+)
