ee6ab1ceb42bba17218baeffd6c1871c274128d6
Robert Douglas <rob@anucreative.com>
2019-12-13 13:48:48 +0100

    chore: convert tabs

---
+++ packages/Tabs/theme.js
@@ -0,0 +1,40 @@
+export const getTabs = theme => {
+  const { borderWidths, colors, fontSizes, fontWeights, space } = theme
+
+  return {
+    tabsBorder: {
+      bottom: borderWidths.sm,
+      width: '100%',
+      height: borderWidths.sm,
+      'background-color': colors.nude[200]
+    },
+    item: {
+      default: {
+        color: colors.nude[600],
+        'font-size': fontSizes.body2,
+        'font-weight': fontWeights.medium
+      },
+      active: {
+        color: colors.dark[700]
+      },
+      focus: {
+        color: colors.nude[800]
+      },
+      disabled: {
+        color: colors.nude[400]
+      }
+    },
+    panel: {
+      'margin-top': space.xl,
+      '&:focus': {
+        outline: 'none'
+      }
+    },
+    activeBar: {
+      bottom: 0,
+      background: colors.primary[500],
+      height: '3px',
+      'border-radius': '3px'
+    }
+  }
+}
