fcb06f5e22726f31b193b9c33d76f287945e11f9
Robert Douglas <rob@anucreative.com>
2019-12-13 13:14:59 +0100

    chore: convert growl

---
+++ packages/Growl/title.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { node, oneOf } from 'prop-types'
+
+import { VARIANTS_TYPE } from '../Core/utils/propTypes'
+
+import { Title } from './styles'
+
+export const GrowlTitle = ({ children, dataTestId, variant = 'info' }) => (
+  <Title data-testid={dataTestId} variant={variant}>
+    {children}
+  </Title>
+)
+
+GrowlTitle.propTypes = {
+  children: node.isRequired,
+  variant: oneOf(VARIANTS_TYPE)
+}
