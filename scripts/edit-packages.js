d51153ba5141d8e3ba71d516c0a9509f93be6a6d
Robert Douglas <rob@anucreative.com>
2019-12-20 09:45:25 +0100

    feat:  script to update all package.jsons

---
+++ scripts/edit-packages.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+
+const kebabCase = require('lodash.kebabcase')
+
+fs.readdirAsync = util.promisify(fs.readdir)
+fs.writeFileAsync = util.promisify(fs.writeFile)
+
+const ROOT_PATH = path.join(__dirname, '../packages')
+
+// UPDATE THIS FUNCTION TO RETURN NEW CONFIG FOR PACKAGE.JSON
+// componentName: ConnectedField
+// pkgName: connected-field
+// config: content of package.json
+// eslint-disable-next-line no-unused-vars
+const getNewConfig = ({ componentName, config, pkgName }) => {
+  return {
+    main: `dist/${pkgName}.cjs.js`,
+    module: `dist/${pkgName}.es.js`
+  }
+}
+
+const collectPackages = packages =>
+  Promise.all(
+    packages.map(pkg => ({
+      name: pkg.name,
+      config: require(`${ROOT_PATH}/${pkg.name}/package.json`)
+    }))
+  )
+
+const updatePackageConfig = pkg => {
+  const componentName = pkg.name
+  const pkgName = kebabCase(componentName)
+  const newConfig = {
+    ...pkg.config,
+    ...getNewConfig({ ...pkg, pkgName })
+  }
+  fs.writeFileAsync(`${ROOT_PATH}/${componentName}/package.json`, JSON.stringify(newConfig, 0, 2))
+    // eslint-disable-next-line no-console
+    .then(() => console.debug('Successfully wrote', pkgName))
+    .catch(handleError)
+}
+
+const handleError = err => {
+  // eslint-disable-next-line no-console
+  console.error(err)
+  throw err
+}
+
+fs.readdirAsync(ROOT_PATH, { withFileTypes: true })
+  .then(files => files.filter(f => f.isDirectory()))
+  .then(collectPackages)
+  .then(packages => packages.forEach(updatePackageConfig))
+  .catch(handleError)
