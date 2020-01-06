b292ad74ccdcc06afd5f1c315e5be5e9c226ab33
Robert Douglas <rob@anucreative.com>
2020-01-06 12:32:22 +0100

    fix: revert babel-plugin-styled-components to latest version

--- babel.config.js
+++ babel.config.js
@@ -6,11 +6,11 @@ module.exports = {
   },
   presets: [['@babel/preset-env', { modules: false, loose: true }], '@babel/preset-react'],
   plugins: [
+    ['transform-react-remove-prop-types', { removeImport: true }],
     [
       'styled-components',
       { ssr: true, displayName: true, pure: true, transpileTemplateLiterals: true }
     ],
-    ['transform-react-remove-prop-types', { removeImport: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }]
   ],
   overrides: [
