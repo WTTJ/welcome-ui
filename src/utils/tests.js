9e7aa30ec99cc6458d7db1d700a0c0fabd22553d
Robert Douglas <rob@anucreative.com>
2020-01-09 10:48:20 +0100

    chore: add types, tsconfig and upgrade jest-dom

--- src/utils/tests.js
+++ src/utils/tests.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { ThemeProvider } from '@xstyled/styled-components'
 import { StaticRouter } from 'react-router-dom'
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 import 'jest-styled-components'
 
 import { createTheme } from '../../packages/Core/theme/core'
