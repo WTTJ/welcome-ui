60e73c134cf24b65b890817add03ef4b54bbdffe
Robert Douglas <rob@anucreative.com>
2020-01-09 11:55:11 +0100

    chore: update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,16 @@
-module.exports = require('wttj-config-front/.eslintrc')
+const config = require('wttj-config-front/.eslintrc')
+
+module.exports = {
+  ...config,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    ...config.rules,
+    'react/prop-types': 'off'
+  }
+}
