d22ac2f68aec93b049be0d54c816634b787cd085
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-01-21 14:29:51 +0100

    Update packages/FileDrop/index.js
    Co-Authored-By: Robert Douglas <rob@anucreative.com>

--- packages/FileDrop/index.js
+++ packages/FileDrop/index.js
@@ -175,7 +175,7 @@ FileDrop.type = 'FileDrop'
 FileDrop.displayName = 'FileDrop'
 
 FileDrop.propTypes /* remove-proptypes */ = {
-  /** Can accept a string ie "image/png" or a string of strings ie "image/png,image/jpeg"  */
+  /** Pass a comma-separated string of file types e.g. "image/png" or "image/png,image/jpeg"  */
   accept: string,
   children: func,
   disabled: bool,
