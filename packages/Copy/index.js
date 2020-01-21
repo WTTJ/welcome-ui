7c6d0bc16ea719dab569277344c31ce832b9e1b8
Robert Douglas <robert@wttj.co>
2020-01-21 09:30:05 +0000

    fix: let useCopyText take a string (or number) again

--- packages/Copy/index.js
+++ packages/Copy/index.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import get from 'lodash.get'
+import { elementType, number, oneOfType, shape, string } from 'prop-types'
 import copyToClipboard from 'copy-to-clipboard'
 
-export function useCopyText(copyRef, timeout) {
-  const [copied, setCopied] = React.useState()
+export function useCopyText(content, timeout) {
+  const [copied, setCopied] = useState()
 
-  const copy = React.useCallback(() => {
-    const value =
-      copyRef && copyRef.current && (copyRef.current.textContent || copyRef.current.value)
+  const copy = useCallback(() => {
+    let value
+    if (['number', 'string'].includes(typeof content)) {
+      value = content
+    } else if (content.current) {
+      value = content.current.textContent || content.current.value
+    }
+
     const copiedString = copyToClipboard(value)
     setCopied(copiedString)
 
     if (timeout) {
       setTimeout(setCopied, timeout)
     }
-  }, [copyRef, timeout])
+  }, [content, timeout])
 
   return [copy, copied]
 }
