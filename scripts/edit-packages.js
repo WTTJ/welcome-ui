54c79a288b068182b9015d83580ce2ecfc6c04fe
Paul-Xavier Ceccaldi <pix@wttj.co>
2020-01-31 09:04:58 +0100

    chore: upgrade styled-components to v5 (#518)
    * chore: upgrade styled-components to v5

--- scripts/edit-packages.js
+++ scripts/edit-packages.js
@@ -11,7 +11,7 @@ const kebabCase = require('lodash.kebabcase')
 fs.readdirAsync = util.promisify(fs.readdir)
 fs.writeFileAsync = util.promisify(fs.writeFile)
 
-const ROOT_PATH = path.join(__dirname, '../packages')
+const DIRECTORIES = [path.join(__dirname, '../packages'), path.join(__dirname, '../icons')]
 
 // UPDATE THIS FUNCTION TO RETURN NEW CONFIG FOR PACKAGE.JSON
 // componentName: ConnectedField
@@ -20,33 +20,37 @@ const ROOT_PATH = path.join(__dirname, '../packages')
 // eslint-disable-next-line no-unused-vars
 const getNewConfig = ({ componentName, config, pkgName }) => {
   return {
+    license: 'MIT',
     publishConfig: {
       access: 'public'
     }
   }
 }
 
-const collectFolders = () =>
+const collectFolders = dir =>
   fs
-    .readdirAsync(ROOT_PATH, { withFileTypes: true })
-    .then(files => files.filter(f => f.isDirectory()))
+    .readdirAsync(dir, { withFileTypes: true })
+    .then(files => files.filter(f => f.isDirectory()).map(folder => ({ dir, folder })))
 
 const collectPackages = folder =>
   folder
-    .filter(({ name }) => !name.startsWith('_'))
-    .map(({ name }) => ({
+    .filter(
+      ({ folder: { name } }) => !(name.startsWith('_') || ['dist', 'node_modules'].includes(name))
+    )
+    .map(({ dir, folder: { name } }) => ({
+      dir,
       pkgName: kebabCase(name),
       componentName: name,
-      config: require(`${ROOT_PATH}/${name}/package.json`)
+      config: require(`${dir}/${name}/package.json`)
     }))
 
 const updatePackageConfig = pkg => {
-  const { componentName, config, pkgName } = pkg
+  const { componentName, config, dir, pkgName } = pkg
   const newConfig = {
     ...config,
     ...getNewConfig(pkg)
   }
-  fs.writeFileAsync(`${ROOT_PATH}/${componentName}/package.json`, JSON.stringify(newConfig, 0, 2))
+  fs.writeFileAsync(`${dir}/${componentName}/package.json`, JSON.stringify(newConfig, 0, 2))
     // eslint-disable-next-line no-console
     .then(() => console.debug('Successfully wrote', pkgName))
     .catch(handleError)
@@ -58,7 +62,9 @@ const handleError = err => {
   throw err
 }
 
-collectFolders()
-  .then(collectPackages)
-  .then(packages => packages.forEach(updatePackageConfig))
-  .catch(handleError)
+DIRECTORIES.forEach(dir =>
+  collectFolders(dir)
+    .then(collectPackages)
+    .then(packages => packages.forEach(updatePackageConfig))
+    .catch(handleError)
+)
