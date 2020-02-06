ab025ce4b906ed3796fec21201e864e09e126068
Robert Douglas <robert@wttj.co>
2020-02-06 08:40:03 +0000

    chore: point to reakit/Tooltip rather than reakit (for treeshaking)

--- packages/Tooltip/styles.js
+++ packages/Tooltip/styles.js
@@ -1,6 +1,6 @@
 import styled from '@xstyled/styled-components'
 import { th } from '@xstyled/system'
-import { Tooltip as ReakitTooltip } from 'reakit'
+import { Tooltip as ReakitTooltip } from 'reakit/Tooltip'
 import { filterComponent, system } from '@welcome-ui/system'
 
 export const Tooltip = styled(filterComponent(ReakitTooltip))`
