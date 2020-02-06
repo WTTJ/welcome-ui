1c8375d4cfca5111bda944a441457a6906810df9
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-02-06 18:17:56 +0100

    fix: toggle animation on disabled (#528)

--- packages/Toggle/styles.js
+++ packages/Toggle/styles.js
@@ -12,12 +12,12 @@ const afterCheckedStyles = css`
 
 const checkedStyles = css`
   ${th('fields.toggles.checked')};
-  ${afterCheckedStyles}
+  ${afterCheckedStyles};
 `
 
 const checkedDisabledStyles = css`
   ${th('fields.toggles.checkedDisabled')};
-  ${afterCheckedStyles}
+  ${afterCheckedStyles};
 `
 
 export const Toggle = styled(filterFieldComponent(ReakitCheckbox))(
@@ -25,43 +25,45 @@ export const Toggle = styled(filterFieldComponent(ReakitCheckbox))(
     const toggleSize = th('fields.toggles.default.width')
 
     return css`
-    appearance: none;
-    background: transparent;
-    border: ${th.borderWidth('sm')} solid;
-    outline: none;
-    ${th('fields.toggles.default')};
-    ${disabled && th('fields.toggles.disabled')};
-    position: relative;
-    display: block;
-    width: calc(${toggleSize} * 2);
-    height: calc(${toggleSize} + 2 * ${th.borderWidth('sm')});
-    cursor: ${disabled ? 'not-allowed' : 'pointer'};
-    border-radius: ${toggleSize};
-    transition: 0.3s;
-    order: ${order};
-
-    &::after {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: ${toggleSize};
-      height: ${toggleSize};
+      appearance: none;
+      background: transparent;
+      border: ${th.borderWidth('sm')} solid;
+      outline: none;
+      ${th('fields.toggles.default')};
+      ${disabled && th('fields.toggles.disabled')};
+      position: relative;
+      display: block;
+      width: calc(${toggleSize} * 2);
+      height: calc(${toggleSize} + 2 * ${th.borderWidth('sm')});
+      cursor: ${disabled ? 'not-allowed' : 'pointer'};
       border-radius: ${toggleSize};
       transition: 0.3s;
-    }
+      order: ${order};
 
-    &:active::after {
-      width: ${disabled ? null : '55%'};
-    }
+      &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: ${toggleSize};
+        height: ${toggleSize};
+        border-radius: ${toggleSize};
+        transition: 0.3s;
+      }
 
-    &:focus {
-      border-color: ${th('colors.primary.500')};
-    }
+      &:not(:disabled) {
+        &:active::after {
+          width: ${disabled ? null : '55%'};
+        }
+      }
 
-    ${checked && !disabled && checkedStyles}
-    ${checked && disabled && checkedDisabledStyles}
-    ${connected ? componentSystem : system};
-  `
+      &:focus {
+        border-color: ${th('colors.primary.500')};
+      }
+
+      ${checked && !disabled && checkedStyles};
+      ${checked && disabled && checkedDisabledStyles};
+      ${connected ? componentSystem : system};
+    `
   }
 )
