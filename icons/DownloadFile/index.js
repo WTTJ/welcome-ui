b026b29d71e253e134899b1afea9d976e5e08f25
theojungle <mesniltheo@gmail.com>
2020-02-12 13:47:50 +0100

    fix: wrong icon name invalid on npm

--- icons/DownloadFile/index.js
+++ icons/DownloadFile/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { Icon } from '@welcome-ui/icon'
 
 import content from './content.js'
-export const UploadIcon = props => <Icon alt="Upload" content={content} {...props} />
+export const DownloadFileIcon = props => <Icon alt="DownloadFile" content={content} {...props} />
--- icons/UploadFile/index.js
+++ icons/UploadFile/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { Icon } from '@welcome-ui/icon'
 
 import content from './content.js'
-export const DownloadIcon = props => <Icon alt="Download" content={content} {...props} />
+export const UploadFileIcon = props => <Icon alt="UploadFile" content={content} {...props} />
