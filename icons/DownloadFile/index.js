d8b334bfab78ba5a6d61133fcf0bafc5f331812f
theojungle <mesniltheo@gmail.com>
2020-02-12 14:28:11 +0100

    fix: remove for now Download icon

--- icons/DownloadFile/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Icon } from '@welcome-ui/icon'
-
-import content from './content.js'
-export const DownloadFileIcon = props => <Icon alt="DownloadFile" content={content} {...props} />
