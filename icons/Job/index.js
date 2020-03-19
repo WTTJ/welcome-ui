31f3bf0b3bee9914d076016f37a5aa432b142c87
Robert Douglas <robert@wttj.co>
2020-03-19 13:50:26 +0000

    chore: resize new icons

--- icons/Job/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Icon } from '@welcome-ui/icon'
-
-import content from './content.js'
-export const JobIcon = props => <Icon alt="Job" content={content} {...props} />
--- icons/Notice/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Icon } from '@welcome-ui/icon'
-
-import content from './content.js'
-export const NoticeIcon = props => <Icon alt="Notice" content={content} {...props} />
