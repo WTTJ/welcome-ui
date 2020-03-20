24bc9ecc05c82d100ac61e4ad69f832447e1b55e
Robert Douglas <robert@wttj.co>
2020-03-20 13:23:42 +0000

    chore: update french flag

--- icons/FlagFr/content.js
+++ icons/FlagFr/content.js
@@ -2,5 +2,5 @@ export default {
   width: 15,
   height: 15,
   block:
-    '<defs><mask id="fr-a" x="0" y="0" width="14" height="10"><rect width="14" height="10" rx="2" fill="#FFF"/></mask></defs><g mask="url(#fr-a)" transform="scale(7.1420 7.51806)"><path fill="#1035BB" d="M0 0h4.67v10H0z"/><path fill="#FFF" d="M4.67 0h4.67v10H4.67z"/><path fill="#F44653" d="M9.33 0H14v10H9.33z"/></g>'
+    '<defs><mask id="fr-a" x="0" y="0" width="14" height="10"><rect width="14" height="10" rx="2" fill="#FFF"/></mask></defs><g mask="url(#fr-a)" transform="scale(7.142 7.51806)"><path fill="#1035BB" d="M0 0h4.67v10H0z"/><path fill="#FFF" d="M4.67 0h4.67v10H4.67z"/><path fill="#F44653" d="M9.33 0H14v10H9.33z"/></g>'
 }
