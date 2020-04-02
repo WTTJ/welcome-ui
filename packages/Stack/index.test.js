6d88814469bb87aa6a4c062a6afac2344dce9852
François Robichet <Calvein@users.noreply.github.com>
2020-04-02 11:30:04 +0200

    refactor(box): update to use xstyled's Box component directly
    * refactor(box): update to use xstyled's Box component directly
    
    * test: tweak tests using Box component

--- packages/Stack/index.test.js
+++ packages/Stack/index.test.js
@@ -12,8 +12,8 @@ describe('<Stack>', () => {
     const foo = getByText('Foo')
     const bar = getByText('Bar')
 
-    expect(foo.parentElement).toHaveStyleRule('margin-bottom', '0.75rem')
-    expect(bar.parentElement).not.toHaveStyleRule()
+    expect(getComputedStyle(foo.parentElement).marginBottom).toBe('0.75rem')
+    expect(getComputedStyle(bar.parentElement).marginBottom).toBe('')
   })
 
   it('should render correctly with custom props', () => {
@@ -24,7 +24,7 @@ describe('<Stack>', () => {
     )
     const foo = getByText('Foo')
 
-    expect(foo.parentElement).toHaveStyleRule('margin-right', '1.5rem')
+    expect(getComputedStyle(foo.parentElement).marginRight).toBe('1.5rem')
     expect(foo.parentElement.tagName).toBe('LI')
   })
 })
