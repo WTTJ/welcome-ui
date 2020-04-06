0a27af41e4f900ac17f5e9efc66543e42c898467
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-04-06 12:39:35 +0200

    refactor(themes): remove themes from Core package (#578)

--- packages/Core/index.js
+++ packages/Core/index.js
@@ -1,7 +1,5 @@
 // Themes
 export { createTheme } from './theme/core'
-export { welcomekitTheme } from './theme/welcomekit'
-export { welcomeTheme } from './theme/welcome'
 
 // Base
 export { GlobalStyle } from './utils/base'
