a052260b87f1fe7919ef224627a7ee9eb8549509
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2020-04-14 14:54:53 +0200

    fix: default color for loader

--- packages/Loader/index.js
+++ packages/Loader/index.js
@@ -4,13 +4,15 @@ import { Box } from '@welcome-ui/box'
 
 import * as S from './styles'
 
-export const Loader = forwardRef(({ color, dataTestId, size = 'sm', ...rest }, ref) => (
-  <Box data-testid={dataTestId} display="flex" ref={ref}>
-    <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
-    <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
-    <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
-  </Box>
-))
+export const Loader = forwardRef(
+  ({ color = 'currentColor', dataTestId, size = 'sm', ...rest }, ref) => (
+    <Box data-testid={dataTestId} display="flex" ref={ref}>
+      <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
+      <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
+      <S.LoadingDot backgroundColor={color} shape="circle" size={size} {...rest} />
+    </Box>
+  )
+)
 
 Loader.displayName = 'Loader'
 
