6cbc8a74d7745799d57d87073c046e38a8d09276
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-04-15 11:27:01 +0200

    fix(breadcrumb): line height (#601)

--- packages/Breadcrumb/styles.js
+++ packages/Breadcrumb/styles.js
@@ -3,11 +3,14 @@ import { Box } from '@welcome-ui/box'
 
 export const Breadcrumb = styled(Box)`
   ${th('breadcrumbs.list')};
+  height: 100%;
 `
 
 export const List = styled.ol`
   display: flex;
+  align-items: center;
   max-width: 100%;
+  height: 100%;
   overflow-x: auto;
   margin: 0;
   padding: 0;
