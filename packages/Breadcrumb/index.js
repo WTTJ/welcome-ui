c8eec7b4bd9205c91e8247482123c80f772debc3
theojungle <mesniltheo@gmail.com>
2020-04-20 12:16:58 +0200

    feat(breadcrumb): add gradient on mobile

--- packages/Breadcrumb/index.js
+++ packages/Breadcrumb/index.js
@@ -1,4 +1,4 @@
-import React, { cloneElement, forwardRef } from 'react'
+import React, { cloneElement, forwardRef, useEffect, useRef, useState } from 'react'
 import { RightIcon } from '@welcome-ui/icons.right'
 import { node, oneOfType, string } from 'prop-types'
 
@@ -8,7 +8,21 @@ import { Item } from './Item'
 import * as S from './styles'
 
 export const Breadcrumb = forwardRef(
-  ({ renderChildrenAs = 'a', children, separator = <RightIcon size="sm" />, ...rest }, ref) => {
+  (
+    {
+      renderChildrenAs = 'a',
+      children,
+      separator = <RightIcon size="sm" />,
+      gradientBackground = 'light.900',
+      ...rest
+    },
+    ref
+  ) => {
+    const wrapperRef = ref || useRef(null)
+    const [listRef, setListRef] = useState(useRef(null))
+    const [scrollLeft, setScrollLeft] = useState()
+    const [scrollRight, setScrollRight] = useState()
+
     const clones = children.map((child, index) => {
       const isLastChild = children.length === index + 1
 
@@ -20,9 +34,44 @@ export const Breadcrumb = forwardRef(
       })
     })
 
+    function onListScroll() {
+      setScrollLeft(listRef.current.scrollLeft)
+      setScrollRight(
+        listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.clientWidth)
+      )
+    }
+
+    useEffect(() => {
+      setListRef(listRef)
+
+      const scrollToLeft =
+        listRef.current.scrollWidth === listRef.current.offsetWidth
+          ? undefined
+          : listRef.current.scrollWidth
+
+      setTimeout(() => {
+        listRef.current.scrollLeft = scrollToLeft
+      }, 500)
+      setScrollLeft(scrollToLeft)
+      setScrollRight(
+        listRef.current.scrollWidth - (listRef.current.scrollLeft + listRef.current.clientWidth)
+      )
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-      <S.Breadcrumb as="nav" ref={ref} {...rest}>
-        <S.List>{clones}</S.List>
+      <S.Breadcrumb as="nav" ref={wrapperRef} {...rest}>
+        <S.GradientLeft
+          gradientBackground={gradientBackground}
+          show={scrollLeft && scrollLeft > 0}
+        />
+        <S.List onScroll={onListScroll} ref={listRef}>
+          {clones}
+        </S.List>
+        <S.GradientRight
+          gradientBackground={gradientBackground}
+          show={scrollRight && scrollRight > 0}
+        />
       </S.Breadcrumb>
     )
   }
@@ -30,8 +79,10 @@ export const Breadcrumb = forwardRef(
 
 Breadcrumb.displayName = 'Breadcrumb'
 
-Breadcrumb.propTypes = {
+Breadcrumb.propTypes = /* remove-proptypes */ {
   children: node.isRequired,
+  /** color from theme, add for scroll gradient on mobile */
+  gradientBackground: string,
   renderChildrenAs: oneOfType(COMPONENT_TYPE),
   separator: oneOfType([node, string])
 }
