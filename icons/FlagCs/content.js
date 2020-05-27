ad4ff064028c0838ca6b91cda73a124b550e31cd
Robert Douglas <robert@wttj.co>
2020-05-27 15:35:55 +0000

    chore: remove flags from icon font docs

--- icons/FlagCs/content.js
+++ icons/FlagCs/content.js
@@ -2,5 +2,5 @@ export default {
   width: 15,
   height: 15,
   block:
-    '<defs><path id="cz-a" d="M0 0h100v71.429H0z"/></defs><g fill="none" fill-rule="evenodd" transform="translate(0 14)"><mask id="cz-b" fill="#fff"><use xlink:href="#cz-a"/></mask><path stroke="#F5F5F5" stroke-width="1.786" d="M.893.893h98.214v69.643H.893z"/><path fill="#E8252A" mask="url(#cz-b)" d="M0 33.333h100v38.095H0z"/><path fill="#17579E" mask="url(#cz-b)" d="M0 0l47.619 35.714L0 71.429z"/></g>'
+    '<defs><path id="cz-a" d="M0 0h100v71.429H0z"/></defs><g fill="#fff" fill-rule="evenodd" transform="translate(0 14)"><mask id="cz-b" fill="#fff"><use xlink:href="#cz-a"/></mask><path stroke="#F5F5F5" stroke-width="1.786" d="M.893.893h98.214v69.643H.893z"/><path fill="#E8252A" mask="url(#cz-b)" d="M0 33.333h100v38.095H0z"/><path fill="#17579E" mask="url(#cz-b)" d="M0 0l47.619 35.714L0 71.429z"/></g>'
 }
--- icons/FlagFr/content.js
+++ icons/FlagFr/content.js
@@ -2,5 +2,5 @@ export default {
   width: 15,
   height: 15,
   block:
-    '<defs><path id="fr-a" d="M0 0h100v71.429H0z"/></defs><g fill="none" fill-rule="evenodd" transform="translate(0 14)"><mask id="fr-b" fill="#fff"><use xlink:href="#fr-a"/></mask><path stroke="#F5F5F5" stroke-width="1.786" d="M.893.893h98.214v69.643H.893z"/><path fill="#F44653" mask="url(#fr-b)" d="M66.667 0H100v71.429H66.667z"/><path fill="#1035BB" mask="url(#fr-b)" d="M0 0h33.333v71.429H0z"/></g>'
+    '<defs><path id="fr-a" d="M0 0h100v71.429H0z"/></defs><g fill="#fff" fill-rule="evenodd" transform="translate(0 14)"><mask id="fr-b" fill="#fff"><use xlink:href="#fr-a"/></mask><path stroke="#F5F5F5" stroke-width="1.786" d="M.893.893h98.214v69.643H.893z"/><path fill="#F44653" mask="url(#fr-b)" d="M66.667 0H100v71.429H66.667z"/><path fill="#1035BB" mask="url(#fr-b)" d="M0 0h33.333v71.429H0z"/></g>'
 }
