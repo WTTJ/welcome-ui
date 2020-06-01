54bb652887d40310f477b1c915ea588f667b7214
Mickaël Le Ralec <mickael.leralec@gmail.com>
2020-06-01 18:41:52 +0200

    fix: missing disabled prop on DateTimePicker

--- packages/DateTimePicker/index.js
+++ packages/DateTimePicker/index.js
@@ -1,5 +1,5 @@
 import React, { cloneElement, forwardRef, useEffect, useState } from 'react'
-import { func, node, number, object, oneOf, oneOfType, string } from 'prop-types'
+import { bool, func, node, number, object, oneOf, oneOfType, string } from 'prop-types'
 import { DatePicker } from '@welcome-ui/date-picker'
 import { TimePicker } from '@welcome-ui/time-picker'
 import { DEFAULT_DATE, getDate } from '@welcome-ui/date-time-picker-common'
@@ -9,7 +9,10 @@ import { SIZES_TYPE } from '../../src/utils/propTypes'
 import * as S from './styles'
 
 export const DateTimePicker = forwardRef(
-  ({ children, dataTestId, locale, onChange, size = 'lg', value = DEFAULT_DATE }, ref) => {
+  (
+    { children, dataTestId, disabled = false, locale, onChange, size = 'lg', value = DEFAULT_DATE },
+    ref
+  ) => {
     const formatDate = date => getDate(date, 15)
 
     const [date, setDate] = useState(formatDate(value))
@@ -51,13 +54,20 @@ export const DateTimePicker = forwardRef(
         {!children && (
           <>
             <DatePicker
+              disabled={disabled}
               inputRef={ref}
               locale={locale}
               onChange={handleChange}
               size={size}
               value={value}
             />
-            <TimePicker locale={locale} onChange={handleChange} size={size} value={value} />
+            <TimePicker
+              disabled={disabled}
+              locale={locale}
+              onChange={handleChange}
+              size={size}
+              value={value}
+            />
           </>
         )}
       </S.DateTimePicker>
@@ -69,6 +79,7 @@ DateTimePicker.displayName = 'DateTimePicker'
 
 DateTimePicker.propTypes /* remove-proptypes */ = {
   children: node,
+  disabled: bool,
   locale: object,
   onChange: func,
   size: oneOf(SIZES_TYPE),
