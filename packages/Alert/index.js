27be68ff4d2314d728a6637dc67a72bd907cfd86
François Robichet <calvein@gmail.com>
2020-06-08 10:29:05 +0200

    feat: redesign alerts

--- packages/Alert/index.js
+++ packages/Alert/index.js
@@ -1,28 +1,70 @@
 /* eslint-disable react/no-multi-comp */
 import { node, oneOf } from 'prop-types'
-import React from 'react'
+import React, { Children } from 'react'
+import { Stack } from '@welcome-ui/stack'
+import { Button } from '@welcome-ui/button'
+import { CheckIcon } from '@welcome-ui/icons.check'
+import { InformationIcon } from '@welcome-ui/icons.information'
+import { AlertIcon } from '@welcome-ui/icons.alert'
 
 import * as S from './styles'
 
-const Alert = ({ children, variant = 'error', ...rest }) => (
-  <S.Alert variant={variant} {...rest}>
-    {children}
-  </S.Alert>
-)
+const Alert = ({ children, variant = 'error', ...rest }) => {
+  const hasTitle = Children.toArray(children).some(child => child.type === AlertTitle)
+  const buttonChild = Children.toArray(children).find(child => child.type === AlertButton)
+  const content = Children.toArray(children)
+    .filter(child => child.type !== AlertButton)
+    .map(child => {
+      // Add variant to AlertTitle to show the correct icon
+      if (child.type === AlertTitle) {
+        return React.cloneElement(child, { variant })
+      }
+      return child
+    })
+
+  // Reduce the vertical padding only when we have Title & Button
+  const py = hasTitle && buttonChild && content.length === 1 ? 'sm' : 'xl'
+
+  return (
+    <S.Alert py={py} variant={variant} {...rest}>
+      {buttonChild ? (
+        <Stack alignItems="center" direction="row" justifyContent="space-between">
+          <div>{content}</div>
+          {buttonChild}
+        </Stack>
+      ) : (
+        content
+      )}
+    </S.Alert>
+  )
+}
 
 Alert.propTypes /* remove-proptypes */ = {
   children: node.isRequired,
   variant: oneOf(['success', 'error', 'warning', 'info'])
 }
 
-const AlertTitle = ({ children, dataTestId }) => (
-  <S.Title data-testid={dataTestId}>{children}</S.Title>
+const AlertTitle = ({ children, variant, ...rest }) => (
+  <S.Title variant={variant} {...rest}>
+    <S.Icon>
+      {variant === 'success' && <CheckIcon />}
+      {variant === 'info' && <InformationIcon />}
+      {(variant === 'error' || variant === 'warning') && <AlertIcon />}
+    </S.Icon>
+    {children}
+  </S.Title>
 )
 
 AlertTitle.propTypes /* remove-proptypes */ = {
-  children: node.isRequired
+  children: node.isRequired,
+  variant: oneOf(['success', 'error', 'warning', 'info'])
 }
 
 Alert.Title = AlertTitle
 
-export { Alert, AlertTitle }
+// We need this component to check its existence in <Alert> and to allow uses to add Button in <Alert> content
+const AlertButton = props => <Button size="sm" {...props} />
+
+Alert.Button = AlertButton
+
+export { Alert, AlertTitle, AlertButton }
