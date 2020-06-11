6d5d49e80ed12f3a7199e07aea11dd6f606d4ecd
Paul-Xavier Ceccaldi <pix@wttj.co>
2020-06-11 13:35:54 +0000

    fix: link issue when children is an object
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Guillaume Rousseau <guillaume.rousseau@wttj.co>

--- packages/Link/index.js
+++ packages/Link/index.js
@@ -1,14 +1,15 @@
 /* eslint-disable react/no-multi-comp */
-import React, { cloneElement } from 'react'
+import React, { Children, cloneElement, forwardRef, useEffect, useRef, useState } from 'react'
 import { node, oneOf } from 'prop-types'
 
 import * as S from './styles'
 
 const isString = value => typeof value === 'string'
 
-export const Link = ({ children, variant = 'primary', ...props }) => {
+export const Link = forwardRef(({ children, variant = 'primary', ...props }, ref) => {
   let clones
-  const isChildrenString = isString(children)
+  const linkRef = useRef()
+  const [isChildrenString, setIsChildrenString] = useState(isString(children))
 
   const WrapWithText = ({ children, ...props }) => (
     <span className="wui-text" {...props}>
@@ -17,7 +18,7 @@ export const Link = ({ children, variant = 'primary', ...props }) => {
   )
 
   if (!isChildrenString) {
-    clones = children.map((child, index) => {
+    clones = Children.map(children, (child, index) => {
       const key = `link-child-${index}`
       if (isString(child)) {
         return <WrapWithText key={key}>{child}</WrapWithText>
@@ -26,13 +27,22 @@ export const Link = ({ children, variant = 'primary', ...props }) => {
     })
   }
 
+  useEffect(() => {
+    const innerRef = ref || linkRef
+    if (innerRef) {
+      setIsChildrenString(innerRef.current.childElementCount === 0)
+    }
+  }, [linkRef, ref])
+
   return (
-    <S.Link variant={variant} {...props}>
+    <S.Link ref={ref || linkRef} variant={variant} {...props}>
       {isChildrenString && <WrapWithText>{children}</WrapWithText>}
       {clones}
     </S.Link>
   )
-}
+})
+
+Link.displayName = 'Link'
 
 Link.propTypes = {
   children: node.isRequired,
