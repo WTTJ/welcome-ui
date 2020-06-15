f7d2395f4fd90c538a50c26d7e086f6c1bdff82d
theojungle <mesniltheo@gmail.com>
2020-06-15 14:04:35 +0200

    feat(icons): a 4 new icons

---
+++ icons/Factory/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.js'
+export const FactoryIcon = props => (
+  <Icon alt="Factory" content={content} {...props} />
+)
