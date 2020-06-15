3eb2eaa9a4d8854ec6406ecd3e81966daae856cc
theojungle <mesniltheo@gmail.com>
2020-06-15 16:20:50 +0200

    fix(datepicker): today color selected

--- packages/DateTimePickerCommon/theme.js
+++ packages/DateTimePickerCommon/theme.js
@@ -1,12 +1,18 @@
 export const getDateTimePickerCommon = theme => {
-  const { colors, fontWeights } = theme
+  const { borderWidths, colors, fontWeights } = theme
 
   return {
     item: {
       selected: {
         color: colors.light[900],
         fontWeight: fontWeights.bold,
-        backgroundColor: colors.primary[500]
+        backgroundColor: colors.primary[500],
+        borderColor: colors.primary[500]
+      },
+      today: {
+        borderWidth: borderWidths.sm,
+        borderColor: colors.dark[900],
+        borderStyle: 'solid'
       }
     }
   }
