41ed6a21907eac7bc35f5ca88887656159f638c3
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-06-22 14:10:17 +0200

    fix: revert #707 with new refacto and fix docz build (#710)

--- packages/Field/layout.js
+++ packages/Field/layout.js
@@ -1,32 +1,30 @@
 import styled, { css } from '@xstyled/styled-components'
 import React from 'react'
 
+const getFlexStyles = (align, justify, direction) => css`
+  display: flex;
+  align-items: ${align || direction === 'column' ? 'flex-start' : 'center'};
+  justify-content: ${justify || 'flex-start'};
+`
+
 export const FlexContainer = styled.div(
   ({ align, direction, flex, justify }) => css`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    flex: ${flex};
-    flex-direction: ${direction};
-
-    ${direction === 'column'} {
-      align-items: flex-start;
-    }
-
-    ${align} {
-      align-items: ${align};
-    }
-
-    ${justify} {
-      justify-content: ${justify};
-    }
+    flex-direction: ${direction || null};
+    flex: ${flex || null};
+    ${getFlexStyles(align, justify, direction)};
   `
 )
 
-export const RowContainer = styled(({ direction, flex, ...rest }) => (
+export const RowContainer = styled(({ direction, ...rest }) => (
   <FlexContainer {...rest} direction="row" />
-))
+))`
+  flex: ${props => props.flex || null};
+  ${({ align, justify }) => getFlexStyles(align, justify)};
+`
 
 export const StackContainer = styled(({ direction, ...rest }) => (
   <FlexContainer {...rest} direction="column" />
-))
+))`
+  flex: ${props => props.flex || null};
+  ${({ align, justify }) => getFlexStyles(align, justify)};
+`
