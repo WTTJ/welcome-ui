7c9337a022df00b3b747ab77878a95fb0c58185d
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-06-22 16:49:56 +0200

    feat(markdown): add possibility to custom toolbar item title (#712)

--- packages/MarkdownEditor/constants.js
+++ packages/MarkdownEditor/constants.js
@@ -1,17 +1,17 @@
 export const DEFAULT_TOOLBAR = [
-  { name: 'bold' },
-  { name: 'italic' },
-  { name: 'strikethrough' },
-  { name: 'link' },
+  { name: 'bold', title: 'Bold' },
+  { name: 'italic', title: 'Italic' },
+  { name: 'strikethrough', title: 'Strikethrough' },
+  { name: 'link', title: 'Link' },
   { name: 'divider' },
-  { name: 'heading_1' },
-  { name: 'heading_2' },
+  { name: 'heading_1', title: 'Heading 1' },
+  { name: 'heading_2', title: 'Heading 2' },
   { name: 'divider' },
-  { name: 'unordered_list' },
-  { name: 'ordered_list' },
+  { name: 'unordered_list', title: 'Unordered list' },
+  { name: 'ordered_list', title: 'Ordered list' },
   { name: 'divider' },
-  { name: 'code' },
-  { name: 'quote' },
+  { name: 'code', title: 'Code' },
+  { name: 'quote', title: 'Quote' },
   { name: 'divider' },
-  { name: 'emoji', icon: '🙂' }
+  { name: 'emoji', icon: '🙂', title: 'Emoji' }
 ]
