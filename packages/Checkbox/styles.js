968a3bcd17b2d2ca405a0fdc5b20c72bc1410a6e
theojungle <mesniltheo@gmail.com>
2020-06-30 14:11:32 +0200

    feat: new check icon on Radio and add special style on Radio with hint

--- packages/Checkbox/styles.js
+++ packages/Checkbox/styles.js
@@ -16,13 +16,13 @@ export const Checkbox = styled(filterFieldComponent(ReakitCheckbox))(
 
     &[aria-checked='true'] {
       &::after {
-        content: url(${`'data:image/svg+xml; utf8, <svg viewBox="0 0 10 8" xmlns="http://www.w3.org/2000/svg"><path d="M8.48.751a.914.914 0 0 1 1.26 0 .837.837 0 0 1 0 1.215L4.26 7.249a.913.913 0 0 1-1.319-.064L.201 3.96A.838.838 0 0 1 .326 2.75a.913.913 0 0 1 1.253.12l2.117 2.493L8.48.75z" fill="${theme.defaultFields.checkableField.checked.color}" fill-rule="evenodd"/></svg>'`});
+        content: url(${`'data:image/svg+xml; utf8, <svg viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg"><path d="M10.113 1.273a1.085 1.085 0 011.526-.082c.433.386.481 1.041.118 1.485l-.035.04-7.245 8.01a1.083 1.083 0 01-1.474.126l-.047-.039-2.59-2.277A1.076 1.076 0 01.274 7.01a1.085 1.085 0 011.483-.126l.042.035 1.786 1.57 6.528-7.216z" fill="${theme.defaultFields.checkableField.checked.color}" /></svg>'`});
         position: absolute;
-        top: 0;
+        top: 2px;
         right: 0;
         bottom: 0;
         left: 0;
-        width: 10;
+        width: 12px;
         margin: auto;
         text-align: center;
         transition: medium;
--- packages/Radio/theme.js
+++ packages/Radio/theme.js
@@ -1,14 +1,24 @@
 export const getRadios = theme => {
-  const { colors, toRem } = theme
+  const { colors, fontSizes, space, toRem } = theme
 
   return {
     default: {
-      width: toRem(18),
-      height: toRem(18)
+      width: toRem(20),
+      height: toRem(20)
     },
     checked: {
       backgroundColor: colors.primary[500],
       borderColor: colors.primary[500]
+    },
+    withHint: {
+      default: {
+        fontSize: fontSizes.body2,
+        color: colors.dark[900]
+      },
+      hint: {
+        marginTop: space.xs,
+        color: colors.dark[200]
+      }
     }
   }
 }
