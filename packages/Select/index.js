1346fb74006ff086ff2ff6582c7d27b7d94dfa88
Paul-Xavier Ceccaldi <pix@wttj.co>
2020-07-01 15:11:58 +0000

    fix: expose renderCreateItem on isCreatable Select
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- packages/Select/index.js
+++ packages/Select/index.js
@@ -50,6 +50,7 @@ export const Select = forwardRef(
       onFocus,
       onKeyDown,
       placeholder = 'Choose from…',
+      renderCreateItem = inputValue => `Create "${inputValue}"`,
       renderItem = itemToString,
       renderMultiple = MultipleSelections,
       size = 'lg',
@@ -300,7 +301,7 @@ export const Select = forwardRef(
                         }
                       })}
                     >
-                      {`Create "${inputValue}"`}
+                      {renderCreateItem(inputValue)}
                     </S.Item>
                   )}
                 </S.Menu>
@@ -343,6 +344,7 @@ Select.propTypes /* remove-proptypes */ = {
   }] */
   options: arrayOf(OPTIONS_TYPE),
   placeholder: string,
+  renderCreateItem: func,
   renderItem: func,
   renderMultiple: func,
   size: oneOf(SIZES_TYPE),
