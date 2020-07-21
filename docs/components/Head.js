8ed0bc186e85bfab66c4246e6644a6cd7c214b0c
theojungle <mesniltheo@gmail.com>
2020-07-21 10:32:11 +0200

    docs: add google analytics

--- docs/components/Head.js
+++ docs/components/Head.js
@@ -42,6 +42,19 @@ export const Head = () => {
         content="https://cdn.welcometothejungle.co/images/welcome-ui-og-image-2.png"
         property="og:image"
       />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56009608-24" />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-56009608-24');
+          `
+        }}
+      />
     </NextHead>
   )
 }
