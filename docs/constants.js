6ee286547b760d1dc455087ac913d6f00c8b0310
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2020-08-21 10:59:07 +0200

    feat: optgroups on search & select component

--- docs/constants.js
+++ docs/constants.js
@@ -23,3 +23,26 @@ export const ITEMS = [
   { value: 'xing', label: 'Xing', icon: XingIcon },
   { value: 'youtube', label: 'Youtube', icon: YoutubeIcon }
 ]
+
+export const OPT_GROUP_ITEMS = [
+  {
+    label: 'Professional networks',
+    options: [
+      { value: 'behance', label: 'Behance', icon: BehanceIcon },
+      { value: 'dribbble', label: 'Dribbble', icon: DribbbleIcon },
+      { value: 'github', label: 'Github', icon: GithubIcon },
+      { value: 'linkedin', label: 'Linkedin', icon: LinkedinIcon },
+      { value: 'stackoverflow', label: 'Stack Overflow', icon: StackoverflowIcon },
+      { value: 'xing', label: 'Xing', icon: XingIcon },
+    ],
+  },
+  {
+    label: 'Personal networks',
+    options: [
+      { value: 'instagram', label: 'Instagram', icon: InstagramIcon },
+      { value: 'facebook', label: 'Facebook', icon: FacebookIcon },
+      { value: 'twitter', label: 'Twitter', icon: TwitterIcon },
+      { value: 'youtube', label: 'Youtube', icon: YoutubeIcon }
+    ]
+  }
+]
