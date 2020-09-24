577f6626c6546b4fdf8b207b55a06f9f5e91a4da
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-09-24 10:27:55 +0200

    feat(icons): add ppt icon (#782)

---
+++ icons/Ppt/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const PptIcon = props => <Icon alt="Ppt" content={content} {...props} />
