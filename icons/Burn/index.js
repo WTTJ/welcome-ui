9dd207e96c7d620d0826584e251bdf8fd4434522
guillaumewttj <61152048+guillaumewttj@users.noreply.github.com>
2020-10-12 09:32:42 +0200

    feat: burn icon (#786)
    * fix: conflicts & icon optimize option
    
    * fix: burn icon doc

---
+++ icons/Burn/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const BurnIcon = props => <Icon alt="Burn" content={content} {...props} />
