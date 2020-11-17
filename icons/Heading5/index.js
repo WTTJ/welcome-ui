9a619ce2f76b02858e9d267ad58b57a11900fb22
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2020-11-17 17:59:31 +0100

    feat(icons): add new icons for markdown (#806)

---
+++ icons/Heading5/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const Heading5Icon = props => <Icon alt="Heading5" content={content} {...props} />
