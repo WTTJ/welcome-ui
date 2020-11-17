4929dbc0926e153949ee5306b276a8f1a8b5bbbd
Mesnil Theo <mesniltheo@gmail.com>
2020-11-17 17:50:18 +0100

    feat(icons): add new icons for markdown

---
+++ icons/Heading6/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const Heading6Icon = props => <Icon alt="Heading6" content={content} {...props} />
---
+++ icons/Underline/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const UnderlineIcon = props => <Icon alt="Underline" content={content} {...props} />
