0b430adf09d4ff0f16c1edea28bcd629cf450e4c
François Robichet <calvein@gmail.com>
2021-03-03 11:21:56 +0100

    feat: add flag_fill icon

---
+++ icons/FlagFill/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const FlagFillIcon = props => <Icon alt="FlagFill" content={content} {...props} />
