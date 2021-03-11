b4f0dab7d9e75a56f82970de53a9ef47f9b74898
Robert Douglas <rob@anucreative.com>
2021-03-11 09:09:38 +0100

    fix: prevent next from deploying font

--- scripts/core.sh
+++ scripts/core.sh
@@ -4,7 +4,12 @@ FONTS_DIR=packages/IconFont/fonts
 FONT_FILE="$FONTS_DIR/welcome-icon-font.woff"
 
 if [[ -f "$FONT_FILE" ]]; then
-  BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+  # We get the branch name from HEAD with netlify
+  if [[ -n "$HEAD" ]]; then
+    BRANCH=$HEAD
+  else
+    BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+  fi
   ICON_FONT_HASH=$(sha1sum $FONT_FILE | awk '{ print $1 }')
 
   lerna run build --scope @welcome-ui/core -- --environment BRANCH:$BRANCH,ICON_FONT_HASH:$ICON_FONT_HASH
