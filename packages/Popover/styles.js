fbac3d2f3bdcb813c9d77c804f1d78e6cd83a79f
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2021-04-06 09:19:16 +0200

    chore: use svgBox instead of adding system to svg

--- packages/Popover/styles.js
+++ packages/Popover/styles.js
@@ -7,10 +7,9 @@ export const Arrow = styled(PopoverArrow)`
   color: ${th('popovers.default.backgroundColor')};
 `
 
-export const ArrowItem = styled.svg(
+export const ArrowItem = styled.svgBox(
   ({ $transform }) => css`
     transform: ${$transform};
-    ${system}
   `
 )
 
