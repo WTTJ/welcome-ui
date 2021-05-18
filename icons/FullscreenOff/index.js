b30da2a9cdf6d6aa0aaf6e5fd120f67a098bffd9
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-05-18 13:54:05 +0200

    feat: add fullscreen_off and fullscreen_on icons (#850)
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

---
+++ icons/FullscreenOff/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const FullscreenOffIcon = props => <Icon alt="FullscreenOff" content={content} {...props} />
