e6206d429a34b48b302e9ce09c2edf3d23d7b195
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-06-10 08:18:40 +0000

    chore: upgrade @xstyled/system to v3.0.1
    @xstyled/system
      * @welcome-ui/accordion: ^2.2.3 → ^3.0.1
      * @welcome-ui/alert: ^2.2.3 → ^3.0.1
      * @welcome-ui/avatar: ^2.2.3 → ^3.0.1
      * @welcome-ui/breadcrumb: ^2.2.3 → ^3.0.1
      * @welcome-ui/button: ^2.2.3 → ^3.0.1
      * @welcome-ui/card: ^2.2.3 → ^3.0.1
      * @welcome-ui/checkbox: ^2.2.3 → ^3.0.1
      * @welcome-ui/clear-button: ^2.2.3 → ^3.0.1
      * @welcome-ui/core: ^2.2.3 → ^3.0.1
      * @welcome-ui/date-time-picker: ^2.2.3 → ^3.0.1
      * @welcome-ui/date-time-picker-common: ^2.2.3 → ^3.0.1
      * @welcome-ui/drawer: ^2.2.3 → ^3.0.1
      * @welcome-ui/emoji-picker: ^2.2.3 → ^3.0.1
      * @welcome-ui/field: ^2.2.3 → ^3.0.1
      * @welcome-ui/field-group: ^2.2.3 → ^3.0.1
      * @welcome-ui/file-drop: ^2.2.3 → ^3.0.1
      * @welcome-ui/hint: ^2.2.3 → ^3.0.1
      * @welcome-ui/icon: ^2.2.3 → ^3.0.1
      * @welcome-ui/icons.font: ^2.2.3 → ^3.0.1
      * @welcome-ui/input-text: ^2.2.3 → ^3.0.1
      * @welcome-ui/label: ^2.2.3 → ^3.0.1
      * @welcome-ui/link: ^2.2.3 → ^3.0.1
      * @welcome-ui/loader: ^2.2.3 → ^3.0.1
      * @welcome-ui/markdown-editor: ^2.2.3 → ^3.0.1
      * @welcome-ui/modal: ^2.2.3 → ^3.0.1
      * @welcome-ui/pagination: ^2.2.3 → ^3.0.1
      * @welcome-ui/picker: ^2.2.3 → ^3.0.1
      * @welcome-ui/popover: ^2.2.3 → ^3.0.1
      * @welcome-ui/radio: ^2.2.3 → ^3.0.1
      * @welcome-ui/search: ^2.2.3 → ^3.0.1
      * @welcome-ui/select: ^2.2.3 → ^3.0.1
      * @welcome-ui/stack: ^2.2.3 → ^3.0.1
      * @welcome-ui/swiper: ^2.2.3 → ^3.0.1
      * @welcome-ui/system: ^2.2.3 → ^3.0.1
      * @welcome-ui/table: ^2.2.3 → ^3.0.1
      * @welcome-ui/tag: ^2.2.3 → ^3.0.1
      * @welcome-ui/text: ^2.2.3 → ^3.0.1
      * @welcome-ui/textarea: ^2.2.3 → ^3.0.1
      * @welcome-ui/toast: ^2.2.3 → ^3.0.1
      * @welcome-ui/toggle: ^2.2.3 → ^3.0.1
      * @welcome-ui/tooltip: ^2.2.3 → ^3.0.1
      * @welcome-ui/utils: ^2.2.3 → ^3.0.1
      * @welcome-ui/variant-icon: ^2.2.3 → ^3.0.1

--- packages/Core/theme/transitions.js
+++ packages/Core/theme/transitions.js
@@ -1,11 +1,11 @@
-export const timingFunctions = {
+export const timingFunction = {
   primary: 'ease',
   secondary: 'linear',
   tertiary: 'cubic-bezier(0.41, 0.094, 0.54, 0.07)'
 }
 
 export const transitions = {
-  slow: `500ms ${timingFunctions.tertiary}`,
-  medium: `300ms ${timingFunctions.primary}`,
-  fast: `100ms ${timingFunctions.secondary}`
+  slow: `500ms ${timingFunction.tertiary}`,
+  medium: `300ms ${timingFunction.primary}`,
+  fast: `100ms ${timingFunction.secondary}`
 }
