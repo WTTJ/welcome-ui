554ec74392620ff2014fda888d3b0cb4f013c70c
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-06-10 13:28:35 +0000

    fix: mjs files
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Guillaume Rousseau <guillaume.rousseau@wttj.co>
    Signed-off-by: Mick Le Ralec<mickael.leralec@wttj.co>

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,13 @@
 const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  webpack(config) {
+    config.module.rules.push({
+      type: 'javascript/auto',
+      test: /\.mjs$/,
+      use: []
+    })
+    return config
+  }
 })
