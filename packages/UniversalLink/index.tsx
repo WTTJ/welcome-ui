4008a3727b4e56b5b55ba50366c4034b8a941494
Mickaël Le Ralec <mickael.leralec@gmail.com>
2021-07-08 14:47:15 +0200

    feat: convert <UniversalLink />

---
+++ packages/UniversalLink/index.tsx
@@ -0,0 +1,27 @@
+import React, { forwardRef } from 'react'
+import { WuiProps } from '@welcome-ui/system'
+
+import * as S from './styles'
+
+export interface UniversalLinkProps extends React.HTMLAttributes<HTMLLinkElement> {
+  dataTestId?: string
+  target?: string
+}
+
+export const UniversalLink = forwardRef<HTMLLinkElement, UniversalLinkProps & WuiProps>(
+  ({ children, dataTestId, target, ...rest }, ref) => (
+    <S.UniversalLink
+      color="inherit"
+      data-testid={dataTestId}
+      ref={ref}
+      // for security
+      rel={target === '_blank' ? 'noopener noreferrer' : undefined}
+      target={target}
+      {...rest}
+    >
+      {children}
+    </S.UniversalLink>
+  )
+)
+
+UniversalLink.displayName = 'UniversalLink'
