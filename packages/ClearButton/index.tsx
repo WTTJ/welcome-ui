2961857a1bf2331362fc4b1bfd8d5cea801312c7
cnairiwttj <camille.nairi@wttj.co>
2021-07-08 17:11:19 +0200

    feat: convert <ClearButton />

--- packages/ClearButton/index.tsx
+++ packages/ClearButton/index.tsx
@@ -1,17 +1,13 @@
-import { oneOf } from 'prop-types'
 import React from 'react'
+import { WuiProps } from '@welcome-ui/system'
 
 import * as S from './styles'
 
-export const ClearButton = props => (
+export const ClearButton: React.FC<WuiProps> = props => (
   <S.ClearButton shape="circle" title="Clear" variant="tertiary" {...props}>
     <S.Icon />
   </S.ClearButton>
 )
-
-ClearButton.propTypes /* remove-proptypes */ = {
-  size: oneOf(['xs', 'sm', 'md', 'lg', 'xl'])
-}
 
 // Nested exports
 export const StyledClearButton = S.ClearButton
