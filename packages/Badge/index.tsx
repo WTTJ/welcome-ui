00c896fdac94a2b6e3454916bd698da5a1015357
Mickaël Le Ralec <mickael.leralec@gmail.com>
2021-07-08 09:43:54 +0200

    fix: badge component

--- packages/Badge/index.tsx
+++ packages/Badge/index.tsx
@@ -1,9 +1,10 @@
 import React, { forwardRef } from 'react'
+import { SystemProps } from '@xstyled/system'
 import { Tag, TagProps } from '@welcome-ui/tag'
 
 export type BadgeProps = Omit<TagProps, 'size'>
 
-export const Badge = forwardRef<HTMLDivElement, BadgeProps>((props, ref) => (
+export const Badge = forwardRef<HTMLDivElement, BadgeProps & SystemProps>((props, ref) => (
   <Tag ref={ref} size="sm" {...props} />
 ))
 
