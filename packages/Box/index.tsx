de2ca9375d7f6bbed7d7392e97107a2ae4234ad4
guillaumewttj <61152048+guillaumewttj@users.noreply.github.com>
2021-07-08 16:02:08 +0200

    feat: stack to typescropt (#860)
    * feat: stack to typescropt
    
    * fix: missing converted files & wrong types

--- packages/Box/index.tsx
+++ packages/Box/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import styled from '@xstyled/styled-components'
 import { shouldForwardProp } from '@welcome-ui/system'
-import { WuiProps } from '@welcome-ui/system'
 
-export const Box: React.FC<WuiProps> = styled.box.withConfig({ shouldForwardProp })``
+export const Box = styled.box.withConfig({ shouldForwardProp })``
