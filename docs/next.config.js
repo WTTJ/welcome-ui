25ebca9bdad0915a29b0af9ab383e49427add2de
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-07-08 10:00:33 +0000

    fix: remove mjs config as we no longer have mjs files
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,13 +1,5 @@
 const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack(config) {
-    config.module.rules.push({
-      type: 'javascript/auto',
-      test: /\.mjs$/,
-      use: []
-    })
-    return config
-  }
+  pageExtensions: ['js', 'jsx', 'md', 'mdx']
 })
