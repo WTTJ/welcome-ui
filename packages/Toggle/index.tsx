28e46fd16496e1837ebc05e03152681d0508b26b
cnairiwttj <camille.nairi@wttj.co>
2021-07-09 11:55:32 +0200

    fix: toggle type

--- packages/Toggle/index.tsx
+++ packages/Toggle/index.tsx
@@ -1,12 +1,9 @@
 import React, { forwardRef } from 'react'
-import { Checkbox } from '@welcome-ui/checkbox'
+import { Checkbox, CheckboxProps } from '@welcome-ui/checkbox'
 
 import * as S from './styles'
 
-export interface ToggleProps extends React.HTMLAttributes<HTMLInputElement> {
-  name: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-}
+export type ToggleProps = Omit<CheckboxProps, 'Component'>
 
 const ToggleComponent = forwardRef<HTMLInputElement, ToggleProps>((props, ref) => (
   <Checkbox {...props} Component={S.Toggle} ref={ref} />
