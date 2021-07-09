1f6e5cd21f77d5ca308e766fe97885478790193e
cnairiwttj <camille.nairi@wttj.co>
2021-07-09 11:49:06 +0200

    fix: clearButton type

--- packages/ClearButton/index.tsx
+++ packages/ClearButton/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { WuiProps } from '@welcome-ui/system'
+import { ButtonProps } from '@welcome-ui/button'
 
 import * as S from './styles'
 
-export const ClearButton: React.FC<WuiProps> = props => (
+export type ClearButtonProps = Omit<ButtonProps, 'shape' | 'title' | 'variant'>
+
+export const ClearButton: React.FC<ButtonProps & WuiProps> = props => (
   <S.ClearButton shape="circle" title="Clear" variant="tertiary" {...props}>
     <S.Icon />
   </S.ClearButton>
