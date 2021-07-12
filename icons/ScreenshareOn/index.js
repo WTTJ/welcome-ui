a4613af972026166bc258e4fcb9898494f0e58e4
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-07-12 16:31:16 +0000

    feat: add new icon set
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

---
+++ icons/ScreenshareOn/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const ScreenshareOnIcon = props => <Icon alt="ScreenshareOn" content={content} {...props} />
