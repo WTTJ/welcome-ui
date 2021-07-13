9ce5d9750f42f56fb766b646cdffdd5d32423ef9
Duarte <duarte.paradela@wttj.co>
2021-07-13 14:58:16 +0200

    fix: remove useless WuiProps & updated to typescript test files

--- packages/Shape/styles.ts
+++ packages/Shape/styles.ts
@@ -1,7 +1,8 @@
 import styled, { css } from '@xstyled/styled-components'
-import { system, WuiProps } from '@welcome-ui/system'
+import { system } from '@welcome-ui/system'
 import { getMax } from '@welcome-ui/utils'
 import { Shape as ShapeType } from 'Shape'
+import { Box } from '@welcome-ui/box'
 
 const shapeStyles = (w: string, h: string, shape: ShapeType) => css`
   width: ${getMax(w, h)};
@@ -9,7 +10,7 @@ const shapeStyles = (w: string, h: string, shape: ShapeType) => css`
   border-radius: ${shape === 'circle' && '50%'};
 `
 
-export const Shape = styled.div<{ shape: ShapeType } & WuiProps>(
+export const Shape = styled(Box)<{ shape: ShapeType }>(
   ({ h, shape, w }) => css`
     position: relative;
     overflow: hidden;
