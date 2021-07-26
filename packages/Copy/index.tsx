5595d17af3445cec361736b9bc1dcf855277c222
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2021-07-26 10:39:56 +0200

    fix: use | operator for targeted element in refObject

--- packages/Copy/index.tsx
+++ packages/Copy/index.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react'
 import copyToClipboard from 'copy-to-clipboard'
 
 export function useCopyText(
-  content: React.RefObject<HTMLElement & HTMLInputElement> | number | string,
+  content: React.RefObject<HTMLElement | HTMLInputElement> | number | string,
   timeout: number
 ): [() => void, boolean] {
   const [copied, setCopied] = useState<boolean>()
@@ -11,8 +11,10 @@ export function useCopyText(
     let value
     if (typeof content === 'number' || typeof content === 'string') {
       value = content.toString()
-    } else if (content.current) {
-      value = content.current.textContent || content.current.value
+    } else if (content.current instanceof HTMLInputElement) {
+      value = content.current.value
+    } else if (content.current instanceof HTMLElement) {
+      value = content.current.textContent
     }
 
     const copiedString = copyToClipboard(value)
