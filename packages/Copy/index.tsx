4313617dbc36881ce16bf0ae903688c04155f746
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2021-07-26 10:07:49 +0200

    feat: utils.copy to typescript

--- packages/Copy/index.tsx
+++ packages/Copy/index.tsx
@@ -1,13 +1,16 @@
 import { useCallback, useState } from 'react'
 import copyToClipboard from 'copy-to-clipboard'
 
-export function useCopyText(content, timeout) {
-  const [copied, setCopied] = useState()
+export function useCopyText(
+  content: React.RefObject<HTMLElement & HTMLInputElement> | number | string,
+  timeout: number
+): [() => void, boolean] {
+  const [copied, setCopied] = useState<boolean>()
 
   const copy = useCallback(() => {
     let value
-    if (['number', 'string'].includes(typeof content)) {
-      value = content
+    if (typeof content === 'number' || typeof content === 'string') {
+      value = content.toString()
     } else if (content.current) {
       value = content.current.textContent || content.current.value
     }
