2d99749a37ab2c87c71947ba9913f3901f219b78
Mickaël Le Ralec <mickael.leralec@gmail.com>
2021-08-12 14:26:12 +0200

    feat: convert icon.font to TS

---
+++ packages/IconFont/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { IconProps } from '@welcome-ui/icon'
+
+import { Icon } from './styles'
+import unicodeMap from './unicode.json'
+
+export const IconFont = Icon
+
+// todo move toCamelCase/toPascalCase on ts file
+const toCamelCase = (str: string): string => str.replace(/_(\w)/g, ($, $1) => $1.toUpperCase())
+
+const toPascalCase = (str: string) => {
+  const camelCase = toCamelCase(str)
+  return `${camelCase.charAt(0).toUpperCase()}${camelCase.substr(1)}`
+}
+
+export const Icons = Object.keys(unicodeMap).reduce(
+  (prev: Record<string, (props: IconProps) => JSX.Element>, name) => {
+    const key = toPascalCase(name) as keyof typeof unicodeMap
+    // eslint-disable-next-line react/display-name
+    prev[key] = (props: IconProps) => <Icon {...props} name={name} />
+    return prev
+  },
+  {}
+)
--- packages/IconFont/styles.ts
+++ packages/IconFont/styles.ts
@@ -1,11 +1,17 @@
 import styled, { css } from '@xstyled/styled-components'
 import { th } from '@xstyled/system'
 import { system } from '@welcome-ui/system'
+import { IconOptions } from '@welcome-ui/icon'
 
 import unicodeMap from './unicode.json'
 
+type StyledIcon = {
+  name: keyof typeof unicodeMap
+  size: IconOptions['size']
+}
+
 // stylelint-disable font-family-no-missing-generic-family-keyword
-export const Icon = styled.i(
+export const Icon = styled.i<StyledIcon>(
   ({ name, size = 'md' }) => css`
     display: inline-block;
     font-family: icons;
