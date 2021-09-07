4baac01eb00107236810f3f3d9e5e12dd3ea01ef
Paul-Xavier Ceccaldi <pix@wttj.co>
2021-09-07 17:10:13 +0200

    Feat add new icon set (#907)
    * fix: remove duplicate pen icon
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * feat: add unavailable icon
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * feat: add new icon set
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * fix: change screenshare off and on icons
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * fix: add new icon webfont
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * fix: when adding icons with yarn icons, don't checkout builded icon font
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

---
+++ icons/CommonArea/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Icon } from '@welcome-ui/icon'
+
+import content from './content.json'
+export const CommonAreaIcon = props => <Icon alt="CommonArea" content={content} {...props} />
