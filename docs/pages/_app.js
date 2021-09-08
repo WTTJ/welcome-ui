9c43c4a084351a8f9cb6864b85235b43c4adc2d9
guillaumewttj <61152048+guillaumewttj@users.noreply.github.com>
2021-09-08 17:57:53 +0200

    Fix icons css script & remove deploy on tags (netlify config now) (#908)
    * fix: do not use hex value in generated css stylesheet for icon font
    
    * fix: remove deploy script for docs since netlify handle it

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -6,6 +6,8 @@ import { Provider } from 'reakit'
 import { ThemeProvider } from '../context/theme'
 import { App } from '../components/App'
 
+require('@welcome-ui/icons.font/fonts/welcome-icon-font.css')
+
 const NextApp = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
