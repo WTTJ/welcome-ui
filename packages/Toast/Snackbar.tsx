d4ef18ffbae209f31b4a635e4c9b99ead6fd4b36
Paul-Xavier Ceccaldi <pix@wttj.co>
2022-01-03 15:44:05 +0100

    fix: optionalize button and fix its styles (#985)
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- packages/Toast/Snackbar.tsx
+++ packages/Toast/Snackbar.tsx
@@ -5,19 +5,23 @@ import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
 import { Title, TitleOptions } from './Title'
 import * as S from './styles'
 
+import { Variant } from '.'
+
 export interface SnackbarOptions {
+  variant?: Variant
   onClose: () => void
+  hasCloseButton?: boolean
 }
 
 export type SnackbarProps = CreateWuiProps<'div', SnackbarOptions & TitleOptions>
 
 export const Snackbar = forwardRef<'div', SnackbarProps>(
-  ({ children, icon, onClose, variant = 'info', ...rest }, ref) => (
-    <S.Snackbar ref={ref} variant={variant} {...rest}>
-      <Title icon={icon} pb="0" variant={variant}>
+  ({ children, hasCloseButton = true, icon, onClose, variant = 'info', ...rest }, ref) => (
+    <S.Snackbar hasCloseButton={hasCloseButton} ref={ref} variant={variant} {...rest}>
+      <Title icon={icon} variant={variant}>
         <>
           {children}
-          {onClose && <ClearButton onClick={onClose} />}
+          {hasCloseButton && <ClearButton onClick={onClose} />}
         </>
       </Title>
     </S.Snackbar>
