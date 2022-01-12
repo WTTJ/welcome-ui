a4f050a4b3807b01e99b3832f3bfd691bf3b9386
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2022-01-12 10:17:19 +0100

    feat: handle deployment of previous docs versions on same domain

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,4 +2,5 @@ const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  basePath: process.env.VERSION ? `/${process.env.VERSION}` : undefined
 })
