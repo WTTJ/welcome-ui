3692b165609ec863ca0881bbb651c764b0eb49ca
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-02-10 15:58:50 +0100

    fix: allow mjs in node_modules

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,24 @@
-const withMDX = require('@next/mdx')()
-
-module.exports = withMDX({
+module.exports = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  basePath: process.env.VERSION ? `/${process.env.VERSION}` : undefined
-})
+  basePath: process.env.VERSION ? `/${process.env.VERSION}` : undefined,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: {},
+        },
+      ],
+    })
+
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    return config
+  },
+}
