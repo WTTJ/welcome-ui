2fbdc35ca7b6d07834fa5b570fd782d8e64e0b9e
Mickaël <mickael.leralec@gmail.com>
2022-02-10 13:56:45 +0100

    fix wui props types (#1009)
    * feat: add useTheme
    
    * fix: wui props types with wui theme

--- styled.d.ts
+++ styled.d.ts
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components'
 import '@xstyled/system'
+import '@xstyled/styled-components'
 
-import { WuiTheme } from './packages/Core/theme/types'
+import { WuiTheme } from '@welcome-ui/core'
 
 declare module '@xstyled/system' {
+  export interface Theme extends WuiTheme {}
+}
+
+declare module '@xstyled/styled-components' {
   export interface Theme extends WuiTheme {}
 }
 
