0fc4f3d8e6fd215c33e33f2274395bb35bb30582
Mickaël <mickael.leralec@gmail.com>
2022-02-14 11:01:43 +0100

    feat: add 5xl/6xl on theme space (#1019)

--- packages/Core/theme/space.ts
+++ packages/Core/theme/space.ts
@@ -10,6 +10,8 @@ export type ThemeSpace = {
   xxl: string
   '3xl': string
   '4xl': string
+  '5xl': string
+  '6xl': string
   [key: string]: string
   [key: number]: string
 }
@@ -25,5 +27,7 @@ export const getSpace = (theme: WuiTheme): ThemeSpace => {
     xxl: theme.toRem(24),
     '3xl': theme.toRem(30),
     '4xl': theme.toRem(36),
+    '5xl': theme.toRem(50),
+    '6xl': theme.toRem(64),
   }
 }
