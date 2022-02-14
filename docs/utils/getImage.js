fa4c7e75b6e907e65670b9371667e50b8ef87921
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2022-02-14 13:01:28 +0100

    fix: conditionnal chaining on next config access

--- docs/utils/getImage.js
+++ docs/utils/getImage.js
@@ -3,5 +3,5 @@ import getConfig from 'next/config'
 export const getImage = path => {
   const { publicRuntimeConfig } = getConfig()
 
-  return publicRuntimeConfig.basePath ? `${publicRuntimeConfig.basePath}${path}` : path
+  return publicRuntimeConfig?.basePath ? `${publicRuntimeConfig.basePath}${path}` : path
 }
