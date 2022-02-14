319656172e6887a4deec8e523687616f55c012ad
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2022-02-14 12:05:01 +0100

    fix: use basePath for images

--- docs/components/Illustration404.js
+++ docs/components/Illustration404.js
@@ -2,12 +2,13 @@ import React, { memo } from 'react'
 import { Box } from '@welcome-ui/box'
 
 import { useThemeContext } from '../context/theme'
+import { getImage } from '../utils/getImage'
 
 const Illustration404 = memo(function Illustration() {
   const theme = useThemeContext()
   const imageSource = theme === 'dark' ? '/illustration-404-white.png' : '/illustration-404.png'
 
-  return <Box as="img" maxWidth={500} src={imageSource} w={1} />
+  return <Box as="img" maxWidth={500} src={getImage(imageSource)} w={1} />
 })
 
 export default Illustration404
