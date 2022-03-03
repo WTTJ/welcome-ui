0baab47246244e8db70c16e0655cf332a6eec9fe
Guillaume Rousseau <guillaume.rousseau@wttj.co>
2022-03-03 15:16:29 +0100

    chore: remove final form

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/mocks/styleMock.js',
-    'react-final-form': '<rootDir>/node_modules/react-final-form',
   },
   setupFilesAfterEnv: ['<rootDir>/mocks/setup.js'],
   modulePaths: ['<rootDir>/node_modules'],
