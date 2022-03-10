907a95bdb19089e87eee8bf9523d84584808b60a
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2022-03-10 11:14:00 +0100

    feat: add Flex component (#1032)
    Signed-off-by: Théo Mesnil <mesniltheo@gmail.com>

---
+++ packages/Flex/README.md
@@ -0,0 +1,17 @@
+# @welcome-ui/flex
+
+The [Flex](https://welcome-ui.com/components/flex) component from [@welcome-ui](https://welcome-ui.com).
+
+![npm bundle size](https://img.shields.io/bundlephobia/minzip/@welcome-ui/flex) [![License](https://img.shields.io/npm/l/welcome-ui.svg)](https://github.com/WTTJ/welcome-ui/blob/master/LICENSE) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-mediumspringgreen.svg)](ttps://github.com/WTTJ/welcome-ui/blob/master/CONTRIBUTING.mdx)
+
+## Installation
+
+    yarn add @welcome-ui/flex
+
+## Import
+
+    import { Flex } from '@welcome-ui/flex'
+
+## Documentation
+
+See the [documentation](https://welcome-ui.com/components/flex) or [package source](https://github.com/WTTJ/welcome-ui/tree/master/packages/Flex) for more details.
---
+++ packages/Flex/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Box } from '@welcome-ui/box'
+import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
+import { SystemProps } from '@xstyled/system'
+
+export interface FlexOptions {
+  /**
+   * same as alignItems
+   */
+  align?: SystemProps['alignItems']
+  /**
+   * same as justifyContent
+   */
+  justify?: SystemProps['justifyContent']
+  /**
+   * same as flexWrap
+   */
+  wrap?: SystemProps['flexWrap']
+  /**
+   * same as flexDirection
+   */
+  direction?: SystemProps['flexDirection']
+  /**
+   * same as flexBasis
+   */
+  basis?: SystemProps['flexBasis']
+  /**
+   * same as flexGrow
+   */
+  grow?: SystemProps['flexGrow']
+  /**
+   * same as flexShrink
+   */
+  shrink?: SystemProps['flexShrink']
+}
+
+export type FlexProps = CreateWuiProps<'div', FlexOptions>
+
+export const Flex = forwardRef<'div', FlexProps>(
+  ({ align, basis, dataTestId, direction, grow, justify, shrink, wrap, ...rest }, ref) => {
+    return (
+      <Box
+        alignItems={align}
+        data-testid={dataTestId}
+        display="flex"
+        flexBasis={basis}
+        flexDirection={direction}
+        flexGrow={grow}
+        flexShrink={shrink}
+        flexWrap={wrap}
+        justifyContent={justify}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+)
+
+Flex.displayName = 'Flex'
