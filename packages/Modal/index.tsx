bcd3f45310a98733811136f66da5dfccd2769ae4
cnairiwttj <86954832+cnairiwttj@users.noreply.github.com>
2022-05-04 14:55:07 +0200

    fix: extract datatestid from rest to avoid doublon (#1253)
    * fix: extract datatestid from rest to avoid doublon
    
    * fix: extract dataTestId from rest props

--- packages/Modal/index.tsx
+++ packages/Modal/index.tsx
@@ -36,79 +36,84 @@ export function useModalState(options?: ModalInitialState): ModalStateReturn {
   return useDialogState({ animated: true, ...options })
 }
 
-const ModalComponent = forwardRef<'div', ModalProps>((props, ref) => {
-  const {
-    ariaLabel,
-    children,
-    hide,
-    hideOnClickOutside = true,
-    onClose,
-    closeElement: CloseElement = Close,
-    size = 'lg',
-    tabIndex,
-    ...rest
-  } = props
+const ModalComponent = forwardRef<'div', ModalProps>(
+  (
+    {
+      ariaLabel,
+      children,
+      hide,
+      hideOnClickOutside = true,
+      onClose,
+      closeElement: CloseElement = Close,
+      size = 'lg',
+      tabIndex,
+      dataTestId,
+      ...rest
+    },
+    ref
+  ) => {
+    const headerRef = useRef(null)
+    const footerRef = useRef(null)
+    const headerHeight = headerRef?.current?.clientHeight
+    const footerHeight = footerRef?.current?.clientHeight
 
-  const headerRef = useRef(null)
-  const footerRef = useRef(null)
-  const headerHeight = headerRef?.current?.clientHeight
-  const footerHeight = footerRef?.current?.clientHeight
-
-  function setRef(name?: string) {
-    if (name === 'ModalTitle') {
-      return headerRef
-    }
-
-    if (name === 'ModalFooter') {
-      return footerRef
-    }
-
-    return undefined
-  }
-
-  function getContentStyles(name?: string) {
-    if (name === 'Content') {
-      return {
-        mt: { xs: headerHeight, md: 0 },
-        mb: { xs: footerHeight, md: 0 },
-        pr: !headerHeight ? '5xl' : undefined,
+    function setRef(name?: string) {
+      if (name === 'ModalTitle') {
+        return headerRef
       }
+
+      if (name === 'ModalFooter') {
+        return footerRef
+      }
+
+      return undefined
     }
 
-    return {}
+    function getContentStyles(name?: string) {
+      if (name === 'Content') {
+        return {
+          mt: { xs: headerHeight, md: 0 },
+          mb: { xs: footerHeight, md: 0 },
+          pr: !headerHeight ? '5xl' : undefined,
+        }
+      }
+
+      return {}
+    }
+
+    const closeModal = () => {
+      onClose?.()
+      hide()
+    }
+
+    return (
+      <S.Backdrop {...rest} hideOnClickOutside={hideOnClickOutside}>
+        <S.Dialog
+          aria-label={ariaLabel}
+          data-testid={dataTestId}
+          hide={closeModal}
+          hideOnClickOutside={hideOnClickOutside}
+          ref={ref}
+          size={size}
+          tabIndex={tabIndex}
+          {...rest}
+        >
+          <CloseElement onClick={closeModal} />
+          {Children.map(children, (child: JSX.Element) => {
+            if (!child) return null
+            const name = child?.type?.displayName || child?.type?.name
+
+            return cloneElement(child, {
+              ref: setRef(name),
+              ...getContentStyles(name),
+              ...child.props,
+            })
+          })}
+        </S.Dialog>
+      </S.Backdrop>
+    )
   }
-
-  const closeModal = () => {
-    onClose?.()
-    hide()
-  }
-
-  return (
-    <S.Backdrop {...rest} hideOnClickOutside={hideOnClickOutside}>
-      <S.Dialog
-        aria-label={ariaLabel}
-        hide={closeModal}
-        hideOnClickOutside={hideOnClickOutside}
-        ref={ref}
-        size={size}
-        tabIndex={tabIndex}
-        {...rest}
-      >
-        <CloseElement onClick={closeModal} />
-        {Children.map(children, (child: JSX.Element) => {
-          if (!child) return null
-          const name = child?.type?.displayName || child?.type?.name
-
-          return cloneElement(child, {
-            ref: setRef(name),
-            ...getContentStyles(name),
-            ...child.props,
-          })
-        })}
-      </S.Dialog>
-    </S.Backdrop>
-  )
-})
+)
 
 ModalComponent.displayName = 'Modal'
 
