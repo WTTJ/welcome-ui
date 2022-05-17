d3ee8b9819bbb4022b2591b34522e4df0c56e37d
Diane Guillot <36013102+Leiksa@users.noreply.github.com>
2022-05-17 18:02:28 +0200

    fix: condition to check hex

--- packages/Utils/hex-to-rgba.ts
+++ packages/Utils/hex-to-rgba.ts
@@ -1,13 +1,9 @@
 import { hexToRGB } from './hex-to-rgb'
 
-const colorFormatRegex =
-  /^(rgb|hsl)(a?)[(]\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*(?:,\s*([\d.]+)\s*)?[)]$/
-
 export const hexToRGBA = (hex: string, transparency = 1): string => {
   if (!hex) return
 
-  // check if it's rgb(), rgba(), hsl() and hsla()
-  if (!hex.startsWith('#') && colorFormatRegex.test(hex)) return hex
+  if (!hex.startsWith('#')) return hex
 
   const toRgb = hexToRGB(hex)
 
