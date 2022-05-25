2af82f66d5f51332d874f5dd0d5dfed1641c1211
cnairiwttj <86954832+cnairiwttj@users.noreply.github.com>
2022-05-25 14:30:19 +0200

    fix: add styled system to UniversalLink (#1323)

--- packages/UniversalLink/styles.ts
+++ packages/UniversalLink/styles.ts
@@ -1,7 +1,7 @@
 import styled from '@xstyled/styled-components'
 import { shouldForwardProp } from '@welcome-ui/system'
 
-export const UniversalLink = styled('a').withConfig({ shouldForwardProp })`
+export const UniversalLink = styled.aBox.withConfig({ shouldForwardProp })`
   color: inherit;
   text-decoration: none;
 `
