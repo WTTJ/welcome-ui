da1a720f677cb2b6c07784d07f5768252368fb8b
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-06-11 19:06:15 +0200

    chore: update ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    // setupFiles: ['./tests/setup.ts', './tests/globals.ts'],
-    // coverage: {
-    //   exclude: ['src/**/styles.ts', 'src/**/*.test.*', 'tests'],
-    // },
   },
 })
