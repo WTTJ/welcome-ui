3d9a5afb7d9d82a336dd64a9b0a3b1bb206aabd6
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-06-13 11:47:59 +0200

    fix: resolve src instead of built files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
+import fs from 'fs'
+import { resolve } from 'path'
+
 import { defineConfig } from 'vitest/config'
+
+import { toKebabCase } from './utils/strings'
+
+/* in test mode, we must resolve source files instead of built files */
+const resolveSrc = () => {
+  const packages = fs.readdirSync(resolve(__dirname, 'packages'))
+
+  return packages.reduce((aliases, name) => {
+    const packageName = `@welcome-ui/${toKebabCase(name)}`
+    const packagePath = `packages/${name}/index.tsx`
+    const path = fs.existsSync(packagePath)
+      ? packagePath
+      : packagePath.replace('index.tsx', 'index.ts')
+
+    return { ...aliases, [packageName]: resolve(__dirname, path) }
+  }, {})
+}
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: resolveSrc(),
   },
 })
