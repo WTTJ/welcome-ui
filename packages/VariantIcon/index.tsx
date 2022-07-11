d3565e578fb90f4d99828a5e6ea8913455db128f
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-07-11 19:10:38 +0200

    refactor: variant icon component

--- packages/VariantIcon/index.tsx
+++ packages/VariantIcon/index.tsx
@@ -1,12 +1,12 @@
 import React, { useMemo } from 'react'
 import { AlertIcon } from '@welcome-ui/icons'
+import { Box } from '@welcome-ui/box'
 import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
 import { CheckIcon, InformationIcon } from '@welcome-ui/icons'
-import { Variant } from '@welcome-ui/utils'
+import { getVariantColor, Variant } from '@welcome-ui/utils'
+import { useTheme } from 'styled-components'
 
-import * as S from './styles'
-
-export interface VariantIconOptions {
+export type VariantIconOptions = {
   icon?: JSX.Element
   variant?: Variant
 }
@@ -15,6 +15,9 @@ export type VariantIconProps = CreateWuiProps<'div', VariantIconOptions>
 
 export const VariantIcon = forwardRef<'div', VariantIconProps>(
   ({ icon, variant, ...rest }, ref) => {
+    const theme = useTheme()
+    const color = getVariantColor(variant)(theme) as string
+
     const Icon = useMemo(() => {
       if (icon === null) return null
       if (icon) return icon
@@ -23,11 +26,21 @@ export const VariantIcon = forwardRef<'div', VariantIconProps>(
       if (variant === 'error' || variant === 'warning') return <AlertIcon />
     }, [icon, variant])
 
-    return Icon ? (
-      <S.VariantIcon ref={ref} variant={variant} {...rest}>
+    if (!Icon) return null
+
+    return (
+      <Box
+        $alignSelf="center"
+        $color={color}
+        $display="inline-flex"
+        $flexShrink="0"
+        $mr="xxs"
+        ref={ref}
+        {...rest}
+      >
         {Icon}
-      </S.VariantIcon>
-    ) : null
+      </Box>
+    )
   }
 )
 
