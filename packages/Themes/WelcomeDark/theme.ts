d6cd8752253e13253746b48e834141f022c54ec5
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-07-12 15:11:46 +0200

    refactor: themes

--- packages/Themes/WelcomeDark/theme.ts
+++ packages/Themes/WelcomeDark/theme.ts
@@ -20,20 +20,20 @@ const palette = {
 
 export const colors = {
   light: {
-    100: theme.colors.dark[100],
-    200: theme.colors.dark[200],
-    400: theme.colors.dark[400],
-    500: theme.colors.dark[500],
-    700: theme.colors.dark[700],
-    900: theme.colors.dark[900],
+    100: theme.colors['dark.100'],
+    200: theme.colors['dark.200'],
+    400: theme.colors['dark.400'],
+    500: theme.colors['dark.500'],
+    700: theme.colors['dark.700'],
+    900: theme.colors['dark.900'],
   },
   dark: {
-    100: theme.colors.light[100],
-    200: theme.colors.light[200],
-    400: theme.colors.light[400],
-    500: theme.colors.light[500],
-    700: theme.colors.light[700],
-    900: theme.colors.light[900],
+    100: theme.colors['light.100'],
+    200: theme.colors['light.200'],
+    400: theme.colors['light.400'],
+    500: theme.colors['light.500'],
+    700: theme.colors['light.700'],
+    900: theme.colors['light.900'],
   },
   nude: {
     100: palette.isabelline,
@@ -43,8 +43,8 @@ export const colors = {
     700: palette.ironside,
     900: palette.dune,
   },
-  border: theme.colors.light[200],
-  underline: theme.colors.primary[700],
+  border: theme.colors['light.200'],
+  underline: theme.colors['primary.700'],
 }
 
 export const welcomeDarkTheme: RecursivePartial<WuiTheme> = merge(welcomeTheme, {
