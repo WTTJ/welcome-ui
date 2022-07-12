b6cf9db8a4984cf8b8dc4d6c68e590f51f473e6b
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-07-12 15:47:19 +0200

    refactor: flex/grid

--- packages/Grid/index.tsx
+++ packages/Grid/index.tsx
@@ -6,29 +6,29 @@ import { Item } from './Item'
 
 export interface GridOptions {
   /** same as gridArea */
-  area?: WuiProps['gridArea']
+  area?: WuiProps['$gridArea']
   /** same as gridAutoColumns */
-  autoColumns?: WuiProps['gridAutoColumns']
+  autoColumns?: WuiProps['$gridAutoColumns']
   /** same as gridAutoFlow */
-  autoFlow?: WuiProps['gridAutoFlow']
+  autoFlow?: WuiProps['$gridAutoFlow']
   /** same as gridAutoRows */
-  autoRows?: WuiProps['gridAutoRows']
+  autoRows?: WuiProps['$gridAutoRows']
   /** same as gridColumn */
-  column?: WuiProps['gridColumn']
+  column?: WuiProps['$gridColumn']
   /** same as columnGap */
-  columnGap?: WuiProps['columnGap']
+  columnGap?: WuiProps['$gridColumnGap']
   /** same as gridGap */
-  gap?: WuiProps['gap']
+  gap?: WuiProps['$gridGap']
   /** same as gridRow */
-  row?: WuiProps['gridRow']
+  row?: WuiProps['$gridRow']
   /** same as gridRowGap */
-  rowGap?: WuiProps['rowGap']
+  rowGap?: WuiProps['$gridRowGap']
   /** same as gridTemplateAreas */
-  templateAreas?: WuiProps['gridTemplateAreas']
+  templateAreas?: WuiProps['$gridTemplateAreas']
   /** same as gridTemplateColumns */
-  templateColumns?: WuiProps['gridTemplateColumns']
+  templateColumns?: WuiProps['$gridTemplateColumns']
   /** same as gridTemplateRows */
-  templateRows?: WuiProps['gridTemplateRows']
+  templateRows?: WuiProps['$gridTemplateRows']
 }
 
 export type GridProps = CreateWuiProps<'div', GridOptions>
@@ -55,21 +55,21 @@ const GridComponent = forwardRef<'div', GridProps>(
   ) => {
     return (
       <Box
-        columnGap={columnGap}
+        $display="grid"
+        $gridArea={area}
+        $gridAutoColumns={autoColumns}
+        $gridAutoFlow={autoFlow}
+        $gridAutoRows={autoRows}
+        $gridColumn={column}
+        $gridColumnGap={columnGap}
+        $gridGap={gap}
+        $gridRow={row}
+        $gridRowGap={rowGap}
+        $gridTemplateAreas={templateAreas}
+        $gridTemplateColumns={templateColumns}
+        $gridTemplateRows={templateRows}
         data-testid={dataTestId}
-        display="grid"
-        gap={gap}
-        gridArea={area}
-        gridAutoColumns={autoColumns}
-        gridAutoFlow={autoFlow}
-        gridAutoRows={autoRows}
-        gridColumn={column}
-        gridRow={row}
-        gridTemplateAreas={templateAreas}
-        gridTemplateColumns={templateColumns}
-        gridTemplateRows={templateRows}
         ref={ref}
-        rowGap={rowGap}
         {...rest}
       />
     )
