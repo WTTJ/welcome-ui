927b17a26274749d6b9ecc5522c882a3013d0a3f
Isaac <maamouche.isaac@gmail.com>
2022-08-16 14:52:07 +0200

    feat: updated accordion acording to the figma specs

--- packages/Accordion/styles.ts
+++ packages/Accordion/styles.ts
@@ -5,6 +5,10 @@ import { Box } from '@welcome-ui/box'
 export const Accordion = styled.div`
   ${th('accordions.wrapper')};
   ${system}
+  transition: medium;
+  &:hover {
+    border-color: ${th('colors.light.200')};
+  }
 `
 
 export const Icon = styled(Box)<{ visible: boolean }>(
@@ -13,27 +17,44 @@ export const Icon = styled(Box)<{ visible: boolean }>(
     ${th('accordions.icon')};
     transform: ${visible ? 'rotate3d(0, 0, 1, 90deg)' : 'rotate3d(0)'};
     transition: medium;
+    --size: 24px;
+    width: var(--size);
+    height: 24;
+    color: inherit;
+    display: flex;
+    border-radius: 12;
+    & *:first-child {
+      margin: auto;
+    }
   `
 )
 
 export const Disclosure = styled(ReakitDisclosure)`
+  ${th('texts.h5')};
   ${th('accordions.title')};
   width: 100%;
-  padding: ${th('accordions.padding')};
+  padding: ${th('accordions.padding')}; //css du thème ignorés
+  // padding: 16;
   background-color: transparent;
   border: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
-
+  gap: xxl;
   &:focus,
   &:hover {
     cursor: pointer;
   }
 
+  &:hover {
+    border-color: ${th('colors.light.200')};
+    ${Icon} {
+      background-color: nude.200;
+    }
+  }
+
   &:focus {
     outline: 0;
-
     ${Icon} {
       color: inherit;
     }
@@ -43,14 +64,11 @@ export const Disclosure = styled(ReakitDisclosure)`
 export const Content = styled(DisclosureContent)(
   ({ visible }) => css`
     ${th('accordions.content')};
-    margin-top: -sm;
-    padding-left: ${th('accordions.padding')};
-    padding-right: ${th('accordions.padding')};
-    padding-bottom: sm;
-
+    padding-inline: 16px; // Ne fonctionne pas sans le px
+    color: ${th('colors.dark.200')};
     ${visible &&
     css`
-      padding-bottom: ${th('accordions.padding')};
+      padding-bottom: 16;
     `}
   `
 )
