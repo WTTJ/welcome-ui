b28d1a9592108b5d0397f1dbab62f9be0110e077
Jimmy Jouanne <jimmy.jouanne@wttj.co>
2022-08-26 11:40:10 +0200

    test(badge): check of properties

--- packages/Badge/index.test.tsx
+++ packages/Badge/index.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import { render } from '../../utils/tests'
+import { createTheme } from '../Core/theme/core'
 
 import { Badge } from './index'
+
+const theme = createTheme()
 
 const content = '1'
 
@@ -11,5 +14,51 @@ describe('<Badge>', () => {
     const { container } = render(<Badge>{content}</Badge>)
 
     expect(container).toHaveTextContent(content)
+  })
+
+  it('should have correct size', () => {
+    const { getByTestId } = render(
+      <Badge dataTestId="badge" size="sm">
+        {content}
+      </Badge>
+    )
+    const badge = getByTestId('badge')
+
+    expect(badge).toHaveStyleRule('padding', theme.badges.sizes.sm.padding)
+  })
+
+  it('should have correct primary variant color', () => {
+    const { getByTestId } = render(
+      <Badge dataTestId="badge" variant="primary">
+        {content}
+      </Badge>
+    )
+    const badge = getByTestId('badge')
+
+    expect(badge).toHaveStyleRule('background-color', theme.colors.primary[500])
+    expect(badge).toHaveStyleRule('color', 'rgba(0,0,0,1)')
+  })
+
+  it('should have correct primary variant color if disabled', () => {
+    const { getByTestId } = render(
+      <Badge dataTestId="badge" disabled variant="primary">
+        {content}
+      </Badge>
+    )
+    const badge = getByTestId('badge')
+
+    expect(badge).toHaveStyleRule('background-color', theme.colors.primary[600])
+    expect(badge).toHaveStyleRule('color', theme.colors.primary[800])
+  })
+
+  it('should have correct circle shape', () => {
+    const { getByTestId } = render(
+      <Badge dataTestId="badge" shape="circle">
+        {content}
+      </Badge>
+    )
+    const badge = getByTestId('badge')
+
+    expect(badge).toHaveStyleRule('border-radius', '0.875rem')
   })
 })
