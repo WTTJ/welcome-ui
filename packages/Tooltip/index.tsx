5e2ad413c88658cf7840e625228c9ef930f13369
Isaac <maamouche.isaac@gmail.com>
2023-02-08 10:31:07 +0100

    fix: add div around tooltip children

--- packages/Tooltip/index.tsx
+++ packages/Tooltip/index.tsx
@@ -10,6 +10,7 @@ import Popper, { Placement } from 'popper.js'
 import { TooltipReference, useTooltipState } from 'reakit/Tooltip'
 import { useDialogState } from 'reakit/Dialog'
 import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
+import { Box } from '@welcome-ui/box'
 
 import * as S from './styles'
 
@@ -143,7 +144,7 @@ export const Tooltip = forwardRef<'div', TooltipProps>((props, ref): React.React
   return (
     <>
       <TooltipReference as={undefined} {...tooltip}>
-        {referenceProps => cloneElement(child, referenceProps)}
+        {referenceProps => cloneElement(<Box>{child}</Box>, referenceProps)}
       </TooltipReference>
 
       <S.Tooltip ref={ref} {...tooltip} {...rest}>
