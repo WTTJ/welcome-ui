4b22927f8c11b292eb1c436f98715d9289f3952c
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2023-02-15 16:25:41 +0100

    fix: force label color
    We need to force label color because we got an issue on modal, the Body is in black-900 color and the label get the dark 900 color.

--- packages/Core/src/theme/labels.ts
+++ packages/Core/src/theme/labels.ts
@@ -5,10 +5,11 @@ import { WuiTheme } from './types'
 export type ThemeLabels = CSSObject
 
 export const getLabels = (theme: WuiTheme): ThemeLabels => {
-  const { fontSizes, fontWeights } = theme
+  const { fontSizes, fontWeights, colors } = theme
 
   return {
     fontSize: fontSizes.sm,
     fontWeight: fontWeights.medium,
+    color: colors['dark-500']
   }
 }
