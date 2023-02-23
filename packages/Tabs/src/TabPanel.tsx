62ef0b18f1b3de11cefffe393856b3142ddd8f85
Théo Mesnil <mesniltheo@gmail.com>
2023-02-23 10:54:50 +0100

    chore: rework Tab
    Signed-off-by: Théo Mesnil <mesniltheo@gmail.com>

--- packages/Tabs/src/TabPanel.tsx
+++ packages/Tabs/src/TabPanel.tsx
@@ -1,31 +1,27 @@
 import React from 'react'
-import {
-  TabPanel as ReakitTabPanel,
-  TabPanelOptions as ReakitTabPanelOptions,
-  TabStateReturn,
-} from 'reakit/Tab'
+import { TabPanel as ReakitTabPanel, TabStateReturn } from 'reakit/Tab'
 import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
 
 import * as S from './styles'
 
-export type TabPanelOptions = Pick<TabStateReturn, 'orientation'> & ReakitTabPanelOptions
+export type TabPanelOptions = { state: TabStateReturn }
 export type TabPanelProps = CreateWuiProps<typeof ReakitTabPanel, TabPanelOptions>
 
 /**
  * @name Tabs.TabPanel
  */
-export const TabPanel = forwardRef<'div', TabPanelProps>((props, ref) => {
-  const { as, children, orientation, tabId, ...rest } = props
-
-  return (
-    <ReakitTabPanel as={undefined} ref={ref} tabId={tabId} {...rest}>
-      {tabPanelProps => (
-        <S.TabPanel as={as} orientation={orientation} {...tabPanelProps}>
-          {children}
-        </S.TabPanel>
-      )}
-    </ReakitTabPanel>
-  )
-})
+export const TabPanel = forwardRef<'div', TabPanelProps>(
+  ({ as, children, state, tabId, ...rest }, ref) => {
+    return (
+      <ReakitTabPanel as={undefined} ref={ref} tabId={tabId} {...state} {...rest}>
+        {tabPanelProps => (
+          <S.TabPanel as={as} orientation={state?.orientation} {...tabPanelProps}>
+            {children}
+          </S.TabPanel>
+        )}
+      </ReakitTabPanel>
+    )
+  }
+)
 
 TabPanel.displayName = 'TabPanel'
--- packages/Tabs/src/index.tsx
+++ packages/Tabs/src/index.tsx
@@ -6,23 +6,25 @@ import { TabList } from './TabList'
 import { TabPanel } from './TabPanel'
 import * as S from './styles'
 
-export type TabOptions = ReakitTabOptions
+export type TabOptions = { state: ReakitTabOptions }
 export type TabProps = CreateWuiProps<'button', TabOptions>
 
 /**
  * @name Tabs
  */
-export const TabComponent = forwardRef<'button', TabProps>(({ as, children, id, ...rest }, ref) => {
-  return (
-    <ReakitTab as={undefined} id={id} ref={ref} {...rest}>
-      {tabProps => (
-        <S.Tab as={as} {...tabProps}>
-          {children}
-        </S.Tab>
-      )}
-    </ReakitTab>
-  )
-})
+export const TabComponent = forwardRef<'button', TabProps>(
+  ({ as, children, id, state, ...rest }, ref) => {
+    return (
+      <ReakitTab as={undefined} id={id} ref={ref} {...state} {...rest}>
+        {tabProps => (
+          <S.Tab as={as} {...tabProps}>
+            {children}
+          </S.Tab>
+        )}
+      </ReakitTab>
+    )
+  }
+)
 
 TabComponent.displayName = 'Tab'
 
