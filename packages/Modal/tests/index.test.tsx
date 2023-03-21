9790e3f421517a0f1e59cd2d3bbab5008603c7c1
Diane Guillot <36013102+Leiksa@users.noreply.github.com>
2023-03-21 14:05:32 +0100

    fix: modal content children type (#2017)
    * fix: modal content children type
    
    * fix: add test

--- packages/Modal/tests/index.test.tsx
+++ packages/Modal/tests/index.test.tsx
@@ -24,4 +24,26 @@ describe('<Modal>', () => {
     fireEvent.click(getByText('open'))
     expect(queryByRole('dialog')).toHaveTextContent('Modal open')
   })
+
+  it('should render conditionally', () => {
+    const ModalTest = () => {
+      const modalState = useModalState()
+      const shouldRender = false
+
+      return (
+        <>
+          <Modal.Trigger state={modalState}>open</Modal.Trigger>
+          <Modal ariaLabel="modal" state={modalState}>
+            <Modal.Content>
+              {shouldRender && <Modal.Body>Modal.Body exist?</Modal.Body>}
+            </Modal.Content>
+          </Modal>
+        </>
+      )
+    }
+
+    const { getByText, queryByRole } = render(<ModalTest />)
+    fireEvent.click(getByText('open'))
+    expect(queryByRole('dialog')).not.toHaveTextContent('Modal.Body exist?')
+  })
 })
