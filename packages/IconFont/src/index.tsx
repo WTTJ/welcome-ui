0c92b210c2e663f56be0c1544b5e674493c958ca
Mickaël <mickael.leralec@gmail.com>
2023-05-31 18:31:46 +0200

    fix: icons.font type (#1973)
    * fix: icons.font type
    
    * fix: add icon keys on the delcaration file
    
    * fix: missing ref on breadcrulb
    
    * fix: export icons

--- packages/IconFont/src/index.tsx
+++ packages/IconFont/src/index.tsx
@@ -2,10 +2,23 @@ import React from 'react'
 import { IconProps } from '@welcome-ui/icon'
 
 import { Icon, StyledIconProps } from './styles'
-import unicodeMap from './unicode.json'
+import unicodeJson from './unicode.json'
 
 export type IconFontProps = StyledIconProps
 export const IconFont = Icon
+
+type FormatIconJSX<S extends string> = S extends `${infer F}${infer R}`
+  ? F extends '_'
+    ? FormatIconJSX<Capitalize<R>>
+    : `${F}${FormatIconJSX<R>}`
+  : ''
+
+// this is usefull to add the keys on the declaration file
+const unicodeMap = { ...unicodeJson }
+
+export type IconKey = keyof typeof unicodeMap
+export type IconKeyFormatted = FormatIconJSX<Capitalize<IconKey>>
+export type IconsType = Record<IconKeyFormatted, (props: IconProps) => JSX.Element>
 
 // todo move toCamelCase/toPascalCase on ts file
 const toCamelCase = (str: string): string => str.replace(/_(\w)/g, ($, $1) => $1.toUpperCase())
@@ -15,21 +28,22 @@ const toPascalCase = (str: string) => {
   return `${camelCase.charAt(0).toUpperCase()}${camelCase.substr(1)}`
 }
 
-export const Icons = Object.keys(unicodeMap).reduce(
-  (prev: Record<string, (props: IconProps) => JSX.Element>, name: keyof typeof unicodeMap) => {
-    const key = toPascalCase(name) as keyof typeof unicodeMap
-    prev[key] = (props: IconProps) => {
-      const className = props.className || ''
-      return (
-        <Icon
-          {...props}
-          className={`${className} wui-icon-font`}
-          data-testid={props.dataTestId && `icon-font-${props.dataTestId}`}
-          name={name}
-        />
-      )
-    }
-    return prev
-  },
-  {}
-)
+const iconsKeys = Object.keys(unicodeMap) as IconKey[]
+
+export const Icons = iconsKeys.reduce<IconsType>((prev, name) => {
+  const key = toPascalCase(name) as IconKeyFormatted
+
+  prev[key] = (props: IconProps) => {
+    const className = props.className || ''
+    return (
+      <Icon
+        {...props}
+        className={`${className} wui-icon-font`}
+        data-testid={props.dataTestId && `icon-font-${props.dataTestId}`}
+        name={name}
+      />
+    )
+  }
+
+  return prev
+}, {} as IconsType)
