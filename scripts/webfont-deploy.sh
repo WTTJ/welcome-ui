a8624d35f78682f506032e6667625fe0ebcb7a59
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2023-07-31 18:44:07 +0200

    chore: remove old fonts type like woff and add cache on icon font (#2153)

--- scripts/webfont-deploy.sh
+++ scripts/webfont-deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 FONTS_DIR=packages/IconFont/fonts
-FONT_FILE="$FONTS_DIR/welcome-icon-font.woff"
+FONT_FILE="$FONTS_DIR/welcome-icon-font.woff2"
 
 if [[ -f "$FONT_FILE" ]]; then
   ICON_FONT_HASH=$(sha1sum $FONT_FILE | awk '{ print $1 }')
 
   echo "Copying fonts to s3://welcome-ui/public/fonts/icon-font/$ICON_FONT_HASH/"
 
-  aws s3 sync $FONTS_DIR s3://welcome-ui/public/fonts/icon-font/$ICON_FONT_HASH/
+  aws s3 sync $FONTS_DIR s3://welcome-ui/public/fonts/icon-font/$ICON_FONT_HASH/ --cache-control 'public, max-age=31536000' --exclude '*' --include 'welcome-icon-font.woff2'
 
   echo "Copying fonts to s3://wttj-production/fonts/icon-font/$ICON_FONT_HASH/"
 
-  aws s3 sync $FONTS_DIR s3://wttj-production/fonts/icon-font/$ICON_FONT_HASH/
+  aws s3 sync $FONTS_DIR s3://wttj-production/fonts/icon-font/$ICON_FONT_HASH/ --cache-control 'public, max-age=31536000' --exclude '*' --include 'welcome-icon-font.woff2'
 else
   echo "Font doesn't exist. Please run 'yarn webfont:build'"
   exit 1
