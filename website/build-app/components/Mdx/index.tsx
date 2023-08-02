29cdd2e77482182569c22dd4f7d0ed3f0edd074b
theojungle <mesniltheo@gmail.com>
2023-08-02 17:34:58 +0200

    chore: revert base of new website documentation

--- website/build-app/components/Mdx/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-
-const markdown = `Just a link: https://reactjs.com.`
-
-export function Mdx({ children = markdown }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
-}
