b71b181bafed7d9c0b112013624ef84108a2dc12
isaacmaamouche <maamouche.isaac@gmail.com>
2023-08-08 17:24:24 +0200

    fix: handle NaN value

--- packages/Tabs/src/utils.ts
+++ packages/Tabs/src/utils.ts
@@ -1,8 +1,8 @@
 export const getParentScale = (element: HTMLElement | null): number => {
   if (!element) return 1
-  const elementScale = window.getComputedStyle(element).scale
-  if (elementScale !== 'none') {
-    return Number(elementScale)
+  const elementScale = Number(window.getComputedStyle(element).scale)
+  if (!isNaN(elementScale)) {
+    return elementScale
   } else {
     return getParentScale(element.parentElement)
   }
