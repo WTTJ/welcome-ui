8ca83870ddc56eeed6742033e7556988c005f221
theojungle <mesniltheo@gmail.com>
2023-08-09 17:39:25 +0200

    docs: add sidebar navigation

---
+++ website/build-app/components/Sidebar/index.tsx
@@ -0,0 +1,35 @@
+import { ResultsPageTree } from '@/build-app/utils/pages'
+import startCase from 'lodash/startCase'
+import Link from 'next/link'
+
+type SidebarProps = {
+  pages: ResultsPageTree
+  pathToPages: string
+}
+
+export function Sidebar({ pages, pathToPages }: SidebarProps) {
+  return (
+    <ul>
+      {Object.keys(pages).map((entry: string) => (
+        <>
+          {Array.isArray(pages[entry]) ? (
+            <li>
+              {startCase(entry)}
+              <ul>
+                {Array.from(pages[entry]).map(page => (
+                  <li>
+                    <Link href={`${pathToPages}/${entry}/${page}`}>{startCase(page)}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ) : (
+            <li>
+              <Link href={`${pathToPages}/${entry}`}>{startCase(entry)}</Link>
+            </li>
+          )}
+        </>
+      ))}
+    </ul>
+  )
+}
