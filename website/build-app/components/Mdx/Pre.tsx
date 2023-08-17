4ebbd195c9d17192acd04b0dcffe863c19834725
theojungle <mesniltheo@gmail.com>
2023-08-17 21:36:44 +0200

    docs: add code mirror

---
+++ website/build-app/components/Mdx/Pre.tsx
@@ -0,0 +1,27 @@
+'use client'
+import CodeMirror from '@uiw/react-codemirror'
+import { javascript } from '@codemirror/lang-javascript'
+import * as React from 'react'
+
+interface PreProps {
+  children: React.ReactNode[]
+}
+
+export function Pre({ children, ...rest }: PreProps) {
+  // @ts-ignore
+  const [code] = children[0].props.children
+  const [codeUpdated, setCodeUpdate] = React.useState(code)
+
+  const onChange = React.useCallback((newCode: React.ReactNode) => {
+    setCodeUpdate(newCode)
+  }, [])
+
+  return (
+    <CodeMirror
+      value={codeUpdated}
+      height="200px"
+      extensions={[javascript({ jsx: true, typescript: true })]}
+      onChange={onChange}
+    />
+  )
+}
