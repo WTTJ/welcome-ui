33408c0aa3a9037af40fd6c55a476e15a395e455
isaacmaamouche <maamouche.isaac@gmail.com>
2023-08-22 10:04:15 +0200

    fix: check if window is defined for ssr

--- packages/Tabs/src/utils.ts
+++ packages/Tabs/src/utils.ts
@@ -1,6 +1,11 @@
+//Check if the element, or its parent, has a scale property
 export const getParentScale = (element: HTMLElement | null): number => {
-  if (!element) return 1
+  //If the element doesn't exist, or if window is undefined, return 1
+  if (!element || typeof window === 'undefined') return 1
   const elementScale = Number(window.getComputedStyle(element).scale)
+
+  //If the scale property is not unvalid or undefined, return its value
+  //Else check the parent's scale property
   if (!isNaN(elementScale)) {
     return elementScale
   } else {
