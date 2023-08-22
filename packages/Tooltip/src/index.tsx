6ab5463fe8b3c8d09f207178c3db86047c0ad02c
Mickaël Le Ralec <mickael.leralec@gmail.com>
2023-08-22 09:33:07 +0200

    fix: missing system props on ariakit tooltip

--- packages/Tooltip/src/index.tsx
+++ packages/Tooltip/src/index.tsx
@@ -1,73 +1,79 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import * as Ariakit from '@ariakit/react'
+import { Box } from '@welcome-ui/box'
+import { CreateWuiProps, forwardRef } from '@welcome-ui/system'
 
 import * as S from './styles'
 
-export type TooltipProps = {
+export type TooltipOptions = {
   children: React.ReactNode
   content: string | JSX.Element
   fixed?: boolean
   placement?: Ariakit.TooltipStoreProps['placement']
 }
 
-export const Tooltip = ({
-  children,
-  content,
-  fixed = false,
-  placement = fixed ? 'top' : 'bottom',
-}: TooltipProps): React.ReactElement => {
-  const tooltip = Ariakit.useTooltipStore({ placement, animated: true })
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-  const getState = tooltip.getState
-  const render = tooltip.render
+export type TooltipProps = CreateWuiProps<'div', TooltipOptions>
 
-  const updatePosition = () => {
-    const { mounted, popoverElement } = getState()
+export const Tooltip = forwardRef<'div', TooltipProps>(
+  ({ children, content, fixed = false, placement = fixed ? 'top' : 'bottom', ...rest }, ref) => {
+    const tooltip = Ariakit.useTooltipStore({ placement, animated: true })
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    const getState = tooltip.getState
+    const render = tooltip.render
 
-    if (!popoverElement) return
+    const updatePosition = () => {
+      const { mounted, popoverElement } = getState()
 
-    Object.assign(popoverElement.style, {
-      display: mounted ? 'block' : 'none',
-      position: 'absolute',
-      left: `${position.x}px`,
-      top: `${position.y + window.scrollY + 20}px`,
-    })
-  }
+      if (!popoverElement) return
 
-  const child = (children as JSX.Element)?.props?.disabled
-    ? React.Children.only(<S.ChildItem>{children}</S.ChildItem>)
-    : children
-
-  useEffect(() => {
-    function onMouseMove({ clientX, clientY }: { clientX: number; clientY: number }) {
-      setPosition({ x: clientX, y: clientY })
-      render()
+      Object.assign(popoverElement.style, {
+        display: mounted ? 'block' : 'none',
+        position: 'absolute',
+        left: `${position.x}px`,
+        top: `${position.y + window.scrollY + 20}px`,
+      })
     }
 
-    const { anchorElement } = getState()
+    const child = (children as JSX.Element)?.props?.disabled
+      ? React.Children.only(<S.ChildItem>{children}</S.ChildItem>)
+      : children
 
-    if (anchorElement && !fixed) {
-      anchorElement.addEventListener('mousemove', onMouseMove)
-
-      return () => {
-        anchorElement.removeEventListener('mousemove', onMouseMove)
+    useEffect(() => {
+      function onMouseMove({ clientX, clientY }: { clientX: number; clientY: number }) {
+        setPosition({ x: clientX, y: clientY })
+        render()
       }
+
+      const { anchorElement } = getState()
+
+      if (anchorElement && !fixed) {
+        anchorElement.addEventListener('mousemove', onMouseMove)
+
+        return () => {
+          anchorElement.removeEventListener('mousemove', onMouseMove)
+        }
+      }
+    }, [render, fixed, getState])
+
+    // If no content, simply return the children
+    if (!content) {
+      return children as React.ReactElement
     }
-  }, [render, fixed, getState])
 
-  // If no content, simply return the children
-  if (!content) {
-    return children as React.ReactElement
+    return (
+      <>
+        <Ariakit.TooltipAnchor render={child as ReactElement} store={tooltip} />
+        <Ariakit.Tooltip
+          ref={ref}
+          render={<Box {...rest} />}
+          store={tooltip}
+          updatePosition={!fixed ? updatePosition : undefined}
+        >
+          <S.FadeIn fixed={fixed} placement={placement}>
+            {content}
+          </S.FadeIn>
+        </Ariakit.Tooltip>
+      </>
+    )
   }
-
-  return (
-    <>
-      <Ariakit.TooltipAnchor render={child as ReactElement} store={tooltip} />
-      <Ariakit.Tooltip store={tooltip} updatePosition={!fixed ? updatePosition : undefined}>
-        <S.FadeIn fixed={fixed} placement={placement}>
-          {content}
-        </S.FadeIn>
-      </Ariakit.Tooltip>
-    </>
-  )
-}
+)
