6e666ac6cc21011b7c5078a6d7c98ae3ad2fc1f7
François Robichet <Calvein@users.noreply.github.com>
2023-09-13 10:29:30 +0200

    Use theme spaces for DropdownMenu gutter (#2205)
    * chore: use wui theme spaces for gutter
    
    * refactor: add default gutter space in theme
    
    * refactor: use xs as default gutter
    
    * chore: use spaces from theme
    
    * test: add tests for gutter

--- packages/DropdownMenu/src/index.tsx
+++ packages/DropdownMenu/src/index.tsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import * as Ariakit from '@ariakit/react'
 import { CreateWuiProps, forwardRef, WuiProps } from '@welcome-ui/system'
+import { useTheme } from '@xstyled/styled-components'
+import type { WuiTheme } from '@welcome-ui/core'
 
 import { Arrow } from './Arrow'
 import { Item } from './Item'
 import { Separator } from './Separator'
 import * as S from './styles'
 
-export interface DropdownMenuOptions extends Ariakit.MenuProps {
+export interface DropdownMenuOptions extends Omit<Ariakit.MenuProps, 'gutter'> {
   /** add custom props from styled system on DropdownMenu inner */
   innerProps?: WuiProps
+  /** default 4px (space.xs) */
+  gutter?: keyof WuiTheme['space'] | number
 }
 
 export type DropdownMenuProps = CreateWuiProps<'div', DropdownMenuOptions>
 
 const DropdownMenuComponent = forwardRef<'div', DropdownMenuProps>(
-  ({ children, dataTestId, innerProps = {}, store, gutter = 10, ...rest }, ref) => {
+  ({ children, dataTestId, innerProps = {}, store, gutter = 'xs', ...rest }, ref) => {
+    const theme = useTheme()
     const arrowElement = store.useState('arrowElement')
     const isOpen = store.useState('open')
+
+    let parsedGutter = gutter
+    if (typeof parsedGutter === 'string') {
+      // The value from the theme is in rem, e.g: 1.5rem
+      // So we parse it to float and pass it to theme.toPx that will convert it to px, e.g: 24px
+      // Since we want a number we parse it to int
+      parsedGutter = parseInt(theme.toPx(parseFloat(theme.space[gutter])), 10) || 0
+    }
+    if (arrowElement) {
+      parsedGutter = 0
+    }
 
     return (
       isOpen && (
@@ -25,7 +41,7 @@ const DropdownMenuComponent = forwardRef<'div', DropdownMenuProps>(
           alwaysVisible
           aria-label="dropdown-menu"
           data-testid={dataTestId}
-          gutter={arrowElement ? 0 : gutter}
+          gutter={parsedGutter}
           ref={ref}
           render={<S.Inner {...innerProps} />}
           store={store}
