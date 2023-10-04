128cd70927c2f65d8d2cdb1d9dfde85d51dcc2bf
theojungle <mesniltheo@gmail.com>
2023-10-04 18:42:07 +0200

    feat: add pages from supernova components

--- website/build-app/components/Mdx/Paragraph.tsx
+++ website/build-app/components/Mdx/Paragraph.tsx
@@ -6,9 +6,9 @@ import { Tokens } from './Tokens'
 
 export const Paragraph = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   //@ts-ignore
-  const isDoDont = !!children?.props?.children === 'Do' || !!children?.props?.children === "Don't"
+  const isDoDont = children?.props?.children === 'Do' || children?.props?.children === "Don't"
   //@ts-ignore
-  const isToken = !!children?.[0]?.props?.children?.startsWith('Token Group')
+  const isToken = children?.[0]?.props?.children?.startsWith('Token Group')
 
   if (isDoDont) return null
 
