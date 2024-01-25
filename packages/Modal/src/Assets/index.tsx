a02332659a58b6231615b81e7f94123d1dac5042
theojungle <mesniltheo@gmail.com>
2024-01-25 16:24:27 +0100

    feat: add news AssetModal components for picture, video and swiper Modal

---
+++ packages/Modal/src/Assets/index.tsx
@@ -0,0 +1,12 @@
+import { AssetWithTitle } from './AssetWithTitle'
+import { Backdrop } from './Backdrop'
+import { Iframe } from './Iframe'
+import { Content, Dialog } from './styles'
+
+// Nested exports
+export const Assets = Object.assign(Dialog, {
+  AssetWithTitle,
+  Backdrop,
+  Content,
+  Iframe,
+})
