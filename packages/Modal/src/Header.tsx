a67fdd128a4939d76122797d361c5cf56185ba1a
Mickaël Le Ralec <mickael.leralec@gmail.com>
2024-03-14 10:03:52 +0100

    fix: rollback modal header a11y

--- packages/Modal/src/Header.tsx
+++ packages/Modal/src/Header.tsx
@@ -22,13 +22,9 @@ export const Header = forwardRef<'div', HeaderProps>(({ icon, subtitle, title, .
     <S.Header ref={ref} textAlign={icon ? 'center' : null} w="100%" {...rest}>
       <Close isOnHeader />
       {icon}
-      {typeof title === 'string' ? (
-        <Text mb={subtitle ? 'lg' : 0} mt={icon ? 'xl' : 0} variant="h4">
-          {title}
-        </Text>
-      ) : (
-        title
-      )}
+      <Text mb={subtitle ? 'lg' : 0} mt={icon ? 'xl' : 0} variant="h4">
+        {title}
+      </Text>
       {subtitle && <S.HeaderSubtitle>{subtitle}</S.HeaderSubtitle>}
     </S.Header>
   )
