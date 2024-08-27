86523d7d4ab40aa17258fc237688bc0f9c413583
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2024-08-27 15:56:16 +0200

    chore: upgrade lerna to v8 (#2523)
    * chore: upgrade lerna to v8
    
    * v5.20.0-alpha.0

--- packages/Core/tests/index.test.tsx
+++ packages/Core/tests/index.test.tsx
@@ -1,4 +1,3 @@
-import { welcomeTheme } from '../../Themes/Welcome'
 import { createTheme } from '../src/theme/core'
 import { colors } from '../src/theme/colors'
 
@@ -8,15 +7,8 @@ test('core theme has the correct color for buttons.primary.color', () => {
   expect(theme.buttons.primary.color).toBe(colors['dark-900'])
 })
 
-test('welcomeTheme correctly overwrites core theme.buttons with its custom styles', () => {
-  const theme = createTheme(welcomeTheme)
-
-  expect(theme.buttons.primary.color).toBe('rgba(0, 0, 0, 1)')
-})
-
 test('Can overwrite welcomeTheme.buttons with custom button styles', () => {
   const theme = createTheme({
-    ...welcomeTheme,
     buttons: {
       primary: {
         color: 'red',
