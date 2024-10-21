dbb55e2fb1116eebc9a788c13f22e494880ba7b1
theojungle <mesniltheo@gmail.com>
2024-10-21 16:49:33 +0200

    feat: remove welcome theme from being the default

--- website/build-app/components/ThemeProvider/index.tsx
+++ website/build-app/components/ThemeProvider/index.tsx
@@ -1,17 +1,53 @@
 'use client'
-import { createTheme, WuiProvider, WuiProviderProps } from '@welcome-ui/core'
-import { welcomeTheme } from '@welcome-ui/themes.welcome'
+import { createTheme, darkTheme, WuiProvider, WuiProviderProps } from '@welcome-ui/core'
 import * as React from 'react'
 
 type ThemeProviderProps = {
   children: WuiProviderProps['children']
 }
+type ThemeProps = {
+  children: WuiProviderProps['children']
+}
+type ThemeValue = 'light' | 'dark'
+type ContextReturn = {
+  setTheme: (theme: ThemeValue) => void
+  theme: ThemeValue
+}
 
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const theme = React.useMemo(() => createTheme(welcomeTheme), [])
+export const ThemeContext = React.createContext<ContextReturn>({
+  theme: 'light',
+  setTheme: () => {
+    return 'light'
+  },
+})
+
+export const Theme = ({ children }: ThemeProps) => {
+  const [theme, setTheme] = React.useState<ThemeValue>('light')
+
+  React.useEffect(() => {
+    setTheme((localStorage.getItem('wui-theme') as ThemeValue) || 'light')
+  }, [])
+
+  const values = React.useMemo(() => ({ theme, setTheme }), [theme])
 
   return (
-    <WuiProvider theme={theme} useReset>
+    <ThemeContext.Provider value={values}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const { theme } = React.useContext(ThemeContext)
+  const isDarkTheme = theme === 'dark'
+
+  const themeValues = React.useMemo(
+    () => createTheme(isDarkTheme ? darkTheme : undefined),
+    [isDarkTheme]
+  )
+
+  return (
+    <WuiProvider theme={themeValues} useReset>
       {children}
     </WuiProvider>
   )
