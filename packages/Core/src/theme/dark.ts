673c0f4186d73b48004a83b7ef0fde8f3675e771
theojungle <mesniltheo@gmail.com>
2024-10-21 16:49:33 +0200

    feat: remove welcome theme from being the default

---
+++ packages/Core/src/theme/dark.ts
@@ -0,0 +1,15 @@
+import { WuiTheme } from './types'
+import { colors } from './colors'
+
+type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] | RecursivePartial<T[P]>
+}
+
+export const colorsDark: WuiTheme['colors'] = {
+  ...colors,
+  'primary-500': '#FF0000',
+}
+
+export const darkTheme: RecursivePartial<WuiTheme> = {
+  colors: colorsDark,
+}
