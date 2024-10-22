4047ed6fcc01c77d652a32c3d1c725efdb4cbfc0
theojungle <mesniltheo@gmail.com>
2024-10-22 17:31:35 +0200

    feat: add dark mode

--- packages/Core/src/theme/dark.ts
+++ packages/Core/src/theme/dark.ts
@@ -1,13 +1,36 @@
 import { WuiTheme } from './types'
-import { colors } from './colors'
+import { colors, getColors } from './colors'
 
 type RecursivePartial<T> = {
   [P in keyof T]?: T[P] | RecursivePartial<T[P]>
 }
 
+/**
+ * We reverse the palette colors (basic token color) and inject this new values on dark theme
+ */
+const generateDarkColors = Object.keys(colors).reduce((acc, key) => {
+  if (
+    key.startsWith('secondary-') ||
+    key.startsWith('danger-') ||
+    key.startsWith('warning-') ||
+    key.startsWith('success-') ||
+    key.startsWith('info-') ||
+    key === 'overlay'
+  )
+    return acc
+
+  const number = 100 - Number(key.slice(-2))
+  const variant = key.slice(0, key.length - 2)
+
+  return {
+    ...acc,
+    [key]: colors[`${variant}${number}` as keyof WuiTheme['colors']],
+  }
+}, {})
+
 export const colorsDark: WuiTheme['colors'] = {
   ...colors,
-  'primary-50': '#FF0000',
+  ...getColors(generateDarkColors as WuiTheme['colors']),
 }
 
 export const darkTheme: RecursivePartial<WuiTheme> = {
