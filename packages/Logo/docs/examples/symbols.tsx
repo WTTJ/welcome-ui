980dacbd902600f213cfc61e249652d229b79792
theojungle <mesniltheo@gmail.com>
2024-10-23 11:43:50 +0200

    feat: rework logos

--- packages/Logo/docs/examples/symbols.tsx
+++ packages/Logo/docs/examples/symbols.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react'
-import { Symbol, SymbolYellow } from '@welcome-ui/logo'
+import { Symbol } from '@welcome-ui/logo'
 
 const Example = () => {
-  return (
-    <>
-      <Symbol h={100} />
-      <SymbolYellow h={100} />
-    </>
-  )
+  return <Symbol h={100} />
 }
 
 export default Example
