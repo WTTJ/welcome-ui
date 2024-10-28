6e48d8b2c92ef31e7c368188be29a45e2c17b3dc
Paul-Xavier Ceccaldi <pix@wttj.co>
2024-10-28 14:21:20 +0100

    fix: avoid welcome-font cut letters (#2561)
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- packages/Text/src/styles.ts
+++ packages/Text/src/styles.ts
@@ -11,6 +11,15 @@ const MOBILE_VARIANTS = {
   h5: 'h6',
   h6: 'h6',
 }
+const headingsLineHeightFixer = {
+  h0: 0.825,
+  h1: 0.75,
+  h2: 0.825,
+  h3: 1.075,
+  h4: 1,
+  h5: 0.9,
+  h6: 0.9,
+}
 
 const getBlockHeight = (lines: number) => css`
   /* stylelint-disable-next-line value-no-vendor-prefix */
@@ -24,9 +33,25 @@ const getBlockHeight = (lines: number) => css`
   word-break: ${lines === 1 ? 'break-all' : null};
 `
 
-export const Text = styled.p<TextOptions>(({ lines, variant, withDash }) => {
+const fixHeadingsLineHeightStyles = (variant: TextOptions['variant']) => css`
+  & {
+    line-height: ${headingsLineHeightFixer[variant as keyof typeof headingsLineHeightFixer]};
+  }
+  &::before {
+    content: '';
+    vertical-align: text-bottom;
+  }
+`
+
+export const Text = styled.p<TextOptions>(({ lines, theme, variant, withDash }) => {
   const mobileVariant = MOBILE_VARIANTS[variant as keyof typeof MOBILE_VARIANTS]
   const isHeading = variant?.startsWith('h')
+  // only add lineHeight fixer styles when these concditions are fulfilled
+  const shouldFixHeadingsLineHeight =
+    lines &&
+    lines !== Infinity &&
+    Object.keys(headingsLineHeightFixer).includes(variant) &&
+    theme.fonts.headings.includes('welcome-font')
 
   return css`
     ${th(`texts.${mobileVariant || variant}`)};
@@ -57,6 +82,8 @@ export const Text = styled.p<TextOptions>(({ lines, variant, withDash }) => {
       ${th(`texts.${variant}`)};
       ${system};
     }
+
+    ${shouldFixHeadingsLineHeight && fixHeadingsLineHeightStyles(variant)};
 
     ${system};
   `
