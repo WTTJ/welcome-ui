01f19c6fcb97fe189445cd9150c3675025ade2d4
theojungle <mesniltheo@gmail.com>
2024-11-04 18:30:43 +0100

    docs: fix wrong avatar name

--- packages/Avatar/docs/examples/texts.tsx
+++ packages/Avatar/docs/examples/texts.tsx
@@ -4,11 +4,11 @@ import { Avatar } from '@welcome-ui/avatar'
 const Example = () => {
   return (
     <>
-      <Avatar name="Welcome jungle" size="sm" />
-      <Avatar name="Welcome t jungle" />
-      <Avatar name="Welcome to jungle" size="lg" />
-      <Avatar name="Welcome to t Jungle" size="xl" />
-      <Avatar name="Welcome to the jungle" size="xxl" />
+      <Avatar name="Welcome to the Jungle" size="sm" />
+      <Avatar name="Welcome to the Jungle" />
+      <Avatar name="Welcome to the Jungle" size="lg" />
+      <Avatar name="Welcome to the Jungle" size="xl" />
+      <Avatar name="Welcome to the Jungle" size="xxl" />
       <Avatar color="orange-50" fontSize={20} h={130} name="Custom" w={130} />
     </>
   )
