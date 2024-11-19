2a71f059ba2fa8381110a8bf4f6f65837f0983d7
Robert Douglas <rob@anucreative.com>
2024-11-19 09:39:00 +0100

    test: add test for selecting dropdown menu item (#2612)

--- packages/DropdownMenu/tests/index.test.tsx
+++ packages/DropdownMenu/tests/index.test.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fireEvent, renderHook, screen, waitFor } from '@testing-library/react'
 
 import { render } from '../../../utils/tests'
-import { DropdownMenu, useDropdownMenu } from '../src'
+import { DropdownMenu, type DropdownMenuProps, useDropdownMenu } from '../src'
 
 const content = 'jungle'
+const triggerDataTestId = 'trigger'
+const dropdownDataTestId = 'menu'
+
+const options = [
+  { value: 1, label: 'one' },
+  { value: 2, label: 'two' },
+  { value: 3, label: 'three' },
+  { value: 4, label: 'four' },
+  { value: 5, label: 'five' },
+]
+
+const DropdownRenderer: React.FC<DropdownMenuProps> = () => {
+  const dropdownMenu = useDropdownMenu()
+
+  const [selected, setSelected] = useState('one')
+
+  const handleChange = (value: string) => {
+    setSelected(value)
+  }
+
+  return (
+    <>
+      <DropdownMenu.Trigger
+        data-testid={triggerDataTestId}
+        id={triggerDataTestId}
+        store={dropdownMenu}
+      >
+        {selected}
+      </DropdownMenu.Trigger>
+      <DropdownMenu dataTestId={dropdownDataTestId} id={dropdownDataTestId} store={dropdownMenu}>
+        {options.map(item => (
+          <DropdownMenu.Item
+            id={item.label}
+            key={item.label}
+            onClick={() => handleChange(item.label)}
+          >
+            {item.label}
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu>
+    </>
+  )
+}
 
 describe('<DropdownMenu>', () => {
-  it('should render correctly', () => {
+  test('should render correctly', () => {
     const dataTestId = 'dropdownMenu'
     const {
       result: { current: dropdownMenu },
@@ -22,6 +65,21 @@ describe('<DropdownMenu>', () => {
     const dropdown = screen.getByTestId(dataTestId)
 
     expect(dropdown).toHaveTextContent(content)
+  })
+
+  test('should handle click item', async () => {
+    render(<DropdownRenderer />)
+
+    const trigger = screen.getByTestId(triggerDataTestId)
+    await fireEvent.click(trigger)
+
+    const dropdownMenuItemToDropdown = screen.getByRole('menuitem', { name: 'four' })
+    expect(dropdownMenuItemToDropdown).toBeInTheDocument()
+
+    // Why fireEvent ? https://github.com/testing-library/user-event/discussions/1156
+    await fireEvent.click(dropdownMenuItemToDropdown)
+
+    expect(trigger).toHaveTextContent('four')
   })
 
   test.each([
