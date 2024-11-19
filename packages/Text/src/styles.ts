1ad7f350531b68d267799a919d2fd6e2f191b778
theojungle <mesniltheo@gmail.com>
2024-11-19 10:22:05 +0100

    fix: truncate Text with welcome font

--- packages/Text/src/styles.ts
+++ packages/Text/src/styles.ts
@@ -11,15 +11,6 @@ const MOBILE_VARIANTS = {
   h5: 'h6',
   h6: 'h6',
 }
-const headingsLineHeightFixer = {
-  h0: 0.825,
-  h1: 0.75,
-  h2: 0.825,
-  h3: 1.075,
-  h4: 1,
-  h5: 0.9,
-  h6: 0.9,
-}
 
 const getBlockHeight = (lines: number) => css`
   /* stylelint-disable-next-line value-no-vendor-prefix */
@@ -33,24 +24,14 @@ const getBlockHeight = (lines: number) => css`
   word-break: ${lines === 1 ? 'break-all' : null};
 `
 
-const fixHeadingsLineHeightStyles = (variant: TextOptions['variant']) => css`
-  & {
-    line-height: ${headingsLineHeightFixer[variant as keyof typeof headingsLineHeightFixer]};
-  }
-  &::before {
-    content: '';
-    vertical-align: text-bottom;
-  }
-`
-
 export const Text = styled.p<TextOptions>(({ lines, theme, variant, withDash }) => {
   const mobileVariant = MOBILE_VARIANTS[variant as keyof typeof MOBILE_VARIANTS]
   const isHeading = variant?.startsWith('h')
-  // only add lineHeight fixer styles when these concditions are fulfilled
+  // only add lineHeight fixer styles when these conditions are fulfilled
   const shouldFixHeadingsLineHeight =
     lines &&
     lines !== Infinity &&
-    Object.keys(headingsLineHeightFixer).includes(variant) &&
+    variant.startsWith('h') &&
     theme.fonts.headings.includes('welcome-font')
 
   return css`
@@ -83,7 +64,12 @@ export const Text = styled.p<TextOptions>(({ lines, theme, variant, withDash }) 
       ${system};
     }
 
-    ${shouldFixHeadingsLineHeight && fixHeadingsLineHeightStyles(variant)};
+    ${shouldFixHeadingsLineHeight &&
+    css`
+      & {
+        line-height: 1.4;
+      }
+    `}
 
     ${system};
   `
