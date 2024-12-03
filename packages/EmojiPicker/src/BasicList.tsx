e885b9808d2869ae60fe5cf6f7798abdb8f3a423
Steven Persia <persia.steven8@gmail.com>
2024-12-03 11:03:21 +0100

    chore(emoji-picker): remove EmojiPicker component

--- packages/EmojiPicker/src/BasicList.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { List, ListProps } from './List'
-import basicEmojis from './basicEmojis.json'
-
-export const BasicList: React.FC<ListProps> = props => {
-  return <List emojis={basicEmojis} {...props} />
-}
