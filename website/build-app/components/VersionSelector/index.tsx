b7205de722e3d521e5c284764a9b71e7168148cb
Théo Mesnil <mesniltheo@gmail.com>
2025-01-29 10:16:01 +0100

    feat: add old website v6

--- website/build-app/components/VersionSelector/index.tsx
+++ website/build-app/components/VersionSelector/index.tsx
@@ -5,7 +5,8 @@ import lib from '../../../../lib/package.json'
 import { Select } from '@/Select'
 
 const versions = [
-  { value: 'v6', label: `${lib.version}` },
+  { value: 'v7', label: `${lib.version}` },
+  { value: 'v6', label: 'v6' },
   { value: 'v5', label: 'v5' },
   { value: 'v4', label: 'v4' },
   { value: 'v3', label: 'v3' },
@@ -13,9 +14,8 @@ const versions = [
 
 export const VersionSelector = () => {
   const handleChange = (value: unknown) => {
-    if (['v3', 'v4', 'v5'].includes(value as string)) {
-      window.open(`http://welcome-ui.com/${value}`, '_self')
-    }
+    if (value === 'v7') return
+    window.open(`http://welcome-ui.com/${value}`, '_self')
   }
 
   return (
@@ -25,7 +25,7 @@ export const VersionSelector = () => {
       onChange={handleChange}
       options={versions}
       size="sm"
-      value="v6"
+      value="v7"
     />
   )
 }
