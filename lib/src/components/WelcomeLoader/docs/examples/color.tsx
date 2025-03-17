7a5fb85670bc45ea2f8501ca8c9e12e276ad7000
Robert Douglas <rob@anucreative.com>
2025-03-17 10:50:27 +0100

    chore: only allow black or white for WelcomeLoader

--- lib/src/components/WelcomeLoader/docs/examples/color.tsx
+++ lib/src/components/WelcomeLoader/docs/examples/color.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
 
 import { WelcomeLoader } from '@/WelcomeLoader'
+import { Box } from '@/Box'
 
 const Example = () => {
-  return <WelcomeLoader color="neutral-90" />
+  return (
+    <Box display="flex">
+      <Box backgroundColor="neutral-20" borderRadius="lg">
+        <WelcomeLoader color="black" />
+      </Box>
+      <Box backgroundColor="neutral-90" borderRadius="lg">
+        <WelcomeLoader color="white" />
+      </Box>
+    </Box>
+  )
 }
 
 export default Example
--- lib/src/components/WelcomeLoader/index.tsx
+++ lib/src/components/WelcomeLoader/index.tsx
@@ -1,35 +1,34 @@
 import React from 'react'
 import { useLottie } from 'lottie-light-react'
-import { useTheme } from 'styled-components'
 
 import loader from './loader.json'
-import { convertHexToVectorColor } from './utils'
 
 import { forwardRef } from '@/System'
 import { Box, BoxProps } from '@/Box'
 
+type ValidBrandColor = 'black' | 'white'
+export type WelcomeLoaderProps = BoxProps & { color?: ValidBrandColor }
+
 const currentColorRegex = new RegExp('1,0.803921568627,0,1', 'g')
 
-export const WelcomeLoader = forwardRef<'div', BoxProps>(({ color, w = 150, ...props }, ref) => {
-  const theme = useTheme()
-  let animationData = loader
+export const WelcomeLoader = forwardRef<'div', WelcomeLoaderProps>(
+  ({ color, w = 150, ...props }, ref) => {
+    let animationData = loader
 
-  if (color) {
-    const hex = theme.colors[color as keyof typeof theme.colors]
-    const newColor = convertHexToVectorColor(hex)
-    if (newColor) {
+    if (color && (color === 'black' || color === 'white')) {
+      const newColor = color === 'black' ? '0,0,0' : '1,1,1'
       const updated = JSON.stringify(loader, null, 0).replace(currentColorRegex, newColor)
       animationData = JSON.parse(updated)
     }
+
+    const { View } = useLottie({ animationData, loop: true })
+
+    return (
+      <Box ref={ref} w={w} {...props}>
+        {View}
+      </Box>
+    )
   }
-
-  const { View } = useLottie({ animationData, loop: true })
-
-  return (
-    <Box ref={ref} w={w} {...props}>
-      {View}
-    </Box>
-  )
-})
+)
 
 WelcomeLoader.displayName = 'WelcomeLoader'
