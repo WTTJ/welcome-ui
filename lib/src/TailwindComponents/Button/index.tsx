6ac1654dce9871fde09bca11a7c85c978c473903
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-08-06 18:43:55 +0200

    chore: add variants sizes and shape to button
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Camille Nairi<camille.nairi@wttj.co>

--- lib/src/TailwindComponents/Button/index.tsx
+++ lib/src/TailwindComponents/Button/index.tsx
@@ -1,16 +1,38 @@
+import type { CSSProperties } from 'react'
 import { forwardRef } from 'react'
+
+import { shapes, sizes, theme, variableMap, variants } from './theme'
 
 type ButtonProps = {
   children?: React.ReactNode
   className?: string
+  shape?: 'circle' | 'square'
+  size: 'lg' | 'md' | 'sm'
+  style?: CSSProperties
+  variant?: 'disabled' | 'ghost' | 'primary' | 'secondary' | 'tertiary'
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, className }, ref) => {
-  return (
-    <button className={className} ref={ref}>
-      {children || 'Test button tailwind'}
-    </button>
-  )
-})
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, shape = 'square', size = 'md', style, variant = 'primary' }, ref) => {
+    const base =
+      'relative inline-flex items-center justify-center text-center cursor-pointer transition-all'
+    const dynamicClasses =
+      'bg-color(--backgroundColor) border-color(--borderColor) px-(--paddingX) py(--paddingY) border-radius(--borderRadius)'
+    const classNames = `${base} ${dynamicClasses} ${className}`
+    const styles = {
+      ...variableMap(theme),
+      ...variableMap(variants[variant as keyof typeof variants]),
+      ...variableMap(sizes[size as keyof typeof sizes]),
+      ...variableMap(shapes[shape as keyof typeof shapes]),
+      ...style,
+    }
+
+    return (
+      <button className={classNames} ref={ref} style={{ ...styles } as CSSProperties} type="button">
+        {children || 'Test button tailwind'}
+      </button>
+    )
+  }
+)
 
 Button.displayName = 'TailwindButton'
