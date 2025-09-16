065aa35ded6d032c2e5f2ab0a0cc59fcde3d525a
diez-wttj <zeid.fazazi@wttj.co>
2025-09-16 18:09:33 +0200

    feat: add Loader component

---
+++ lib/src/components/Loader/docs/examples/color.tsx
@@ -0,0 +1,7 @@
+import { Loader } from '../../index'
+
+const Example = () => {
+  return <Loader color="--color-yellow-40" />
+}
+
+export default Example
---
+++ lib/src/components/Loader/docs/examples/sizes.tsx
@@ -0,0 +1,18 @@
+import { Stack } from '@old/Stack'
+
+import { Loader } from '../../index'
+
+const Example = () => {
+  return (
+    <Stack spacing="xl">
+      <Loader size="xs" />
+      <Loader />
+      <Loader size="md" />
+      <Loader size="lg" />
+      <Loader size="40px" />
+      <Loader size={50} />
+    </Stack>
+  )
+}
+
+export default Example
