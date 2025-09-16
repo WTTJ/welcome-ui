269b21f478fcdad7e75e46f8916431fd01f3b730
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-09-16 11:42:18 +0200

    fix: allow tailwind css class overrides and add according doc (#2827)
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Robert Douglas <robert@wttj.co>

--- lib/src/components/Button/docs/examples/variants.tsx
+++ lib/src/components/Button/docs/examples/variants.tsx
@@ -4,9 +4,7 @@ const Example = () => {
   return (
     <>
       <Button>primary</Button>
-      <Button className="mt-3xl" variant="secondary">
-        secondary
-      </Button>
+      <Button variant="secondary">secondary</Button>
       <Button variant="tertiary">tertiary</Button>
       <Button variant="ghost">ghost</Button>
     </>
