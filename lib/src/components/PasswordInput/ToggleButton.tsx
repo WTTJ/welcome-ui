71ede41d210160919f4c7556408fea6ac7bc8f61
Robert Douglas <rob@anucreative.com>
2025-09-19 16:49:55 +0200

    chore: migrate PasswordInput

---
+++ lib/src/components/PasswordInput/ToggleButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Button } from '@/components/Button'
+import { HideIcon, ShowIcon } from '@/components/Icon'
+
+export type ToggleButtonProps = React.ComponentPropsWithoutRef<typeof Button> & {
+  isHidden: boolean
+}
+
+export const ToggleButton: React.FC<ToggleButtonProps> = ({
+  isHidden,
+  onClick,
+  title,
+  ...rest
+}) => {
+  return (
+    <Button
+      aria-controls="password"
+      aria-expanded={!isHidden}
+      onClick={onClick}
+      shape="circle"
+      size="xs"
+      title={title}
+      variant="ghost"
+      {...rest}
+    >
+      {isHidden ? <ShowIcon /> : <HideIcon />}
+    </Button>
+  )
+}
