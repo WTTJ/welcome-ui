9d3538ed79113327804a9a36d957db73c39e3762
Robert Douglas <rob@anucreative.com>
2025-09-19 16:26:18 +0200

    chore: migrate AspectRatio
    This reverts commit 8fb0669ce81b57cbb6bc826a9d5917442ecf2c4a.

---
+++ lib/src/components/AspectRatio/docs/examples/video.tsx
@@ -0,0 +1,15 @@
+import { AspectRatio } from '@/components/AspectRatio'
+
+const Example = () => {
+  return (
+    <AspectRatio ratio={16 / 9}>
+      <iframe
+        allowFullScreen
+        src="https://www.youtube.com/embed/jNQXAC9IVRw"
+        title="YouTube video player"
+      />
+    </AspectRatio>
+  )
+}
+
+export default Example
--- website/build-app/examples.js
+++ website/build-app/examples.js
@@ -3,211 +3,71 @@
 This file is auto-generate with yarn watch command, do not change it directly!
 **/
 
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
 export default {
-  '/Avatar/docs/examples/images.tsx': dynamic(
-    () => import('../../lib/src/components/Avatar/docs/examples/images.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Avatar/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Avatar/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Avatar/docs/examples/sizes.tsx': dynamic(
-    () => import('../../lib/src/components/Avatar/docs/examples/sizes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Avatar/docs/examples/texts.tsx': dynamic(
-    () => import('../../lib/src/components/Avatar/docs/examples/texts.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/ai.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/ai.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/danger.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/danger.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/disabled.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/disabled.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/icons.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/icons.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/is-loading.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/is-loading.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/link.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/link.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/shapes.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/shapes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/sizes.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/sizes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/tailwind-class-overrides.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/Button/docs/examples/tailwind-class-overrides.tsx').then(
-        mod => mod
-      ),
-    { ssr: false }
-  ),
-  '/Button/docs/examples/variants.tsx': dynamic(
-    () => import('../../lib/src/components/Button/docs/examples/variants.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/ButtonGroup/docs/examples/overload.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/ButtonGroup/docs/examples/overload.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/ButtonGroup/docs/examples/overview.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/ButtonGroup/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Hint/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Hint/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Hint/docs/examples/variants.tsx': dynamic(
-    () => import('../../lib/src/components/Hint/docs/examples/variants.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Icon/docs/examples/color.tsx': dynamic(
-    () => import('../../lib/src/components/Icon/docs/examples/color.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Icon/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Icon/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Icon/docs/examples/sizes.tsx': dynamic(
-    () => import('../../lib/src/components/Icon/docs/examples/sizes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/clearable.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/clearable.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/disabled.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/disabled.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/icon.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/icon.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/sizes.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/sizes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/transparent.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/InputText/docs/examples/transparent.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/InputText/docs/examples/variants.tsx': dynamic(
-    () => import('../../lib/src/components/InputText/docs/examples/variants.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Label/docs/examples/disabled.tsx': dynamic(
-    () => import('../../lib/src/components/Label/docs/examples/disabled.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Label/docs/examples/example.tsx': dynamic(
-    () => import('../../lib/src/components/Label/docs/examples/example.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Label/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Label/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Label/docs/examples/required.tsx': dynamic(
-    () => import('../../lib/src/components/Label/docs/examples/required.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Label/docs/examples/variants.tsx': dynamic(
-    () => import('../../lib/src/components/Label/docs/examples/variants.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/PasswordInput/docs/examples/overview.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/PasswordInput/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Text/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Text/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Text/docs/examples/overwrite.tsx': dynamic(
-    () => import('../../lib/src/components/Text/docs/examples/overwrite.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Text/docs/examples/truncation.tsx': dynamic(
-    () => import('../../lib/src/components/Text/docs/examples/truncation.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Text/docs/examples/withDash.tsx': dynamic(
-    () => import('../../lib/src/components/Text/docs/examples/withDash.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Textarea/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Textarea/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Textarea/docs/examples/variants.tsx': dynamic(
-    () => import('../../lib/src/components/Textarea/docs/examples/variants.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/hint.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/hint.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/icon.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/icon.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/label.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/label.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/overview.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/size.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/size.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/Toggle/docs/examples/variant.tsx': dynamic(
-    () => import('../../lib/src/components/Toggle/docs/examples/variant.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/VariantIcon/docs/examples/overview.tsx': dynamic(
-    () =>
-      import('../../lib/src/components/VariantIcon/docs/examples/overview.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-  '/VariantIcon/docs/examples/sizes.tsx': dynamic(
-    () => import('../../lib/src/components/VariantIcon/docs/examples/sizes.tsx').then(mod => mod),
-    { ssr: false }
-  ),
-}
+  "/AspectRatio/docs/examples/map.tsx": dynamic(() => import("../../lib/src/components/AspectRatio/docs/examples/map.tsx").then(mod => mod), { ssr: false }),
+  "/AspectRatio/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/AspectRatio/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/AspectRatio/docs/examples/ratio.tsx": dynamic(() => import("../../lib/src/components/AspectRatio/docs/examples/ratio.tsx").then(mod => mod), { ssr: false }),
+  "/AspectRatio/docs/examples/video.tsx": dynamic(() => import("../../lib/src/components/AspectRatio/docs/examples/video.tsx").then(mod => mod), { ssr: false }),
+  "/Avatar/docs/examples/images.tsx": dynamic(() => import("../../lib/src/components/Avatar/docs/examples/images.tsx").then(mod => mod), { ssr: false }),
+  "/Avatar/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Avatar/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Avatar/docs/examples/sizes.tsx": dynamic(() => import("../../lib/src/components/Avatar/docs/examples/sizes.tsx").then(mod => mod), { ssr: false }),
+  "/Avatar/docs/examples/texts.tsx": dynamic(() => import("../../lib/src/components/Avatar/docs/examples/texts.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/ai.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/ai.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/danger.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/danger.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/disabled.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/disabled.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/icons.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/icons.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/is-loading.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/is-loading.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/link.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/link.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/shapes.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/shapes.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/sizes.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/sizes.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/tailwind-class-overrides.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/tailwind-class-overrides.tsx").then(mod => mod), { ssr: false }),
+  "/Button/docs/examples/variants.tsx": dynamic(() => import("../../lib/src/components/Button/docs/examples/variants.tsx").then(mod => mod), { ssr: false }),
+  "/ButtonGroup/docs/examples/overload.tsx": dynamic(() => import("../../lib/src/components/ButtonGroup/docs/examples/overload.tsx").then(mod => mod), { ssr: false }),
+  "/ButtonGroup/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/ButtonGroup/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Hint/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Hint/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Hint/docs/examples/variants.tsx": dynamic(() => import("../../lib/src/components/Hint/docs/examples/variants.tsx").then(mod => mod), { ssr: false }),
+  "/Icon/docs/examples/color.tsx": dynamic(() => import("../../lib/src/components/Icon/docs/examples/color.tsx").then(mod => mod), { ssr: false }),
+  "/Icon/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Icon/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Icon/docs/examples/sizes.tsx": dynamic(() => import("../../lib/src/components/Icon/docs/examples/sizes.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/clearable.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/clearable.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/disabled.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/disabled.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/icon.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/icon.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/sizes.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/sizes.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/transparent.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/transparent.tsx").then(mod => mod), { ssr: false }),
+  "/InputText/docs/examples/variants.tsx": dynamic(() => import("../../lib/src/components/InputText/docs/examples/variants.tsx").then(mod => mod), { ssr: false }),
+  "/Label/docs/examples/disabled.tsx": dynamic(() => import("../../lib/src/components/Label/docs/examples/disabled.tsx").then(mod => mod), { ssr: false }),
+  "/Label/docs/examples/example.tsx": dynamic(() => import("../../lib/src/components/Label/docs/examples/example.tsx").then(mod => mod), { ssr: false }),
+  "/Label/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Label/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Label/docs/examples/required.tsx": dynamic(() => import("../../lib/src/components/Label/docs/examples/required.tsx").then(mod => mod), { ssr: false }),
+  "/Label/docs/examples/variants.tsx": dynamic(() => import("../../lib/src/components/Label/docs/examples/variants.tsx").then(mod => mod), { ssr: false }),
+  "/Pagination/docs/examples/custom-navigation.tsx": dynamic(() => import("../../lib/src/components/Pagination/docs/examples/custom-navigation.tsx").then(mod => mod), { ssr: false }),
+  "/Pagination/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Pagination/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Pagination/docs/examples/range.tsx": dynamic(() => import("../../lib/src/components/Pagination/docs/examples/range.tsx").then(mod => mod), { ssr: false }),
+  "/PasswordInput/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/PasswordInput/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Text/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Text/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Text/docs/examples/overwrite.tsx": dynamic(() => import("../../lib/src/components/Text/docs/examples/overwrite.tsx").then(mod => mod), { ssr: false }),
+  "/Text/docs/examples/truncation.tsx": dynamic(() => import("../../lib/src/components/Text/docs/examples/truncation.tsx").then(mod => mod), { ssr: false }),
+  "/Text/docs/examples/withDash.tsx": dynamic(() => import("../../lib/src/components/Text/docs/examples/withDash.tsx").then(mod => mod), { ssr: false }),
+  "/Textarea/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Textarea/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Textarea/docs/examples/variants.tsx": dynamic(() => import("../../lib/src/components/Textarea/docs/examples/variants.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/hint.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/hint.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/icon.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/icon.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/label.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/label.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/size.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/size.tsx").then(mod => mod), { ssr: false }),
+  "/Toggle/docs/examples/variant.tsx": dynamic(() => import("../../lib/src/components/Toggle/docs/examples/variant.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/disabled.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/disabled.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/fixed.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/fixed.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/long-text.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/long-text.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/placement.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/placement.tsx").then(mod => mod), { ssr: false }),
+  "/Tooltip/docs/examples/withArrow.tsx": dynamic(() => import("../../lib/src/components/Tooltip/docs/examples/withArrow.tsx").then(mod => mod), { ssr: false }),
+  "/VariantIcon/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/VariantIcon/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/VariantIcon/docs/examples/sizes.tsx": dynamic(() => import("../../lib/src/components/VariantIcon/docs/examples/sizes.tsx").then(mod => mod), { ssr: false }),
+  "/WelcomeLoader/docs/examples/overview.tsx": dynamic(() => import("../../lib/src/components/WelcomeLoader/docs/examples/overview.tsx").then(mod => mod), { ssr: false }),
+  "/WelcomeLoader/docs/examples/resize.tsx": dynamic(() => import("../../lib/src/components/WelcomeLoader/docs/examples/resize.tsx").then(mod => mod), { ssr: false })
+};
