f3bfda947c571401768b6649e0ee2182cff46007
diez-wttj <zeid.fazazi@wttj.co>
2025-09-19 19:27:28 +0200

    Chore migrate Logo

---
+++ lib/src/components/Logo/index.tsx
@@ -0,0 +1,6 @@
+import { SolutionsSymbol } from './Solutions'
+import { Symbol } from './Symbol'
+import { Welcome as Logo } from './Welcome'
+import { WelcomeUI as WelcomeUILogo } from './Wui'
+
+export { Logo, SolutionsSymbol, Symbol, WelcomeUILogo }
---
+++ lib/src/components/Logo/types.ts
@@ -0,0 +1,3 @@
+import type { BoxProps } from '@old/Box'
+
+export type LogoProps = Pick<BoxProps, 'h' | 'w'>
