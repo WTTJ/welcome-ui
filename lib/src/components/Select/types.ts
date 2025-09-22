138632024aad0c571580c150de5e0c5f491a1411
isaacmaamouche <maamouche.isaac@gmail.com>
2025-09-22 15:45:54 +0200

    chore: remove duplciate input prop

--- lib/src/components/Select/types.ts
+++ lib/src/components/Select/types.ts
@@ -18,27 +18,20 @@ export interface SelectOptions {
   allowUnselectFromList?: boolean
   /** We need to add `autoComplete` off to avoid select UI issues when is an input */
   autoComplete?: string
-  autoFocus?: boolean
   dataTestId?: string
   disableCloseOnSelect?: boolean
-  disabled?: boolean
   groupsEnabled?: boolean
   hasIcon?: boolean
   icon?: React.ReactElement
   iconPlacement?: 'both' | 'left' | 'right'
-  id?: string
   isClearable?: boolean
   isCreatable?: boolean
   isMultiple?: boolean
   isSearchable?: boolean
-  name?: string
-  onBlur?: () => void
   onChange?: (value: SelectOptionValue | SelectOptionValue[], event?: CreateEvent) => void
   onClick?: (event: React.MouseEvent<HTMLElement>) => void
   onCreate?: (option: string, event: CreateEvent) => void
-  onFocus?: () => void
   options: SelectOptionsType
-  placeholder?: string
   renderCreateItem?: (inputValue: SelectValue) => void
   renderGroupHeader?: (option: SelectOptionGroup) => React.ReactNode
   renderItem?: (item: SelectOption, isItemSelected?: boolean) => React.ReactElement | string
