abcd634b43ff811d0fb72e469c2c1797af8c5ef7
isaacmaamouche <maamouche.isaac@gmail.com>
2025-09-25 09:45:09 +0200

    chore: merge double iframe wrapper

--- lib/src/components/Modal/Assets/Iframe.tsx
+++ lib/src/components/Modal/Assets/Iframe.tsx
@@ -6,9 +6,5 @@ import type { IframeProps } from './types'
 const cx = classNames(modalStyles)
 
 export const Iframe = ({ children }: IframeProps) => {
-  return (
-    <div className={cx('root', 'iframe-wrapper')}>
-      <div className="h-full mx-0 my-auto aspect-[calc(16/9)]">{children}</div>
-    </div>
-  )
+  return <div className={cx('root', 'iframe-wrapper')}>{children}</div>
 }
