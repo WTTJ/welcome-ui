8e9b48cc3d01f7d33a95190d6dd278122128db30
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-09-26 16:25:37 +0200

    chore: pass through all css module files for uniformization (#2912)
    * chore: pass through all css module files for uniformization
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * fix: unused css variable for arrow placement
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Robel Tekle <robel.tekle@wttj.co>
    
    * chore: fix select css order and selector
    
    * chore: remove invalid pseudo state
    
    * chore: remove useless div wrapper
    
    * fix: take camille's notes into account
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Camille Nairi<camille.nairi@wttj.co>
    
    ---------
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Robel Tekle <robel.tekle@wttj.co>
    Signed-off-by: Camille Nairi<camille.nairi@wttj.co>
    Co-authored-by: isaacmaamouche <maamouche.isaac@gmail.com>

--- lib/src/components/Popover/Arrow.tsx
+++ lib/src/components/Popover/Arrow.tsx
@@ -10,24 +10,15 @@ type ArrowProps = {
 }
 const cx = classNames(popoverStyles)
 
-const rotation = {
-  bottom: 'rotateZ(360deg)',
-  left: 'rotateZ(90deg)',
-  right: 'rotateZ(-90deg)',
-  top: 'rotateZ(180deg)',
-}
-
 export const Arrow = ({ store }: ArrowProps) => {
   const currentPlacement = Ariakit.useStoreState(store, 'currentPlacement')
 
   const [parentPlacement] = currentPlacement.split('-')
-  const transform = rotation[parentPlacement as keyof typeof rotation]
 
   return (
     <Ariakit.PopoverArrow className={cx('arrow')} store={store}>
       <svg
-        className={cx('arrow-item')}
-        style={{ '--popover-transform': transform } as React.CSSProperties}
+        className={cx('arrow-item', parentPlacement && `placement-${parentPlacement}`)}
         xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M7 30L15 22L23 30H7Z" fill="currentColor" fillRule="nonzero" id="stroke" />
