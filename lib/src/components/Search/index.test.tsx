57e582c02745ea8ac4be6d2673821d7345651ad8
isaacmaamouche <maamouche.isaac@gmail.com>
2025-09-26 15:26:01 +0200

    chore: migrate search to tailwind

---
+++ lib/src/components/Search/index.test.tsx
@@ -0,0 +1,202 @@
+import { screen, waitFor } from '@testing-library/react'
+
+import { AvatarIcon } from '@/components/Icon'
+
+import { render } from '@tests'
+
+import { Search } from './'
+
+type Item = {
+  poster?: string
+  title?: string
+}
+
+const results = [
+  { poster: 'big-fish.jpg', title: 'Big Fish', year: '2003' },
+  { poster: 'wanda.jpg', title: 'A Fish Called Wanda', year: '1988' },
+  { poster: 'food-tank.jpg', title: 'Food Tank', year: '2009' }, // Doesn't match 'fish'
+  { poster: 'rumble-fish.jpg', title: 'Rumble Fish', year: '1983' },
+  { poster: 'cold-fish.jpg', title: 'Cold Fish', year: '2010' },
+]
+
+export const opt_group_results = [
+  {
+    label: 'Good movies',
+    options: [
+      { poster: 'big-fish.jpg', title: 'Big Fish', year: '2003' },
+      { poster: 'wanda.jpg', title: 'A Fish Called Wanda', year: '1988' },
+      { poster: 'food-tank.jpg', title: 'Food Tank', year: '2009' }, // Doesn't match 'fish'
+    ],
+  },
+  {
+    label: 'Bad movies',
+    options: [
+      { poster: 'rumble-fish.jpg', title: 'Rumble Fish', year: '1983' },
+      { poster: 'cold-fish.jpg', title: 'Cold Fish', year: '2010' },
+    ],
+  },
+]
+
+const defaultProps = {
+  itemToString: (item: Item) => item.title,
+  renderItem: (item: Item) => (
+    <div style={{ alignItems: 'center', display: 'flex' }}>
+      <div className="mr-(var(--space-xs) w-[20px]">
+        <img src={item.poster} />
+      </div>
+      <span>{item.title}</span>
+    </div>
+  ),
+  search: async function asyncSearch(q: string) {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    return results.filter(result => result.title.toLowerCase().indexOf(q) > -1)
+  },
+}
+
+describe('<Search>', () => {
+  it('<Search> has default attributes', () => {
+    render(<Search dataTestId="search" name="search" {...defaultProps} />)
+
+    const search = screen.getByTestId('search')
+
+    expect(search.getAttribute('placeholder')).toBe('Search…')
+    expect(search).toHaveTextContent('')
+  })
+
+  it('<Search> shows options when searching', async () => {
+    const { user } = render(<Search dataTestId="search" name="search" {...defaultProps} />)
+
+    const search = screen.getByTestId('search')
+    await user.type(search, 'fish')
+
+    const options = await waitFor(() => screen.getByRole('listbox').querySelectorAll('li'))
+
+    expect(options.length).toBe(4)
+    expect(options[0]).toHaveTextContent(results[0].title)
+  })
+
+  it('<Search> can choose option', async () => {
+    const { user } = render(<Search dataTestId="search" name="search" {...defaultProps} />)
+
+    const search = screen.getByTestId('search') as HTMLInputElement
+
+    await user.type(search, 'fish')
+
+    const options = await waitFor(() => screen.getByRole('listbox').querySelectorAll('li'))
+
+    await user.click(options[1])
+
+    expect(search.value).toEqual(results[1].title)
+  })
+
+  it('<Search> calls onChange with correct (object) values', async () => {
+    const handleChange = vi.fn()
+    const { user } = render(
+      <Search dataTestId="search" name="search" {...defaultProps} onChange={handleChange} />
+    )
+
+    const search = screen.getByTestId('search')
+
+    await user.type(search, 'fish')
+
+    const options = await waitFor(() => screen.getByRole('listbox').querySelectorAll('li'))
+
+    await user.click(options[1])
+
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(handleChange).toHaveBeenCalledWith(
+      results[1],
+      expect.objectContaining({
+        target: { name: 'search', value: results[1] },
+      }) // Ignore preventDefault
+    )
+  })
+
+  it('<Search> formats items', async () => {
+    const { user } = render(<Search dataTestId="search" name="search" {...defaultProps} />)
+
+    const search = screen.getByTestId('search')
+
+    await user.type(search, 'fish')
+
+    const options = await waitFor(() => screen.getByRole('listbox').querySelectorAll('li'))
+    const image = options[0].querySelector('img')
+
+    expect(image.getAttribute('src')).toBe('big-fish.jpg')
+  })
+
+  it('<Search icon> shows icon', () => {
+    render(
+      <Search
+        dataTestId="search"
+        icon={<AvatarIcon color="neutral-80" data-testid="icon-avatar" />}
+        name="search"
+        value="february"
+        {...defaultProps}
+      />
+    )
+
+    const icon = screen.getByTestId('icon-avatar')
+
+    expect(icon).toBeInTheDocument()
+  })
+
+  it("<Search> doesn't show list if no results", async () => {
+    const { user } = render(<Search dataTestId="search" name="search" {...defaultProps} />)
+
+    const search = screen.getByTestId('search')
+
+    await user.type(search, 'Fish')
+
+    const options = screen.queryByRole('listbox')
+    expect(options).toBeNull()
+  })
+
+  it('<Search groupsEnabled> shows groups header', async () => {
+    const { user } = render(
+      <Search
+        dataTestId="select"
+        groupsEnabled
+        name="search"
+        renderGroupHeader={({ label, options }) => (
+          <div data-testid="group-header">
+            <h4>{label}</h4>
+            <span>{options.length}</span>
+          </div>
+        )}
+        {...defaultProps}
+        search={() => Promise.resolve(opt_group_results)}
+      />
+    )
+
+    const search = screen.getByTestId('select')
+
+    await user.type(search, 'Fish')
+
+    const headers = await waitFor(() => screen.getAllByTestId('group-header'))
+
+    expect(headers.length).toBe(opt_group_results.length)
+
+    headers.forEach((header, i) => {
+      expect(header.querySelector('h4')).toHaveTextContent(opt_group_results[i].label)
+      expect(header.querySelector('span')).toHaveTextContent(
+        opt_group_results[i].options.length.toString()
+      )
+    })
+  })
+
+  it('<Search> shows options with minChars to 0', async () => {
+    const { user } = render(
+      <Search dataTestId="search" minChars={0} name="search" {...defaultProps} />
+    )
+
+    const search = screen.getByTestId('search')
+
+    await user.click(search)
+
+    const options = await waitFor(() => screen.getByRole('listbox').querySelectorAll('li'))
+
+    expect(options.length).toBe(results.length)
+    expect(options[0]).toHaveTextContent(results[0].title)
+  })
+})
---
+++ lib/src/components/Search/index.tsx
@@ -0,0 +1,269 @@
+import type { DownshiftProps, GetRootPropsOptions } from 'downshift'
+import DownshiftImport from 'downshift'
+import React, { forwardRef, Fragment, useCallback, useMemo, useState } from 'react'
+
+import { CloseButton as ClearButton } from '@/components/CloseButton'
+import { FIELD_ICON_SIZE } from '@/constants/field-icon-size'
+import { classNames } from '@/utils'
+import { createEvent } from '@/utils/create-event'
+import { throttle as handleThrottle } from '@/utils/throttle'
+
+import searchStyles from './search.module.scss'
+import type { Item, SearchOption, SearchOptionGroup, SearchProps } from './types'
+
+const EMPTY_STRING = ''
+
+const cx = classNames(searchStyles)
+
+// because of this issue: https://github.com/downshift-js/downshift/issues/1505
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const Downshift: typeof DownshiftImport = DownshiftImport.default || DownshiftImport
+
+export const Search = forwardRef<HTMLInputElement, SearchProps>(
+  (
+    {
+      autoComplete = 'off',
+      autoFocus,
+      className,
+      dataTestId,
+      disabled,
+      groupsEnabled,
+      icon,
+      iconPlacement = 'left',
+      id,
+      itemToString,
+      minChars = 3,
+      name,
+      onBlur,
+      onChange,
+      onClick,
+      onFocus,
+      placeholder = 'Search…',
+      renderGroupHeader,
+      renderItem,
+      search,
+      size = 'md',
+      throttle = 500,
+      transparent,
+      value: selected = EMPTY_STRING,
+      variant,
+      ...rest
+    },
+    ref
+  ) => {
+    // Get initial value from selected value(s)
+    const initialInputValue = selected ? itemToString(selected) : EMPTY_STRING
+
+    // Keep results in state
+    const [results, setResults] = useState<SearchOption[] | SearchOptionGroup[]>([])
+
+    // Update results when searching
+    const searchResults = useCallback(
+      async (value: string) => {
+        if (minChars === 0 || value?.length >= minChars) {
+          const data = await search(value)
+          setResults((data as SearchOption[] | SearchOptionGroup[]) || [])
+        } else {
+          setResults([])
+        }
+      },
+      [minChars, search]
+    )
+
+    const handleInputChange = useMemo(
+      () => handleThrottle(searchResults, throttle, false),
+      [searchResults, throttle]
+    )
+
+    // Send event to parent when value(s) changes
+    const handleChange = (value?: Item) => {
+      const event = createEvent({ name, value })
+      onChange?.(value, event)
+    }
+
+    const handleSelect = (result?: Item) => {
+      if (result) {
+        // If selecting result
+        handleChange(result)
+      } else {
+        // If removing result
+        handleChange()
+        setResults([])
+      }
+    }
+
+    const handleOuterClick = () => {
+      // Reset input value if not selecting a new item
+      if (!selected) {
+        handleSelect()
+      }
+      setResults([])
+    }
+
+    // prevents controlled to uncontrolled switch when itemToString returns falsy value
+    const handleItemToString = (item: Item) => itemToString(item) || ''
+
+    return (
+      <Downshift
+        initialInputValue={initialInputValue}
+        itemToString={handleItemToString}
+        onInputValueChange={handleInputChange}
+        onOuterClick={handleOuterClick}
+        onSelect={handleSelect}
+        selectedItem={selected}
+        {...(rest as DownshiftProps<Item>)}
+      >
+        {({
+          clearSelection,
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          getRootProps,
+          highlightedIndex,
+          inputValue,
+          isOpen,
+          selectedItem,
+          toggleMenu,
+        }) => {
+          const handleClearClick = () => {
+            setResults([])
+            handleChange()
+            clearSelection()
+          }
+          const isShowMenu = isOpen && results.length > 0
+          const DeleteIcon = (
+            <div className={cx('dropdown-indicator')}>
+              <ClearButton animatePresence onClick={handleClearClick} title="Clear" />
+            </div>
+          )
+          const handleInputFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+            onFocus?.(event)
+            handleInputChange('')
+            toggleMenu()
+          }
+          const rootProps = getRootProps(rest as GetRootPropsOptions, { suppressRefError: true })
+          const inputProps = getInputProps({
+            autoComplete,
+            autoFocus,
+            'data-testid': dataTestId,
+            disabled,
+            id,
+            name,
+            onBlur,
+            onClick: onClick,
+            onFocus: handleInputFocus,
+            placeholder,
+            ref,
+            size,
+            tabIndex: 0,
+            variant: isOpen ? 'focused' : variant,
+            ...rest,
+          }) as React.InputHTMLAttributes<HTMLInputElement>
+          const iconSize = FIELD_ICON_SIZE[size]
+
+          return (
+            <div {...rootProps} className={cx('wrapper')}>
+              <div className={cx('input-wrapper')}>
+                <input
+                  className={cx(
+                    'root',
+                    `size-${size}`,
+                    variant && `variant-${variant}`,
+                    transparent && 'transparent',
+                    icon && `icon-placement-${iconPlacement}`,
+                    className
+                  )}
+                  {...inputProps}
+                />
+                {icon ? (
+                  <div
+                    className={cx(
+                      'icon-wrapper',
+                      `icon-placement-${iconPlacement}`,
+                      `size-${size}`
+                    )}
+                  >
+                    {React.cloneElement(icon, { ...icon.props, size: iconSize })}
+                  </div>
+                ) : null}
+                <div className={cx('indicators', `size-${size}`)}>
+                  {inputValue ? DeleteIcon : null}
+                </div>
+              </div>
+              {isShowMenu ? (
+                <ul className={cx('menu')} {...getMenuProps()}>
+                  {
+                    (results as SearchOptionGroup[]).reduce(
+                      (acc, result, resultIndex) => {
+                        const index = acc.itemIndex++
+                        const isHighlighted = highlightedIndex === index
+
+                        if (groupsEnabled) {
+                          acc.itemsToRender.push(
+                            <Fragment key={resultIndex}>
+                              {renderGroupHeader(result as SearchOptionGroup)}
+                              {(result as SearchOptionGroup).options
+                                ? (result as SearchOptionGroup).options.map(
+                                    (option, optionIndex) => {
+                                      const isItemSelected =
+                                        selectedItem &&
+                                        itemToString(selectedItem) === itemToString(option)
+
+                                      return (
+                                        <li
+                                          className={cx(
+                                            'item',
+                                            isHighlighted && 'highlighted',
+                                            isItemSelected && 'selected'
+                                          )}
+                                          key={optionIndex}
+                                          {...getItemProps({
+                                            index,
+                                            item: option,
+                                          })}
+                                        >
+                                          {renderItem(option)}
+                                        </li>
+                                      )
+                                    }
+                                  )
+                                : null}
+                            </Fragment>
+                          )
+                        } else {
+                          const isItemSelected =
+                            selectedItem && itemToString(selectedItem) === itemToString(result)
+
+                          acc.itemsToRender.push(
+                            <li
+                              className={cx(
+                                'item',
+                                isHighlighted && 'highlighted',
+                                isItemSelected && 'selected'
+                              )}
+                              key={resultIndex}
+                              {...getItemProps({
+                                index: resultIndex,
+                                item: result,
+                              })}
+                            >
+                              {renderItem(result)}
+                            </li>
+                          )
+                        }
+
+                        return acc
+                      },
+                      { itemIndex: 0, itemsToRender: [] }
+                    ).itemsToRender
+                  }
+                </ul>
+              ) : null}
+            </div>
+          )
+        }}
+      </Downshift>
+    )
+  }
+)
