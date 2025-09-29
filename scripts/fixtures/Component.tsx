6647be9725688b75055bfaed7f067824eabdcd0b
Robert Douglas <rob@anucreative.com>
2025-09-29 09:47:31 +0200

    chore: add test for v9 upgrade script

---
+++ scripts/fixtures/Component.tsx
@@ -0,0 +1,85 @@
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import { Box } from '../../lib/src/old/components/Box'
+import { IconsFont } from '../../lib/src/old/components/IconsFont'
+import { Link } from '../../lib/src/old/components/Link'
+import { Loader } from '../../lib/src/old/components/Loader'
+import { Text } from '../../lib/src/old/components/Text'
+
+const FormattedMessage = ({
+  defaultMessage,
+  id,
+}: {
+  defaultMessage: string
+  id: string
+}): React.ReactNode => <span id={id}>{defaultMessage}</span>
+
+const signinPath = '/signin'
+
+const LAYOUT_WIDTH = {
+  normal: 400,
+  terms: 560,
+}
+
+// Usage examples
+export const Example = () => (
+  <>
+    <Box display="flex" h="100%">
+      <Box
+        alignItems="center"
+        as="aside"
+        backgroundColor="beige-20"
+        display={{ md: 'flex', xs: 'none' }}
+        flexDirection="column"
+        h="max-content"
+        justifyContent="center"
+        maxWidth={480}
+        minH="100%"
+        w="40%"
+      >
+        <Box pt="3xl">
+          <Link to={signinPath}>
+            <IconsFont.Add h={58} />
+          </Link>
+        </Box>
+        <Box
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+          flexGrow="1"
+          justifyContent="center"
+          maxWidth={420}
+          pb="xl"
+          px={{
+            lg: '3xl',
+            md: 'xl',
+          }}
+          w="100%"
+        >
+          <Text as="h2" mb="xxl" mt="0" variant="h3">
+            This is my content
+          </Text>
+        </Box>
+      </Box>
+      <Box display="flex" flex="1" flexDirection="column">
+        <Box
+          alignItems="center"
+          display="grid"
+          flexGrow="1"
+          maxW={LAYOUT_WIDTH['normal']}
+          mx="auto"
+          px="lg"
+          w="100%"
+        >
+          <Suspense fallback={<Loader />}>
+            <Outlet context={{}} />
+          </Suspense>
+        </Box>
+        <Box display={{ md: 'none' }} mt="xl">
+          <FormattedMessage defaultMessage="Terms of use" id="EBDrtq" />
+        </Box>
+      </Box>
+    </Box>
+  </>
+)
