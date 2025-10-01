d6f1619f98c40b28ec6b40e9922327afe6a7c12d
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-10-01 15:37:06 +0200

    fix: typescript issues on website documentation
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- website/build-app/components/Theme/index.tsx
+++ website/build-app/components/Theme/index.tsx
@@ -1,33 +1,36 @@
 'use client'
 
-import merge from 'lodash.merge'
-
 import { Text } from '@/components/Text'
-import { primitives, semantics } from '@/theme/tokens'
+import tokens from '@/theme/tokens.json'
+import type { FlatTokens, TokensStructure } from '@/theme/utils/parseTokens'
+import { parseTokens } from '@/theme/utils/parseTokens'
 
 type ColorsProps = {
   entry: string
 }
 
 export const Theme = ({ entry }: ColorsProps) => {
-  const merged = merge({}, primitives, semantics)
-  const entries = merged[entry as keyof typeof merged]
+  const entries = parseTokens(tokens as unknown as TokensStructure)
 
   if (!entries) {
     return <div>Unknown entry: {entry}</div>
   }
 
+  const filteredEntries = Object.entries(entries).reduce((acc, [key, value]) => {
+    if (key.startsWith(entry)) {
+      acc[key] = value
+    }
+    return acc
+  }, {} as FlatTokens)
+
   return (
     <div className="bg-beige-20 gap-sm grid mt-lg p-xxl rounded-lg">
-      {Object.entries(entries).map(([key, value], index) => {
+      {Object.entries(filteredEntries).map(([key, value], index) => {
         const border = index !== 0 ? 'border-t border-t-neutral-30' : ''
 
         return (
-          <div
-            className={`${border} gap-xxl grid grid-cols-[150px_150px_150px] pt-sm`}
-            key={`${entry}-${key}`}
-          >
-            <Text className="font-bold text-violet-80">{key.replace('--', '')}</Text>
+          <div className={`${border} gap-xxl grid grid-cols-[150px_150px_150px] pt-sm`} key={key}>
+            <Text className="font-bold text-violet-80">{key}</Text>
             <Text className="text-neutral-70">
               {value as string}
               {typeof value === 'number' && 'px'}
