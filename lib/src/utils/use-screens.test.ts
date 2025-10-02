36c6abc53eab73b71c2b1710006c7599e04f778b
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-10-02 20:03:28 +0200

    chore: initialize tokens for belonging
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Camille Nairi<camille.nairi@wttj.co>

--- lib/src/utils/use-screens.test.ts
+++ lib/src/utils/use-screens.test.ts
@@ -1,14 +1,16 @@
+/* eslint-disable perfectionist/sort-objects */
+
 import { useScreens } from './use-screens'
 
-vi.mock(import('@/theme/tokens.json'), async importOriginal => {
+vi.mock(import('@/theme/tokens/primitives.json'), async importOriginal => {
   const actual = await importOriginal()
   return {
     default: {
       ...actual,
-      breakpoint: {
-        ...actual.breakpoint,
-        mobile: { $value: '24rem' },
-        tablet: { $value: '380' },
+      breakpoints: {
+        ...actual.breakpoints,
+        mobile: { value: '24rem' },
+        tablet: { value: '380' },
       },
     },
   }
@@ -18,15 +20,15 @@ describe('useScreens', () => {
   it('should return the correct screen sizes', async () => {
     const screens = useScreens()
     expect(screens).toEqual({
+      xs: 0,
+      tablet: 380,
+      sm: 480,
+      md: 736,
+      lg: 980,
+      xl: 1280,
       '2xl': 1440,
       '3xl': 1620,
       '4xl': 1920,
-      lg: 980,
-      md: 736,
-      sm: 480,
-      tablet: 380,
-      xl: 1280,
-      xs: 0,
     })
   })
 })
