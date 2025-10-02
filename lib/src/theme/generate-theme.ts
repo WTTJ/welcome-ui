7ffa631f9046814e04c256dbf68fa5ed71b42cb6
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-10-02 20:03:28 +0200

    chore: initialize tokens for belonging
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Signed-off-by: Camille Nairi<camille.nairi@wttj.co>

--- lib/src/theme/generate-theme.ts
+++ lib/src/theme/generate-theme.ts
@@ -2,9 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-import tokens from './tokens.json' assert { type: 'json' }
+import primitives from './tokens/primitives.json' assert { type: 'json' }
 
-const indentation = '  ' // 2 spaces for indentation
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -12,21 +11,23 @@ const fontFacesPath = path.join(__dirname, 'fontFaces.css')
 const basePath = path.join(__dirname, 'base.css')
 const resetPath = path.join(__dirname, 'resets.css')
 const themePath = path.join(__dirname, 'theme.css')
+const techTokensPath = path.join(__dirname, 'techTokens.css')
 const variablesPath = path.join(__dirname, 'variables.css')
 const breakpointsPath = path.join(__dirname, '../utils/scss/breakpoints.scss')
 
 const fontFaces = fs.readFileSync(fontFacesPath, 'utf8')
 const baseStyles = fs.readFileSync(basePath, 'utf8')
+const techTokens = fs.readFileSync(techTokensPath, 'utf8')
 const resetStyles = fs.readFileSync(resetPath, 'utf8')
 const variableStyles = fs.readFileSync(variablesPath, 'utf8')
 
 const getStringFrom = (map: Record<string, string>) => `${map.property} {\n${map.value}}\n`
 
-const addedVariables = '--spacing: 0.0625rem;'
-const resetTailwindTokens = `${indentation}--*: initial;\n`
+const stringifiedTechTokens = `${techTokens}`
+
 const variablesStylesWithTailwindResets = variableStyles.replace(
   '@theme static {\n',
-  `@theme static {\n${resetTailwindTokens}\n${addedVariables}`
+  `@theme static {\n${stringifiedTechTokens}`
 )
 
 const baseLayer = { property: '@layer base', value: `${resetStyles}\n${baseStyles}` }
@@ -38,9 +39,9 @@ const generateThemeCss = () =>
 fs.writeFileSync(themePath, generateThemeCss(), 'utf8')
 
 // Generate scss breakpoints file
-const breakpointsContent = Object.entries(tokens.breakpoint).reduce((acc, [key, value]) => {
-  if (key !== '$type' && typeof value === 'object' && '$value' in value) {
-    return acc + `$breakpoint-${key}: ${value['$value']};\n`
+const breakpointsContent = Object.entries(primitives.breakpoints).reduce((acc, [key, value]) => {
+  if (key !== '$type' && typeof value === 'object' && 'value' in value) {
+    return acc + `$breakpoint-${key}: ${value['value']};\n`
   }
   return acc
 }, '/* screens - auto-generated from token.ts - do not edit directly */\n')
