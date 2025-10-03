022678607b053733e3bc9f6a1a723c53cc7855d1
Robert Douglas <rob@anucreative.com>
2025-10-03 13:56:37 +0200

    chore: fix interpolation

---
+++ scripts/migrate-v9/temp/ExternalSimpleComponent-test/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss'
+export const SimpleComponent = () => {
+  return (
+    <div className="container">
+      <h1 className="header">Simple Header</h1>
+      <section className="content">
+        <button className="button">Click me</button>
+      </section>
+    </div>
+  )
+}
