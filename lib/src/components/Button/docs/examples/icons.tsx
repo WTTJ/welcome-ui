a3b46b46aaa79fe8f6b4fefe5577490b35b4deb6
cnairi <camillenairi@icloud.com>
2025-10-07 13:41:12 +0200

    chore(button): updates doc

--- lib/src/components/Button/docs/examples/icons.tsx
+++ lib/src/components/Button/docs/examples/icons.tsx
@@ -3,32 +3,36 @@ import { AddIcon } from '@/components/Icon'
 
 const Example = () => {
   return (
-    <>
-      <Button>
-        <AddIcon />
-        <span>Button</span>
-      </Button>
-      <Button variant="secondary">
-        <AddIcon />
-        <span>Button</span>
-      </Button>
-      <Button variant="tertiary">
-        <AddIcon />
-        <span>Button</span>
-      </Button>
-      <Button disabled>
-        <AddIcon />
-        <span>Button</span>
-      </Button>
-      <Button size="sm">
-        <AddIcon />
-        <span>Small</span>
-      </Button>
-      <Button size="md">
-        <AddIcon />
-        <span>Medium</span>
-      </Button>
-    </>
+    <div className="flex flex-col gap-md">
+      <div className="flex gap-md">
+        <Button>
+          <AddIcon />
+          <span>Button</span>
+        </Button>
+        <Button variant="secondary">
+          <AddIcon />
+          <span>Button</span>
+        </Button>
+        <Button variant="tertiary">
+          <AddIcon />
+          <span>Button</span>
+        </Button>
+        <Button disabled>
+          <AddIcon />
+          <span>Button</span>
+        </Button>
+      </div>
+      <div className="flex gap-md">
+        <Button size="sm">
+          <AddIcon />
+          <span>Small</span>
+        </Button>
+        <Button size="md">
+          <AddIcon />
+          <span>Medium</span>
+        </Button>
+      </div>
+    </div>
   )
 }
 
