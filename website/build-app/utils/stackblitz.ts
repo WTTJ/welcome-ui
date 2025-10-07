79a468bd14d0fc6498352c0b69f9f9bcaa44228c
Théo Mesnil <mesniltheo@gmail.com>
2025-10-07 09:58:35 +0200

    docs: rework stackblitz

--- website/build-app/utils/stackblitz.ts
+++ website/build-app/utils/stackblitz.ts
@@ -5,6 +5,7 @@ const getIndexHtml = (name: string) => `<html lang="en">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>${name} - Playground [Welcome UI]</title>
+    <link rel="stylesheet" href="/global.css" />
     <style>
       body {
         padding: 20px;
@@ -18,28 +19,74 @@ const getIndexHtml = (name: string) => `<html lang="en">
 </html>`
 
 const indexTsx = `import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { createTheme } from 'welcome-ui/theme';
-import { WuiProvider } from 'welcome-ui/WuiProvider';
+import {createRoot} from 'react-dom/client';
 
 import Demo from './demo'
 
-const theme = createTheme()
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <WuiProvider theme={theme}>
       <Demo />
-    </WuiProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )`
+
+const globalCss = `@import 'tailwindcss';
+@import 'welcome-ui/theme.css';
+/* Temporary import, we should create a welcome-ui base css */
+@import 'welcome-ui/Accordion.css';
+@import 'welcome-ui/Alert.css';
+@import 'welcome-ui/AspectRatio.css';
+@import 'welcome-ui/Avatar.css';
+@import 'welcome-ui/Badge.css';
+@import 'welcome-ui/Breadcrumb.css';
+@import 'welcome-ui/Button.css';
+@import 'welcome-ui/ButtonGroup.css';
+@import 'welcome-ui/Card.css';
+@import 'welcome-ui/Checkbox.css';
+@import 'welcome-ui/CloseButton.css';
+@import 'welcome-ui/CustomPopper.css';
+@import 'welcome-ui/DatePicker.css';
+@import 'welcome-ui/Drawer.css';
+@import 'welcome-ui/DropdownMenu.css';
+@import 'welcome-ui/Field.css';
+@import 'welcome-ui/FieldGroup.css';
+@import 'welcome-ui/FileDrop.css';
+@import 'welcome-ui/FileUpload.css';
+@import 'welcome-ui/Hint.css';
+@import 'welcome-ui/Icon.css';
+@import 'welcome-ui/InputText.css';
+@import 'welcome-ui/Label.css';
+@import 'welcome-ui/Link.css';
+@import 'welcome-ui/Loader.css';
+@import 'welcome-ui/Logo.css';
+@import 'welcome-ui/Modal.css';
+@import 'welcome-ui/Pagination.css';
+@import 'welcome-ui/Popover.css';
+@import 'welcome-ui/Radio.css';
+@import 'welcome-ui/RadioGroup.css';
+@import 'welcome-ui/RadioTab.css';
+@import 'welcome-ui/Search.css';
+@import 'welcome-ui/Select.css';
+@import 'welcome-ui/Slider.css';
+@import 'welcome-ui/Swiper.css';
+@import 'welcome-ui/Table.css';
+@import 'welcome-ui/Tabs.css';
+@import 'welcome-ui/Tag.css';
+@import 'welcome-ui/Text.css';
+@import 'welcome-ui/Textarea.css';
+@import 'welcome-ui/Toast.css';
+@import 'welcome-ui/Toggle.css';
+@import 'welcome-ui/Tooltip.css';
+@import 'welcome-ui/VariantIcon.css';
+@import 'welcome-ui/utils.css';`
 
 const viteConfig = `import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tailwindcss()]
 })`
 
 const getPackageJson = () => `{
@@ -51,13 +98,13 @@ const getPackageJson = () => `{
     "serve": "vite preview"
   },
   "dependencies": {
-    "welcome-ui": "latest",
-    "@xstyled/styled-components": "^3.7.3",
-    "react": "^18.1.0",
-    "react-dom": "^18.1.0",
-    "styled-components": "^5.3.9"
+    "welcome-ui": "0.0.0-dev.1759821506923",
+    "tailwindcss": "^4.1.11",
+    "react": "^19.0.0",
+    "react-dom": "^19.0.0"
   },
   "devDependencies": {
+    "@tailwindcss/vite": "^4.1.14",
     "@types/react": "18.0.25",
     "@types/react-dom": "18.0.11",
     "@vitejs/plugin-react": "^4.3.0",
@@ -81,11 +128,12 @@ export const openStackBlitz = ({ code, name }: { code: string; name: string }) =
     {
       files: {
         'demo.tsx': code,
+        'global.css': globalCss,
         'index.html': indexHtml,
         'index.tsx': indexTsx,
         'package.json': packageJson,
         'tsconfig.json': tsConfig,
-        'vite.config.ts': viteConfig,
+        'vite.config.mjs': viteConfig,
       },
       settings: {
         compile: {
