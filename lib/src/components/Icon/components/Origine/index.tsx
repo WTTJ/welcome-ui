0a664aafddfa295bef9ed70aca3cedb00a631685
StephaneRob <stephane.robino@gmail.com>
2025-10-08 18:09:27 +0200

    chore: init rework icons

--- lib/src/components/Icon/components/Origine/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from '../../Icon'
-import type { IconProps } from '../../Icon.types'
-
-import content from './content.json'
-
-export const OrigineIcon = (props: IconProps) => {
-  return <Icon content={content} {...props} />
-}
