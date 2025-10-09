515765678e3e5069e088a0c1de26460a63ce50a3
Théo Mesnil <mesniltheo@gmail.com>
2025-10-09 11:04:53 +0200

    feat: rework styles for Hint

--- lib/src/components/Hint/types.ts
+++ lib/src/components/Hint/types.ts
@@ -4,5 +4,8 @@ export type HintProps = ComponentPropsWithRef<'span'> & HintOptions
 
 interface HintOptions {
   children: React.ReactNode
+  /**
+   * Change color of text according to the variant
+   */
   variant?: 'danger' | 'success' | 'warning'
 }
