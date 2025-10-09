2a0d489e4dbdd7eca3f2e859266dcdb5f28c03e3
Diane Guillot <36013102+Leiksa@users.noreply.github.com>
2025-10-09 17:09:59 +0200

    chore(link): belonging design

--- lib/src/components/Link/docs/examples/disabled.tsx
+++ lib/src/components/Link/docs/examples/disabled.tsx
@@ -3,10 +3,17 @@ import { Link } from '@/components/Link'
 const Example = () => {
   return (
     <div className="flex flex-col gap-xl">
+<<<<<<< HEAD
       <Link disabled href="/disabled">
         Primary disabled
       </Link>
       <Link disabled href="/disabled" variant="secondary">
+=======
+      <Link disabled href="#">
+        Primary disabled
+      </Link>
+      <Link disabled href="#" variant="secondary">
+>>>>>>> 23a43ee7f (chore(link): belonging design)
         Secondary disabled
       </Link>
     </div>
--- lib/src/components/Link/docs/examples/overview.tsx
+++ lib/src/components/Link/docs/examples/overview.tsx
@@ -1,7 +1,11 @@
 import { Link } from '@/components/Link'
 
 const Example = () => {
+<<<<<<< HEAD
   return <Link href="https://www.welcometothejungle.com">Link</Link>
+=======
+  return <Link href="https://google.com">Link</Link>
+>>>>>>> 23a43ee7f (chore(link): belonging design)
 }
 
 export default Example
