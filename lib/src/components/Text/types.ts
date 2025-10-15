e84d23180ff618b33278e6c5f8ff54d61e088ae1
cnairi <camillenairi@icloud.com>
2025-10-15 14:42:48 +0200

    chore(text): add belonging text component

--- lib/src/components/Text/types.ts
+++ lib/src/components/Text/types.ts
@@ -1,10 +1,37 @@
+/* eslint-disable perfectionist/sort-union-types */
 import type { PolymorphicProps } from '@/theme/types'
-
-import type { TAG_NAMES } from '.'
 
 export interface TextOptions {
   lines?: number
-  variant?: keyof typeof TAG_NAMES
+  sizes?: 'lg' | 'md' | 'sm' | 'xl'
+  strong?: boolean
+  variant?:
+    | 'display-xl'
+    | 'display-lg'
+    | 'display-md'
+    | 'display-sm'
+    | 'heading-xl'
+    | 'heading-lg'
+    | 'heading-md'
+    | 'heading-sm'
+    | 'heading-xs'
+    | 'heading-md-strong'
+    | 'heading-sm-strong'
+    | 'heading-xs-strong'
+    | 'body-xl'
+    | 'body-lg'
+    | 'body-md'
+    | 'body-sm'
+    | 'body-xl-strong'
+    | 'body-lg-strong'
+    | 'body-md-strong'
+    | 'body-sm-strong'
+    | 'label-xl'
+    | 'label-lg'
+    | 'label-md'
+    | 'label-sm'
+    | 'label-md-strong'
+    | 'label-sm-strong'
   withDash?: boolean
 }
 
