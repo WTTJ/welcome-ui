28807c71cc6604e8cbc7b38d551bc26bbdf464f5
cnairi <camillenairi@icloud.com>
2025-10-16 10:22:53 +0200

    chore(inputtext): components migration  to belonging

--- lib/src/components/DatePicker/docs/examples/sizes.tsx
+++ lib/src/components/DatePicker/docs/examples/sizes.tsx
@@ -4,15 +4,13 @@ import { Icon } from '@/components/Icon'
 const Example = () => {
   return (
     <div className="flex flex-col gap-xl">
-      <DatePicker name="welcome" size="xs" value={new Date()} />
       <DatePicker name="welcome" size="sm" value={new Date()} />
+      <DatePicker name="welcome" size="md" value={new Date()} />
       <DatePicker name="welcome" value={new Date()} />
-      <DatePicker name="welcome" size="lg" value={new Date()} />
       Example with icon
-      <DatePicker icon={<Icon name="calendar" />} name="welcome" size="xs" value={new Date()} />
       <DatePicker icon={<Icon name="calendar" />} name="welcome" size="sm" value={new Date()} />
+      <DatePicker icon={<Icon name="calendar" />} name="welcome" size="md" value={new Date()} />
       <DatePicker icon={<Icon name="calendar" />} name="welcome" value={new Date()} />
-      <DatePicker icon={<Icon name="calendar" />} name="welcome" size="lg" value={new Date()} />
     </div>
   )
 }
