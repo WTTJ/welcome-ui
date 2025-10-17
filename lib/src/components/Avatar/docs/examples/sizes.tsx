b7e411ecc12c4035e23ee76866f71dbec64078e2
Théo Mesnil <mesniltheo@gmail.com>
2025-10-17 11:26:58 +0200

    feat: migrate Avatar for belonging

--- lib/src/components/Avatar/docs/examples/sizes.tsx
+++ lib/src/components/Avatar/docs/examples/sizes.tsx
@@ -2,34 +2,41 @@ import { Avatar } from '@/components/Avatar'
 
 const Example = () => {
   return (
-    <>
-      <Avatar
-        name="Small"
-        size="sm"
-        src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4"
-      />
-      <Avatar name="Medium" src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4" />
-      <Avatar
-        name="Large"
-        size="lg"
-        src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4"
-      />
-      <Avatar
-        name="Extra Larga"
-        size="xl"
-        src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4"
-      />
-      <Avatar
-        name="XX Large"
-        size="xxl"
-        src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4"
-      />
-      <Avatar
-        className="size-[130px]"
-        name="Custom value"
-        src="https://avatars3.githubusercontent.com/u/13100706?s=200&v=4"
-      />
-    </>
+    <div className="flex flex-col gap-xxl">
+      <div className="flex gap-xl items-center">
+        <Avatar
+          name="Welcome UI"
+          size="lg"
+          src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+        />
+        <Avatar
+          name="Welcome UI"
+          src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+        />
+        <Avatar
+          name="Welcome UI"
+          size="sm"
+          src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+        />
+        <Avatar
+          name="Welcome UI"
+          size="xs"
+          src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+        />
+      </div>
+      <div className="flex gap-xl items-center">
+        <Avatar name="Welcome UI" size="lg" />
+        <Avatar name="Welcome UI" />
+        <Avatar name="Welcome UI" size="sm" />
+        <Avatar name="Welcome UI" size="xs" />
+      </div>
+      <div className="flex gap-xl items-center">
+        <Avatar size="lg" />
+        <Avatar />
+        <Avatar size="sm" />
+        <Avatar size="xs" />
+      </div>
+    </div>
   )
 }
 
