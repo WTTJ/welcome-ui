eb035154677be26a47aa670122b7033a6a5e8194
Théo Mesnil <mesniltheo@gmail.com>
2025-10-17 16:39:44 +0200

    feat: migrate Toggle for belonging

--- lib/src/components/Toggle/docs/examples/hint.tsx
+++ lib/src/components/Toggle/docs/examples/hint.tsx
@@ -1,14 +1,10 @@
-import * as React from 'react'
-
 import { Field } from '@/components/Field'
 import { Toggle } from '@/components/Toggle'
 
 const Example = () => {
-  const [toggle, setToggle] = React.useState(false)
-
   return (
     <Field hint="a hint" inline label="Toggle">
-      <Toggle checked={toggle} onClick={() => setToggle(!toggle)} />
+      <Toggle />
     </Field>
   )
 }
