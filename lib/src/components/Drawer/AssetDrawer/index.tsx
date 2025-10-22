8e0fe1b989e03d6845ac9b6083590168c93463e9
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-10-22 14:57:52 +0200

    chore: add stylelint custom plugin for token validation
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- lib/src/components/Drawer/AssetDrawer/index.tsx
+++ lib/src/components/Drawer/AssetDrawer/index.tsx
@@ -60,7 +60,7 @@ export const AssetDrawerComponent = forwardRefWithAs<AssetDrawerProps, 'div'>(
         <div className={cx('content-wrapper')}>
           <div
             className={cx('content-max-width')}
-            style={{ '--asset-drawer-content-max-width': drawerMaxWidth } as React.CSSProperties}
+            style={{ '--assetDrawerContentMaxWidth': drawerMaxWidth } as React.CSSProperties}
           >
             <div className={cx('content')}>{children}</div>
           </div>
