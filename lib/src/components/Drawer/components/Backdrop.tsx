63439f349175b91c7d06094ace69ed6c45aa3c82
Jerome Raffin <raffin.jerome@gmail.com>
2025-10-29 16:18:07 +0100

    fix: asset drawer backdrop

--- lib/src/components/Drawer/components/Backdrop.tsx
+++ lib/src/components/Drawer/components/Backdrop.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react'
+
 import { classNames } from '@/utils'
 
 import styles from '../drawer.module.scss'
@@ -5,11 +7,14 @@ import type { BackdropProps } from '../types'
 
 const cx = classNames(styles)
 
-export const Backdrop = ({ hideOnInteractOutside = true, ...rest }: BackdropProps) => {
-  return (
-    <div
-      className={cx('backdrop', hideOnInteractOutside && `drawer-hide-on-interact-outside`)}
-      {...rest}
-    />
-  )
-}
+export const Backdrop = forwardRef<HTMLDivElement, BackdropProps>(
+  ({ hideOnInteractOutside = true, ...rest }, ref) => {
+    return (
+      <div
+        className={cx('backdrop', hideOnInteractOutside && `drawer-hide-on-interact-outside`)}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+)
