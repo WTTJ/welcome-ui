5033d968f50c7ab86672ea107596380037db6f90
Robert Douglas <rob@anucreative.com>
2025-11-04 16:59:43 +0100

    fix: getColorFromName crashes if no name passed

--- lib/src/components/Avatar/tests/utils.test.ts
+++ lib/src/components/Avatar/tests/utils.test.ts
@@ -2,6 +2,7 @@ import { getColorFromName, getInitials } from '../utils'
 
 describe('getColorFromName', () => {
   it('returns a secondary color key based on text length', () => {
+    expect(getColorFromName()).toBe('secondary-blue')
     expect(getColorFromName('a')).toBe('secondary-green')
     expect(getColorFromName('ab')).toBe('secondary-orange')
     expect(getColorFromName('abc')).toBe('secondary-pink')
