de10943931b9b4204b83151fee433fdd5ec8ab5d
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2025-11-06 14:06:03 +0100

    feat: add belonging on Alert (#3043)
    * fix: run prettier with fixer on all scss files
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * chore(link): belonging design
    
    * fix: run prettier with fixer on all scss files
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * fix: run prettier with fixer on all scss files
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    
    * [WUI-233] chore(link): belonging update for disabled state (#3017)
    
    * chore(link): belonging update for disabled state
    
    * chore(link): update token
    
    * fix(link): missing space
    
    * feat: add belonging on Alert
    
    ---------
    
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Co-authored-by: Paul-Xavier Ceccaldi <pix@wttj.co>
    Co-authored-by: Diane Guillot <36013102+Leiksa@users.noreply.github.com>

--- lib/src/components/Alert/docs/examples/sizes.tsx
+++ lib/src/components/Alert/docs/examples/sizes.tsx
@@ -4,13 +4,13 @@ const Example = () => {
   return (
     <div className="flex flex-col gap-xl">
       <Alert cta={<Alert.Button>Button</Alert.Button>}>
-        <Alert.Title>Default size</Alert.Title>
+        <Alert.Title>Default size (md)</Alert.Title>
         Nunc laoreet egestas nulla, et dapibus sem malesuada in. Suspendisse eleifend accumsan
         ultrices. Phasellus iaculis nisi sed dui ornare commodo. Nullam dapibus varius nibh a
         ornare.
       </Alert>
-      <Alert cta={<Alert.Button>Button</Alert.Button>} size="md">
-        <Alert.Title>Medium size</Alert.Title>
+      <Alert cta={<Alert.Button>Button</Alert.Button>} size="lg">
+        <Alert.Title>Large size (lg)</Alert.Title>
         Nunc laoreet egestas nulla, et dapibus sem malesuada in. Suspendisse eleifend accumsan
         ultrices. Phasellus iaculis nisi sed dui ornare commodo. Nullam dapibus varius nibh a
         ornare.
