76b14a62a0b3be27edc75e47238e7fb9313241b3
Théo Mesnil <mesniltheo@gmail.com>
2025-11-06 11:00:22 +0100

    fix: close button on Alert

--- lib/src/components/Alert/index.tsx
+++ lib/src/components/Alert/index.tsx
@@ -73,7 +73,9 @@ const AlertComponent = forwardRef<HTMLDivElement, AlertProps>(
         ref={ref}
         {...rest}
       >
-        {!!handleClose && <CloseButton className="close-button" onClick={handleClose} size="xs" />}
+        {!!handleClose && (
+          <CloseButton className={cx('close-button')} onClick={handleClose} size="xs" />
+        )}
         {showIcon ? (
           <VariantIcon className={cx('variant-icon')} size={size} variant={defaultVariantIcon} />
         ) : null}
