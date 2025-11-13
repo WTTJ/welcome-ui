563d29d2b62f7fc10fbb3055e049ab6fa7d4fbb9
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2025-11-13 13:48:26 +0100

    feat: add new StickyNote component (#3048)
    * feat: add new StickyNotes components
    
    * feat: rename to StickyNote and review changes

---
+++ lib/src/components/StickyNote/index.tsx
@@ -0,0 +1,31 @@
+import { forwardRef } from 'react'
+
+import { CloseButton } from '@/components/CloseButton'
+import { classNames } from '@/utils'
+
+import { Button } from './components/Button'
+import { Title } from './components/Title'
+import styles from './sticky-note.module.scss'
+import type { StickyNoteProps } from './types'
+
+const cx = classNames(styles)
+
+const StickyNoteComponent = forwardRef<HTMLDivElement, StickyNoteProps>(
+  ({ children, className, handleClose, shape = 'square', variant = 'brand' }, ref) => {
+    const hasCloseButton = Boolean(handleClose)
+
+    return (
+      <div className={cx('root', `variant-${variant}`, `shape-${shape}`, className)} ref={ref}>
+        {hasCloseButton ? (
+          <CloseButton className={cx('close-button')} onClick={handleClose} size="sm" />
+        ) : null}
+        <div className={cx('content')}>{children}</div>
+      </div>
+    )
+  }
+)
+
+export const StickyNote = Object.assign(StickyNoteComponent, {
+  Button,
+  Title,
+})
