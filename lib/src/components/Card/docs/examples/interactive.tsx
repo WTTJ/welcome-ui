4be9a56274affb443e5072479a7796d9b220f88c
Simon Degraeve <simon.degraeve@gmail.com>
2025-11-14 15:56:48 +0100

    feat(card): update interactivity

--- lib/src/components/Card/docs/examples/interactive.tsx
+++ lib/src/components/Card/docs/examples/interactive.tsx
@@ -4,22 +4,10 @@ import { Icon } from '@/components/Icon'
 const Example = () => {
   return (
     <>
-      <Card className="max-w-2/3">
-        <Card.Header>
-          <Icon name="book-alt" />
-          The Jungle Book
-        </Card.Header>
-        <Card.Body>
-          After a threat from the tiger Shere Khan forces him to flee the jungle, a man-cub named
-          Mowgli embarks on a journey of self discovery with the help of panther Bagheera and
-          free-spirited bear Baloo.
-        </Card.Body>
-      </Card>
-
-      <Card className="max-w-2/3">
+      <Card className="max-w-2/3" onClick={() => alert('Card clicked')}>
         <Card.Header onClose={() => alert('Closing card')}>
           <Icon name="book-alt" />
-          The Jungle Book
+          The Jungle Book (clickable)
         </Card.Header>
         <Card.Body>
           After a threat from the tiger Shere Khan forces him to flee the jungle, a man-cub named
@@ -28,13 +16,12 @@ const Example = () => {
         </Card.Body>
       </Card>
 
-      <Card className="max-w-2/3">
-        <Card.Header disabled onClose={() => alert('Closing card')} />
-        <Card.Body>
-          <h3 className="flex items-center gap-4">
-            <Icon name="book-alt" size="lg" />
-            The Jungle Book (disabled close)
-          </h3>
+      <Card className="max-w-2/3" disabled onClick={() => alert('Card clicked')}>
+        <Card.Header disabled onClose={() => alert('Closing card')}>
+          <Icon name="book-alt" />
+          The Jungle Book (disabled)
+        </Card.Header>
+        <Card.Body metadata="1894 ‧ Adventure/Drama">
           After a threat from the tiger Shere Khan forces him to flee the jungle, a man-cub named
           Mowgli embarks on a journey of self discovery with the help of panther Bagheera and
           free-spirited bear Baloo.
