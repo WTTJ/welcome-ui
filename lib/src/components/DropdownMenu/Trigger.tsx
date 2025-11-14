cce04cbedd1228a048fc6378f79ca3ab0d17f216
cnairi <camillenairi@icloud.com>
2025-11-14 11:38:21 +0100

    chore(dropdown): add preventDefault on Trigger

--- lib/src/components/DropdownMenu/Trigger.tsx
+++ lib/src/components/DropdownMenu/Trigger.tsx
@@ -1,14 +1,21 @@
 import { MenuButton } from '@ariakit/react'
 import type { MenuButtonProps } from '@ariakit/react'
+import type { MouseEvent } from 'react'
 
 import { forwardRefWithAs } from '@/utils'
 
 import type { TriggerProps } from './types'
 
 export const Trigger = forwardRefWithAs<TriggerProps, 'button'>(
-  ({ as: Component, store, ...rest }, ref) => {
+  ({ as: Component, onClick, store, ...rest }, ref) => {
+    const handleMenuButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
+      onClick?.(e)
+      store.toggle()
+    }
     return (
       <MenuButton
+        onClick={handleMenuButtonClick}
         ref={ref}
         render={Component ? props => <Component {...props} /> : undefined}
         store={store}
