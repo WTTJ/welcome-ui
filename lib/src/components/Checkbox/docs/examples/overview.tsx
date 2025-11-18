368e1043cff4280380eeccb1ac79bd635fb8e4d1
cnairi <camillenairi@icloud.com>
2025-11-18 18:38:33 +0100

    fix(checkbox): allow onChange on Checkbox (for accessing its value outside the component scope)

--- lib/src/components/Checkbox/docs/examples/overview.tsx
+++ lib/src/components/Checkbox/docs/examples/overview.tsx
@@ -3,7 +3,10 @@ import { Checkbox } from '@/components/Checkbox'
 const Example = () => {
   return (
     <>
-      <Checkbox name="default" />
+      <Checkbox
+        name="default"
+        onChange={value => alert(`Checkbox is now ${value ? 'checked' : 'unchecked'}`)}
+      />
       <Checkbox checked name="not-checked" />
       <Checkbox indeterminate name="indeterminate" />
       <Checkbox disabled name="default-disabled" />
