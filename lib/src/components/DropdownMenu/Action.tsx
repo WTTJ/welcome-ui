668a999bc4129db1cf8bda2af9891f1164dacb39
Simon Degraeve <simon.degraeve@gmail.com>
2025-11-20 16:55:16 +0100

    feat(dropdown-menu): update `DropdownMenu` with belonging design (#3060)

---
+++ lib/src/components/DropdownMenu/Action.tsx
@@ -0,0 +1,19 @@
+import { MenuItem } from '@ariakit/react'
+
+import { classNames, forwardRefWithAs } from '@/utils'
+
+import dropdownMenuStyles from './dropdown-menu.module.scss'
+import type { ActionProps } from './types'
+
+const cx = classNames(dropdownMenuStyles)
+
+export const Action = forwardRefWithAs<ActionProps, 'button'>(
+  ({ as: Component = 'button', className, ...props }, ref) => {
+    return (
+      <MenuItem
+        {...props}
+        render={<Component className={cx('item', 'action', className)} ref={ref} />}
+      />
+    )
+  }
+)
