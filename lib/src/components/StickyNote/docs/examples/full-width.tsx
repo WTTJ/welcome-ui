6d619844a11a5fe527abb852068363735d74a946
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2025-12-11 10:56:47 +0100

    feat: add full width option on StickyNote (#3110)

---
+++ lib/src/components/StickyNote/docs/examples/full-width.tsx
@@ -0,0 +1,18 @@
+import { StickyNote } from '@/components/StickyNote'
+
+const Example = () => {
+  return (
+    <div className="flex flex-wrap gap-lg">
+      <StickyNote isFullWidth>
+        <StickyNote.Title>Title</StickyNote.Title>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </StickyNote>
+      <StickyNote isFullWidth shape="rectangle">
+        <StickyNote.Title>Title</StickyNote.Title>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </StickyNote>
+    </div>
+  )
+}
+
+export default Example
