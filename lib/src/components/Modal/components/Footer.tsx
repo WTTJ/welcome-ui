97df5d93386bbfe3af87e2682247d628b8dd5d7c
cnairi <camillenairi@icloud.com>
2025-12-18 16:37:36 +0100

    chore(modal): init modal belonging version

--- lib/src/components/Modal/components/Footer.tsx
+++ lib/src/components/Modal/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { forwardRef } from 'react'
 
-import { Text } from '@/components/Text'
 import { classNames } from '@/utils'
 
 import modalStyles from '../modal.module.scss'
@@ -12,20 +11,10 @@ const cx = classNames(modalStyles)
  * @name Modal.Footer
  */
 export const Footer = forwardRef<HTMLDivElement, FooterProps>(
-  ({ children, className, information, ...rest }, ref) => {
+  ({ children, className, ...rest }, ref) => {
     return (
       <footer className={cx('footer', className)} ref={ref} {...rest}>
         {children ? <div className={cx('footer-children-wrapper')}>{children}</div> : null}
-        {information ? (
-          <div className={cx('footer-information')}>
-            <Text className={cx('footer-information-title')} variant="label-md">
-              {information.title}
-            </Text>
-            <Text className={cx('footer-information-subtitle')} variant="body-md">
-              {information.subtitle}
-            </Text>
-          </div>
-        ) : null}
       </footer>
     )
   }
