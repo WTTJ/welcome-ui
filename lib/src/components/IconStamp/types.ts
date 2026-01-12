9f7723a6ffb01befb3cc9c9f3afe36c90f0ba525
Simon Degraeve <simon.degraeve@gmail.com>
2026-01-12 07:46:31 +0100

    feat: add `IconStamp` component (#3132)

---
+++ lib/src/components/IconStamp/types.ts
@@ -0,0 +1,24 @@
+import type { ComponentPropsWithRef, HTMLAttributes } from 'react'
+
+import type { IconName } from '@/components/Icon/types'
+
+export type IconStampProps = ComponentPropsWithRef<'div'> &
+  HTMLAttributes<HTMLDivElement> &
+  IconStampOptions
+
+export type IconStampVariant =
+  | 'blue'
+  | 'brand'
+  | 'green'
+  | 'orange'
+  | 'pink'
+  | 'red'
+  | 'teal'
+  | 'violet'
+  | 'warm'
+
+interface IconStampOptions {
+  name: IconName
+  size?: 'lg' | 'md' | 'sm'
+  variant?: IconStampVariant
+}
