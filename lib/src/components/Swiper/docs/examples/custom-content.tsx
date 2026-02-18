ed2086040e0a8c1d3bdd788cd35577106a33f250
Jerome Raffin <raffin.jerome@gmail.com>
2026-02-18 18:10:26 +0100

    feat: update examples according to new component

---
+++ lib/src/components/Swiper/docs/examples/custom-content.tsx
@@ -0,0 +1,46 @@
+import { Swiper, useSwiper } from '@/components/Swiper'
+import { Text } from '@/components/Text'
+
+const Example = () => {
+  const swiper = useSwiper({ slides: { perView: { desktop: 2, mobile: 1, tablet: 2 } } })
+
+  return (
+    <Swiper className="h-400" store={swiper}>
+      <div className="flex justify-between items-center mb-xl">
+        <Text variant="heading-lg">Swiper</Text>
+        <div className="flex gap-4">
+          <Swiper.PrevButton placement="inline" />
+          <Swiper.NextButton placement="inline" />
+        </div>
+      </div>
+      <Swiper.Slides>
+        <img
+          src="https://images.unsplash.com/photo-1564460549828-f0219a31bf90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+        <img
+          src="https://images.unsplash.com/photo-1575489272413-cb506258027e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+        <img
+          src="https://images.unsplash.com/photo-1541959833400-049d37f98ccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+        <img
+          src="https://images.unsplash.com/photo-1564460549828-f0219a31bf90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+        <img
+          src="https://images.unsplash.com/photo-1575489272413-cb506258027e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+        <img
+          src="https://images.unsplash.com/photo-1541959833400-049d37f98ccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'cover' }}
+        />
+      </Swiper.Slides>
+    </Swiper>
+  )
+}
+
+export default Example
