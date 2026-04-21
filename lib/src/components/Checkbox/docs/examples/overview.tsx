d8e12f2d9f7f24a7a2cef2fa471b0db35d8d050e
isaacmaamouche <maamouche.isaac@gmail.com>
2026-04-21 10:15:40 +0200

    chore: update docs

--- lib/src/components/Checkbox/docs/examples/overview.tsx
+++ lib/src/components/Checkbox/docs/examples/overview.tsx
@@ -1,17 +1,38 @@
+import { useState } from 'react'
+
 import { Checkbox } from '@/components/Checkbox'
 
 const Example = () => {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = (value: boolean) => {
+    setChecked(value)
+    alert(`Checkbox is now ${value ? 'checked' : 'unchecked'}`)
+  }
+
   return (
-    <>
-      <Checkbox
-        name="default"
-        onChange={value => alert(`Checkbox is now ${value ? 'checked' : 'unchecked'}`)}
-      />
-      <Checkbox checked name="not-checked" />
-      <Checkbox indeterminate name="indeterminate" />
-      <Checkbox disabled name="default-disabled" />
-      <Checkbox checked disabled name="not-checked-disabled" />
-    </>
+    <ul className="flex flex-col gap-md mx-auto">
+      <li className="flex gap-sm items-center">
+        <Checkbox checked={checked} name="default" onChange={handleChange} />
+        <span>Checkbox: controlled state</span>
+      </li>
+      <li className="flex gap-sm items-center">
+        <Checkbox checked={false} name="not-checked" />
+        <p>Checkbox: false</p>
+      </li>
+      <li className="flex gap-sm items-center">
+        <Checkbox indeterminate name="indeterminate" />
+        <p>Checkbox: indeterminate</p>
+      </li>
+      <li className="flex gap-sm items-center">
+        <Checkbox disabled name="default-disabled" />
+        <p>Checkbox: disabled</p>
+      </li>
+      <li className="flex gap-sm items-center">
+        <Checkbox checked disabled name="not-checked-disabled" />
+        <p>Checkbox: checked & disabled</p>
+      </li>
+    </ul>
   )
 }
 
--- lib/src/components/Checkbox/docs/examples/variants.tsx
+++ lib/src/components/Checkbox/docs/examples/variants.tsx
@@ -6,26 +6,37 @@ const Example = () => {
   const [checkbox, setCheckbox] = React.useState(false)
 
   return (
-    <div className="items-center flex gap-sm">
-      <Checkbox
-        checked={checkbox}
-        name="default-variant"
-        onChange={() => setCheckbox(!checkbox)}
-        variant="danger"
-      />
-      <Checkbox
-        checked={checkbox}
-        name="default-variant"
-        onChange={() => setCheckbox(!checkbox)}
-        variant="warning"
-      />
-      <Checkbox
-        checked={checkbox}
-        name="default-variant"
-        onChange={() => setCheckbox(!checkbox)}
-        variant="success"
-      />
-    </div>
+    <ul className="flex flex-col gap-md mx-auto">
+      <li className="flex gap-sm items-center">
+        <Checkbox
+          checked={checkbox}
+          name="danger-variant"
+          onChange={() => setCheckbox(!checkbox)}
+          variant="danger"
+        />
+        <span>Checkbox: danger variant</span>
+      </li>
+
+      <li className="flex gap-sm items-center">
+        <Checkbox
+          checked={checkbox}
+          name="warning-variant"
+          onChange={() => setCheckbox(!checkbox)}
+          variant="warning"
+        />
+        <span>Checkbox: warning variant</span>
+      </li>
+
+      <li className="flex gap-sm items-center">
+        <Checkbox
+          checked={checkbox}
+          name="success-variant"
+          onChange={() => setCheckbox(!checkbox)}
+          variant="success"
+        />
+        <span>Checkbox: success variant</span>
+      </li>
+    </ul>
   )
 }
 
