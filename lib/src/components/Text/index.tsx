d940eef20b6293e5272011d15253641a968b26a9
Jerome Raffin <raffin.jerome@gmail.com>
2026-05-13 09:58:23 +0200

    fix: text component lint error and types

--- lib/src/components/Text/index.tsx
+++ lib/src/components/Text/index.tsx
@@ -1,16 +1,16 @@
-import { createElement, type CSSProperties } from 'react'
+import { type CSSProperties } from 'react'
 
 import { classNames, forwardRefWithAs } from '@/utils'
 
 import textStyles from './text.module.scss'
-import type { TextProps } from './types'
+import type { TextOptions } from './types'
 
 export { textStyles as textClasses }
 
 const cx = classNames(textStyles)
 
-export const Text = forwardRefWithAs<TextProps, 'p'>(
-  ({ as = 'p', children, className, lines, variant = 'md', withDash, ...rest }, ref) => {
+export const Text = forwardRefWithAs<TextOptions, 'p'>(
+  ({ as: Component = 'p', children, className, lines, variant = 'md', withDash, ...rest }, ref) => {
     const isHeading = variant?.startsWith('heading') || variant?.startsWith('display')
     const isMultiLine = lines && lines > 1 && lines !== Number.POSITIVE_INFINITY
     const isSingleLine = lines === 1
@@ -18,8 +18,8 @@ export const Text = forwardRefWithAs<TextProps, 'p'>(
     const classes = cx(
       'root',
       `variant-${variant}`,
-      lines && 'with-lines',
-      isMultiLine && 'multi-line',
+      Boolean(lines) && 'with-lines',
+      Boolean(isMultiLine) && 'multi-line',
       isSingleLine && 'single-line',
       withDash && isHeading && 'with-dash',
       className
@@ -27,15 +27,15 @@ export const Text = forwardRefWithAs<TextProps, 'p'>(
 
     const cssVariables = isMultiLine || isSingleLine ? { '--lineClamp': lines } : {}
 
-    return createElement(
-      as,
-      {
-        className: classes,
-        ref,
-        style: { ...cssVariables } as CSSProperties,
-        ...rest,
-      },
-      children
+    return (
+      <Component
+        className={classes}
+        ref={ref}
+        style={{ ...cssVariables } as CSSProperties}
+        {...rest}
+      >
+        {children}
+      </Component>
     )
   }
 )
