4d25bdf335e72ed42834e558bc3f2a959db97abe
cnairi <camille.nairi@wttj.co>
2026-09-23 15:39:38 +0200

    fix(InputText): fix styling for hover states, variants and transparent

--- lib/src/components/InputText/docs/examples/transparent.tsx
+++ lib/src/components/InputText/docs/examples/transparent.tsx
@@ -2,7 +2,16 @@ import { Icon } from '@/components/Icon'
 import { InputText } from '@/components/InputText'
 
 const Example = () => {
-  return <InputText icon={<Icon name="user-circle" />} placeholder="Welcome" transparent />
+  return (
+    <div className="flex flex-col gap-md">
+      <InputText icon={<Icon name="user-circle" />} placeholder="Welcome" transparent />
+      <InputText placeholder="Welcome" transparent />
+      <InputText placeholder="Welcome" transparent variant="warning" />
+      <InputText placeholder="Welcome" transparent variant="danger" />
+      <InputText placeholder="Welcome" transparent variant="success" />
+      <InputText disabled placeholder="Welcome" transparent />
+    </div>
+  )
 }
 
 export default Example
